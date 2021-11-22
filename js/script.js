fc264b7791c884df8d713ae9d77cc20653cae321
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-22 20:03:13 +0300

    Square Every Digit (7 kyu)

--- js/script.js
+++ js/script.js
@@ -197,3 +197,23 @@ console.log(encryptThis("The more he saw the less he spoke"));
 console.log(encryptThis("The less he spoke the more he heard"));
 console.log(encryptThis("Why can we not all be like that wise old bird"));
 console.log(encryptThis("Thank you Piotr for all your help"));
+
+// Square Every Digit (7 kyu)
+
+/*
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+*/
+
+function squareDigits(num) {
+
+	return parseInt(num.toString().split('').map((item) => item * item).join(''), 10);
+
+}
+
+console.log(squareDigits(3212));//9414
+console.log(squareDigits(2112));//4114
+console.log(squareDigits(0));//0
