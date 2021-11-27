285a3b73b87e1a11ca28e2c53933ff423783a52d
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-27 09:33:57 +0300

    Simple Pig Latin (5 kyu)

--- js/script.js
+++ js/script.js
@@ -336,3 +336,28 @@ console.log(descendingOrder(15));
 console.log(descendingOrder(1021));
 console.log(descendingOrder(123456789));
 console.log(descendingOrder(865974));
+
+// Simple Pig Latin (5 kyu)
+
+/*
+Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+*/
+
+function pigIt(str) {
+
+	return str
+		.split(' ')
+		.map(item => {
+			return item === '!' || item === '?' ? item : item.substring(1) + item[0] + 'ay';
+		})
+		.join(' ');
+
+}
+
+console.log(pigIt('Pig latin is cool'));
+console.log(pigIt('This is my string'));
