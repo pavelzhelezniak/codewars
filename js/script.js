044ea3bbb5b042711562683490a4c845726b67b5
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-28 20:48:16 +0300

    Sum of two lowest positive integers (7 kyu)

--- js/script.js
+++ js/script.js
@@ -361,3 +361,25 @@ function pigIt(str) {
 
 console.log(pigIt('Pig latin is cool'));
 console.log(pigIt('This is my string'));
+
+// Sum of two lowest positive integers (7 kyu)
+
+/*
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.
+No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+*/
+
+function sumTwoSmallestNumbers(numbers) {
+	const sortArray = numbers.sort((a, b) => a - b);
+	return sortArray[0] + sortArray[1];
+}
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]));
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]));
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]));
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]));
