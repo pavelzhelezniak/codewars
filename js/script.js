734e226d3808d4ec0f1d17af4d9924921b3f3aa9
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-07 12:04:38 +0300

    Number of Decimal Digits (7 kyu)

--- js/script.js
+++ js/script.js
@@ -622,3 +622,21 @@ console.log(generateHashtag("Loooooooooooooooooooooooooooooooooooooooooooooooooo
 console.log(generateHashtag("code" + " ".repeat(140) + "wars"));
 console.log(generateHashtag("a".repeat(139)));
 console.log(generateHashtag("a".repeat(140)));
+
+// Number of Decimal Digits (7 kyu)
+
+/*
+Determine the total number of digits in the integer (n>=0) given as input to the function.
+For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits.
+Be careful to avoid overflows/underflows.
+
+All inputs will be valid.
+*/
+
+function digits(n) {
+	return n.toString().length;
+}
+
+console.log(digits(5));
+console.log(digits(12345));
+console.log(digits(9876543210));
