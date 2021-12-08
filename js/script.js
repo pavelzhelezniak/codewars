786f0e51940e2ef99fb384285cd983e514d7bcf0
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-08 13:11:31 +0300

    IP Validation (6 kyu) not resolved!!!!!

--- js/script.js
+++ js/script.js
@@ -691,3 +691,83 @@ console.log(isPrime(45));
 console.log(isPrime(-5));
 console.log(isPrime(-8));
 console.log(isPrime(-41));
+
+// IP Validation (6 kyu)
+
+/*
+Write an algorithm that will identify valid IPv4 addresses in dot-decimal format.
+Ps should be considered valid if they consist of four octets,
+with values between 0 and 255, inclusive.
+
+Valid inputs examples:
+Examples of valid inputs:
+1.2.3.4
+123.45.67.89
+
+Invalid input examples:
+1.2.3
+1.2.3.4.5
+123.456.78.90
+123.045.067.089
+
+Notes:
+Leading zeros (e.g. 01.02.03.04) are considered invalid
+Inputs are guaranteed to be a single string
+*/
+
+/* function isValidIP(str) {
+	let array = str.split('.');
+	if (str === '0.0.0.0') {
+		return true;
+	}
+	if (array.length !== 4) {
+		return false;
+	} else {
+		for (var i = 0; i < array.length; i++) {
+			if (isNaN(Number(array[i]))) {
+				return false;
+			}
+			if (Number(array[i]).toString(10) > 255 || Number(array[i]).toString(10) < 0) {
+				return false;
+			}
+			const newArray = array[i].split('');
+			if (newArray[0] === '0' && typeof Number(newArray[1]) === 'number') {
+				return false;
+			}
+			if (newArray[0] === '\n' || newArray[1] === '\n' || newArray[2] === '\n') {
+				return false;
+			}
+			if (newArray[0] === ' ' || newArray[1] === ' ' || newArray[2] === ' ') {
+				return false;
+			}
+			if (isNaN(Number(newArray[1]))) {
+				return false;
+			}
+
+			// if (parseInt(array[i].split('')) === 0) {
+			// 	return false;
+			// }
+		}
+	}
+
+	return true;
+}
+
+console.log(isValidIP("0.0.0.0"));
+console.log(isValidIP("12.255.56.1"));
+console.log(isValidIP("137.255.156.100"));
+
+console.log(isValidIP(''));
+console.log(isValidIP('abc.def.ghi.jkl'));
+console.log(isValidIP('123.456.789.0'));
+console.log(isValidIP('12.34.56'));
+console.log(isValidIP('01.02.03.04'));
+console.log(isValidIP('256.1.2.3'));
+console.log(isValidIP('1.2.3.4.5'));
+console.log(isValidIP('123,45,67,89'));
+console.log(isValidIP('1e0.1e1.1e2.2e2'));
+console.log(isValidIP(' 1.2.3.4'));
+console.log(isValidIP('1.2.3.4 '));
+console.log(isValidIP('12.34.56.-7'));
+console.log(isValidIP('1.2.3.4\n'));
+console.log(isValidIP('\n1.2.3.4')); */
