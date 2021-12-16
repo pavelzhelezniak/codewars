a2f9d003cf04663f01e31a1e6955ec2374435205
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-16 11:37:36 +0300

    Consecutive strings (6 kyu)

--- js/script.js
+++ js/script.js
@@ -1059,3 +1059,56 @@ console.log(countInversions([1, 2, 3]), 0);
 console.log(countInversions([2, 1, 3]), 1);
 console.log(countInversions([6, 5, 4, 3, 2, 1]), 15);
 console.log(countInversions([6, 5, 4, 3, 3, 3, 3, 2, 1]), 30);
+
+// Consecutive strings (6 kyu)
+
+/* 
+You are given an array(list) strarr of strings and an integer k.
+Your task is to return the first longest string consisting of k
+consecutive strings taken in the array.
+
+Examples:
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2)
+--> "abigailtheta"
+*/
+
+function longestConsec(strarr, k) {
+	let result = '';
+
+	if (strarr.length >= k && k > 0) {
+		let cicleResult = '';
+		for (let i = 0; i <= strarr.length - k; i++) {
+			cicleResult = strarr.slice(i, i + k).join('');
+			if (cicleResult.length > result.length) {
+				result = cicleResult;
+			}
+		}
+	}
+
+	return result;
+}
+
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2));
+console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1));
+console.log(longestConsec([], 3), "");
+console.log(longestConsec(["itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2));
+console.log(longestConsec(["wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu"], 2));
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2));
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3));
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 15));
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 0));
