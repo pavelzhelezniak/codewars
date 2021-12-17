20556d218edf4e92d939b12337df59541c02196e
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-17 19:51:55 +0300

    Bumps in the Road (7 kyu)

--- js/script.js
+++ js/script.js
@@ -1112,3 +1112,143 @@ console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -
 console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3));
 console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 15));
 console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 0));
+
+// Josephus Permutation (5 kyu)
+
+/*
+This problem takes its name by arguably the most important event in the life of the ancient historian Josephus: according to his tale, he and his 40 soldiers were trapped in a cave by the Romans during a siege.
+
+Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they formed a circle and proceeded to kill one man every three, until one last man was left (and that it was supposed to kill himself to end the act).
+
+Well, Josephus and another man were the last two and, as we now know every detail of the story, you may have correctly guessed that they didn't exactly follow through the original idea.
+
+You are now to create a function that returns a Josephus permutation, taking as parameters the initial array/list of items to be permuted as if they were in a circle and counted out every k places until none remained.
+
+Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0..n-1; k will always be >=1.
+
+For example, with n=7 and k=3 josephus(7,3) should act this way.
+
+[1,2,3,4,5,6,7] - initial sequence
+[1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+[1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+[1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+[1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+[1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+[4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+[] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
+So our final result is:
+
+josephus([1,2,3,4,5,6,7],3)==[3,6,2,7,5,1,4]
+For more info, browse the Josephus Permutation page on wikipedia; related kata: Josephus Survivor.
+
+Also, live game demo by OmniZoetrope.
+*/
+
+/*
+Эта проблема получила свое название от, пожалуй, самого важного события в жизни древнего историка Иосифа Флавия: согласно его рассказу, он и его 40 солдат были пойманы в пещере римлянами во время осады.
+
+Отказавшись сдаться врагу, они вместо этого сделали выбор в пользу массового самоубийства с изюминкой: они образовали круг и продолжали убивать одного человека каждые три, пока не остался один последний человек (и предполагалось, что он должен убить себя, чтобы закончить акт). ) .
+
+Итак, Джозефус и еще один мужчина были последними двумя, и, поскольку мы теперь знаем каждую деталь этой истории, вы, возможно, правильно догадались, что они не полностью следовали первоначальной идее.
+
+Теперь вы должны создать функцию, которая возвращает перестановку Иосифа Флавия, принимая в качестве параметров исходный массив / список элементов, которые должны быть переставлены, как если бы они были в круге, и отсчитывали каждые k мест, пока не осталось ни одного.
+
+Подсказки и примечания: помогает начать счет от 1 до n вместо обычного диапазона 0..n-1; k всегда будет> = 1.
+
+Например, при n = 7 и k = 3 josephus(7,3)должно действовать именно так.
+
+[1,2,3,4,5,6,7] - initial sequence
+[1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+[1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+[1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+[1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+[1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+[4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+[] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
+Итак, наш окончательный результат:
+
+josephus([1,2,3,4,5,6,7],3)==[3,6,2,7,5,1,4]
+Для получения дополнительной информации просмотрите страницу перестановки Иосифа Флавия в википедии; связанное ката: Иосиф Флавий, оставшийся в живых .
+
+А также живая демо-версия игры от OmniZoetrope .
+*/
+
+/*
+function josephus(items,k){
+  //your code here
+}
+
+function returnedArray(array, startposition, shift) {
+  return array.filter((item, index) => {
+	 if (index > startposition) {
+		return true;
+	 }
+
+	 return (index % (startposition + shift) === 0);
+  });
+}
+
+function changedStartArray(array, startposition, shift) {
+  return array.filter((item, index) => {
+	 if (index < startposition) {
+		return true;
+	 }
+
+	 return (index % (startposition + shift) !== 0);
+  })
+}
+
+function work(array, startposition, shift) {
+  let startItems = array;
+  let result = [];
+  if(startposition + shift > startItems.length) {
+	 return startItems;
+  } else {
+	 const firstPart = changedStartArray(startItems, startposition, shift);
+	 const secondPart = returnedArray(startItems, startposition, shift);
+	 return work([...firstPart, ...secondPart], firstPart.length, shift);
+  }
+}
+*/
+
+/* function josephus(items, k) {
+	//your code here
+}
+
+console.log(josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2), [2, 4, 6, 8, 10, 3, 7, 1, 9, 5]);
+console.log(josephus(["C", "o", "d", "e", "W", "a", "r", "s"], 4), ['e', 's', 'W', 'o', 'C', 'd', 'r', 'a']);
+console.log(josephus([1, 2, 3, 4, 5, 6, 7], 3), [3, 6, 2, 7, 5, 1, 4]);
+console.log(josephus([], 3), []); */
+
+// Bumps in the Road (7 kyu)
+
+/* 
+Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle
+about 15 more bumps before it dies totally.
+
+Unfortunately for you, your drive is very bumpy!
+Given a string showing either flat road ("_") or bumps ("n"),work out if you make it home safely. 
+15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
+*/
+
+function bump(x) {
+	let count = 15;
+
+	for (let i = 0; i < x.length; i++) {
+		if (x[i] === 'n') {
+			count--;
+		}
+	}
+
+	if (count < 0) {
+		return 'Car Dead';
+	}
+
+	return 'Woohoo!';
+
+}
+
+console.log(bump('n'), 'Woohoo!');
+console.log(bump('_nnnnnnn_n__n______nn__nn_nnn'), 'Car Dead');
+console.log(bump('______n___n_'), 'Woohoo!');
