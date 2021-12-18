04378e64d6c1c04afc6bbfc5b8387ade730bbc81
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-18 17:02:32 +0300

    Power of two (7 kyu)

--- js/script.js
+++ js/script.js
@@ -1252,3 +1252,26 @@ function bump(x) {
 console.log(bump('n'), 'Woohoo!');
 console.log(bump('_nnnnnnn_n__n______nn__nn_nnn'), 'Car Dead');
 console.log(bump('______n___n_'), 'Woohoo!');
+
+// Power of two (7 kyu)
+
+/* 
+Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language)
+that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+
+a power of two is a number of the form 2n where n is an integer,
+i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+
+You may assume the input is always valid.
+
+Beware of certain edge cases - for example, 1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.
+*/
+
+
+function isPowerOfTwo(n) {
+	return Number.isInteger(Math.log2(n));
+}
+
+console.log(isPowerOfTwo(2), true);
+console.log(isPowerOfTwo(4096), true);
+console.log(isPowerOfTwo(5), false);
