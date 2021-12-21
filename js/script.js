ea7e8fec1377c19d0e5bbd2fcee1d7c9bb1fdacc
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-21 15:35:21 +0300

    Single digit (7 kyu)

--- js/script.js
+++ js/script.js
@@ -1305,3 +1305,46 @@ console.log(reverseNumber(4321234), 4321234);
 console.log(reverseNumber(5), 5);
 console.log(reverseNumber(0), 0);
 console.log(reverseNumber(98989898), 89898989);
+
+// Single digit (7 kyu)
+
+/* 
+The goal of this Kata is to reduce the passed integer to a single digit (if not already)
+by converting the number to binary, taking the sum of the binary digits,
+and if that sum is not a single digit then repeat the process.
+
+If the passed integer is already a single digit there is no need to reduce
+n will be an integer such that 0 < n < 10⁹
+For example given 5665 the function should return 5:
+
+5665 --> (binary) 1011000100001
+1011000100001 --> (sum of binary digits) 5
+Given 123456789 the function should return 1:
+
+123456789 --> (binary) 111010110111100110100010101
+111010110111100110100010101 --> (sum of binary digits) 16
+16 --> (binary) 10000
+10000 --> (sum of binary digits) 1
+*/
+
+function singleDigit(n) {
+	if (n < 10) {
+		return n;
+	}
+
+	const binaryNumber = n.toString(2)
+		.split('')
+		.reduce((acc, item) => parseInt(acc, 10) + parseInt(item, 10));
+
+	return singleDigit(binaryNumber);
+}
+
+console.log(singleDigit(5), 5);
+console.log(singleDigit(999), 8);
+console.log(singleDigit(1234444123), 1);
+console.log(singleDigit(443566), 2);
+console.log(singleDigit(565656565), 3);
+console.log(singleDigit(4868872), 8);
+console.log(singleDigit(234234235), 2);
+console.log(singleDigit(567448), 7);
+console.log(singleDigit(1000000000), 3);
