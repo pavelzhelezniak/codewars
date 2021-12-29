6f5a355ed933f6747fc2fccf9a2b0b8d314f92ee
pavelzhelezniak <zhele3o1992@gmail.com>
2021-12-29 12:53:27 +0300

    Highest Scoring Word (6 kyu)

--- js/script.js
+++ js/script.js
@@ -1639,3 +1639,77 @@ console.log(likes(['Peter']), 'Peter likes this');
 console.log(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
 console.log(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
 console.log(likes(['Alex', 'Jacob', 'Mark', 'Max', 'Pavel', 'Max']), 'Alex, Jacob and 4 others like this');
+
+// Highest Scoring Word (6 kyu)
+
+/* 
+Given a string of words, you need to find the highest scoring word.
+Each letter of a word scores points according to its position in the alphabet:
+a = 1, b = 2, c = 3 etc.
+You need to return the highest scoring word as a string.
+If two words score the same, return the word that appears earliest in the original string.
+All letters will be lowercase and all inputs will be valid.
+*/
+
+function high(x) {
+	const ScoreTable = {
+		'a': 1,
+		'c': 3,
+		'b': 2,
+		'd': 4,
+		'e': 5,
+		'f': 6,
+		'g': 7,
+		'h': 8,
+		'i': 9,
+		'j': 10,
+		'k': 11,
+		'l': 12,
+		'm': 13,
+		'n': 14,
+		'o': 15,
+		'p': 16,
+		'q': 17,
+		'r': 18,
+		's': 19,
+		't': 20,
+		'u': 21,
+		'v': 22,
+		'w': 23,
+		'x': 24,
+		'y': 25,
+		'z': 26
+	};
+
+	const arrayWords = x.split(' ');
+	const arrayNumber = [];
+
+	for (let i = 0; i < arrayWords.length; i++) {
+		arrayNumber.push(scoreCalculation(arrayWords[i]));
+
+	}
+
+	function scoreCalculation(word) {
+		let count = 0;
+		for (let i = 0; i < word.length; i++) {
+			if (word[i] in ScoreTable) {
+				count += ScoreTable[word[i]];
+			}
+		}
+
+		return count;
+	}
+
+	const indexMax = arrayNumber.indexOf(Math.max(...arrayNumber));
+
+	return arrayWords[indexMax];
+}
+
+console.log(high('man i need a taxi up to ubud'), 'taxi');
+console.log(high('what time are we climbing up the volcano'), 'volcano');
+console.log(high('take me to semynak'), 'semynak');
+console.log(high('aa b'), 'aa');
+console.log(high('b aa'), 'b');
+console.log(high('bb d'), 'bb');
+console.log(high('d bb'), 'd');
+console.log(high('aaa b'), 'aaa');
