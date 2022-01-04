0b43da033b30947387289f607382f37a0de1ec72
pavelzhelezniak <zhele3o1992@gmail.com>
2022-01-04 19:28:55 +0300

    Maximum Product (7 kyu)

--- js/script.js
+++ js/script.js
@@ -1884,3 +1884,34 @@ console.log(calc('ifkhchlhfd'), 6);
 console.log(calc('aaaaaddddr'), 30);
 console.log(calc('jfmgklf8hglbe'), 6);
 console.log(calc('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 96);
+
+// Maximum Product (7 kyu)
+
+/* 
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+*/
+
+function adjacentElementsProduct(array) {
+	const newArray = [];
+
+	array.forEach((item, index, array) => {
+		if (index !== array.length - 1) {
+			newArray.push(item * array[index + 1])
+		}
+	})
+
+	return Math.max(...newArray)
+}
+
+console.log(adjacentElementsProduct([5, 8]), 40);
+console.log(adjacentElementsProduct([1, 2, 3]), 6);
+console.log(adjacentElementsProduct([1, 5, 10, 9]), 90);
+console.log(adjacentElementsProduct([4, 12, 3, 1, 5]), 48);
+console.log(adjacentElementsProduct([5, 1, 2, 3, 1, 4]), 6);
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]), 21);
+console.log(adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]), 50);
+console.log(adjacentElementsProduct([5, 6, -4, 2, 3, 2, -23]), 30);
+console.log(adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921]), -14);
+console.log(adjacentElementsProduct([5, 1, 2, 3, 1, 4]), 6);
+console.log(adjacentElementsProduct([1, 0, 1, 0, 1000]), 0);
+console.log(adjacentElementsProduct([1, 2, 3, 0]), 6);
