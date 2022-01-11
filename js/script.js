2f9851c73d46bb4b5c1d7e83366e9de8fb3fcd92
pavelzhelezniak <zhele3o1992@gmail.com>
2022-01-11 13:12:21 +0300

    Pyramid Array (6 kyu)

--- js/script.js
+++ js/script.js
@@ -2107,3 +2107,35 @@ console.log(zeroPlentiful([3]), 0);
 console.log(zeroPlentiful([0, 0, 0, 0, 0, 0]), 1);
 console.log(zeroPlentiful([0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 1, 0, 0]), 2);
 console.log(zeroPlentiful([1, 0, 0, 0, 0, 1, 0, 0, 0]), 1);
+
+// Pyramid Array (6 kyu)
+
+/* 
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+Note: the subarrays should be filled with 1s
+*/
+
+function pyramid(n) {
+	const arr = [];
+
+	for (let i = 0; i < n; i++) {
+		const newArr = [];
+
+		for (let k = 0; k <= i; k++) {
+			newArr.push(1);
+		}
+		arr.push(newArr)
+	}
+
+	return arr;
+}
+
+console.log(pyramid(0), []);
+console.log(pyramid(1), [[1]]);
+console.log(pyramid(2), [[1], [1, 1]]);
+console.log(pyramid(3), [[1], [1, 1], [1, 1, 1]]);
