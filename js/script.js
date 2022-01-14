bf188cfbfc24ad183236da86f0f59b99812ec602
pavelzhelezniak <zhele3o1992@gmail.com>
2022-01-14 12:25:06 +0300

    Friend or Foe?

--- js/script.js
+++ js/script.js
@@ -2265,3 +2265,27 @@ console.log(XO("xxOo"), true);
 console.log(XO("xxxm"), false);
 console.log(XO("Oo"), false);
 console.log(XO("ooom"), false);
+
+// Friend or Foe? (7 kyu)
+
+/* 
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.
+*/
+
+function friend(friends) {
+	return friends.filter(item => item.length === 4);
+}
+
+console.log(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"])
+console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"])
+console.log(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"])
+console.log(friend(["Love", "Your", "Face", "1"]), ["Love", "Your", "Face"])
