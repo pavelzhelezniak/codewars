455f0bdc8e0c18215d381926d969bd7520f2b7e2
pavelzhelezniak <zhele3o1992@gmail.com>
2022-01-20 18:10:59 +0300

    Find the stray number (7 kyu)

--- js/script.js
+++ js/script.js
@@ -2419,3 +2419,37 @@ function removeDuplicateWords(s) {
 }
 
 console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'), 'alpha beta gamma delta');
+
+// Find the stray number (7 kyu)
+
+/* 
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+*/
+
+function stray(numbers) {
+	const obj = {}
+
+	for (let value of numbers) {
+		if (value in obj) {
+			obj[value]++;
+		} else {
+			obj[value] = 1;
+		}
+	}
+	for (let value in obj) {
+		if (obj[value] === 1) {
+			return +value;
+		}
+	}
+}
+
+console.log(stray([1, 1, 2]), 2);
+console.log(stray([17, 17, 3, 17, 17, 17, 17]), 3);
