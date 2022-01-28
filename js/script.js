272f6a3b77f7174729141910251676967b54fc1c
pavelzhelezniak <zhele3o1992@gmail.com>
2022-01-28 15:34:44 +0300

    Leap Years (7 kyu)

--- js/script.js
+++ js/script.js
@@ -2649,3 +2649,27 @@ const greet = function (name) {
 
 console.log(greet('riley'), 'Hello Riley!');
 console.log(greet('JACK'), 'Hello Jack!');
+
+// Leap Years (7 kyu)
+
+/* 
+In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+years divisible by 4 are leap years
+but years divisible by 100 are not leap years
+but years divisible by 400 are leap years
+Additional Notes:
+
+Only valid years (positive integers) will be tested, so you don't have to validate them
+Examples can be found in the test fixture.
+ */
+
+function isLeapYear(year) {
+	return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+}
+
+console.log(isLeapYear(1234), false, 'Year 1234');
+console.log(isLeapYear(1984), true, 'Year 1984');
+console.log(isLeapYear(2000), true, 'Year 2000');
+console.log(isLeapYear(2010), false, 'Year 2010');
+console.log(isLeapYear(2013), false, 'Year 2013');
