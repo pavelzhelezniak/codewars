36f285cc2b7ffbebfa6b8c0b97dc67b7032283ec
pavelzhelezniak <zhele3o1992@gmail.com>
2022-01-30 22:45:39 +0300

    Switcheroo (7 kyu)

--- js/script.js
+++ js/script.js
@@ -2696,3 +2696,32 @@ function count(array) {
 }
 
 console.log(count(['a', 'a', 'b', 'b', 'b']), { 'a': 2, 'b': 3 });
+
+// Switcheroo (7 kyu)
+
+/* 
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+function switcheroo(x) {
+	let newStr = '';
+	for (let i = 0; i < x.length; i++) {
+		if (x[i] === 'a') {
+			newStr += 'b'
+		} else if (x[i] === 'b') {
+			newStr += 'a'
+		} else {
+			newStr += x[i];
+		}
+	}
+	return newStr;
+}
+
+console.log(switcheroo('abc'), 'bac');
+console.log(switcheroo('aaabcccbaaa'), 'bbbacccabbb');
+console.log(switcheroo('ccccc'), 'ccccc'); 
