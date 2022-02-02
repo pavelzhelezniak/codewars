5204737e00b90c0ed81e9dcdbf7f5f03dbb0902d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-02 17:28:18 +0300

    very possible sum of two digits (7 kyu)

--- js/script.js
+++ js/script.js
@@ -2869,3 +2869,33 @@ console.log(multiplyAll([1, 2, 3])(2), [2, 4, 6]);
 console.log(multiplyAll([1, 2, 3])(0), [0, 0, 0]);
 console.log(multiplyAll([1, 2, 3])(1), [1, 2, 3]);
 console.log(multiplyAll([])(10), [], "should return an empty array");
+
+// Every possible sum of two digits (7 kyu)
+
+/* 
+Given a long number, return all the possible sum of two digits of it.
+
+For example, 12345: all possible sum of two digits from that number are:
+
+[ 1 + 2, 1 + 3, 1 + 4, 1 + 5, 2 + 3, 2 + 4, 2 + 5, 3 + 4, 3 + 5, 4 + 5 ]
+Therefore the result must be:
+
+[ 3, 4, 5, 6, 5, 6, 7, 7, 8, 9 ]
+*/
+
+function digits(num) {
+	const arr = [];
+	num = num.toString();
+	for (let i = 0; i < num.length; i++) {
+		for (let k = i + 1; k < num.length; k++) {
+			arr.push(parseInt(num[i]) + parseInt(num[k]));
+		}
+	}
+	return arr;
+}
+
+console.log(digits(156), [6, 7, 11]);
+console.log(digits(81596), [9, 13, 17, 14, 6, 10, 7, 14, 11, 15]);
+console.log(digits(3852), [11, 8, 5, 13, 10, 7]);
+console.log(digits(3264128), [5, 9, 7, 4, 5, 11, 8, 6, 3, 4, 10, 10, 7, 8, 14, 5, 6, 12, 3, 9, 10]);
+console.log(digits(999999), [18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18]);
