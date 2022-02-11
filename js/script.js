0dc4a341f6c6f00f861d1b6199a118c547b9f4b9
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-11 19:37:52 +0300

    Remove the minimum (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3225,3 +3225,34 @@ console.log(validatePIN("098765"), true, "Wrong output for '098765'");
 console.log(validatePIN("000000"), true, "Wrong output for '000000'");
 console.log(validatePIN("123456"), true, "Wrong output for '123456'");
 console.log(validatePIN("090909"), true, "Wrong output for '090909'");
+
+// Remove the minimum (7 kyu)
+
+/* 
+The museum of incredible dull things
+The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+Task
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+Examples
+removeSmallest([1,2,3,4,5]) = [2,3,4,5]
+removeSmallest([5,3,2,1,4]) = [5,3,2,4]
+removeSmallest([2,2,1,2,1]) = [2,2,2,1]
+*/
+
+function removeSmallest(numbers) {
+	const numbersCopy = [...numbers];
+	const indexNumMin = numbersCopy.indexOf(Math.min(...numbersCopy));
+	numbersCopy.splice(indexNumMin, 1);
+	return numbersCopy;
+}
+
+console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]");
+console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]");
+console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]");
+console.log(removeSmallest([]), [], "Wrong result for []");
