f32d1fdec3590f2656417a81eefa01404f865a91
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-13 16:15:13 +0300

    Two to One (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3307,3 +3307,33 @@ console.log(spinWords("This is another test"), "This is rehtona test");
 console.log(spinWords("You are almost to the last test"), "You are tsomla to the last test");
 console.log(spinWords("Just kidding there is still one more"), "Just gniddik ereht is llits one more");
 console.log(spinWords("Seriously this is the last one"), "ylsuoireS this is the last one");
+
+// Two to One (7 kyu)
+
+/* 
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+*/
+
+function longest(s1, s2) {
+	let a = [... new Set(s1.split(""))];
+	let b = [... new Set(s2.split(""))];
+
+	for (let i = 0; i < b.length; i++) {
+		a.push(b[i]);
+	}
+	let initCombo = [... new Set(a)];
+	let combo = initCombo.sort().join("");
+	return combo;
+}
+
+console.log(longest("aretheyhere", "yestheyarehere"), "aehrsty")
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu")
+console.log(longest("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
