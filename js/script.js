d8c60707b7c1196e9da4505c1fe594f6523465f1
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-15 12:32:54 +0300

    Sum of a sequence (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3356,3 +3356,33 @@ function addBinary(a, b) {
 console.log(addBinary(1, 2))
 console.log(addBinary(2, 3))
 console.log(addBinary(3, 4))
+
+//Sum of a sequence (7 kyu)
+
+/*Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)*/
+
+const sequenceSum = (begin, end, step) => {
+	let count = 0;
+	if (end < begin) {
+		return 0;
+	}
+	for (let i = begin; i <= end; i += step) {
+		count += i;
+	}
+	return count;
+};
+
+console.log(sequenceSum(2, 6, 2), 12)
+console.log(sequenceSum(1, 5, 1), 15)
+console.log(sequenceSum(1, 5, 3), 5) 
