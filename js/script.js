c6c30396edb3ad4c3bdb1acdcf025a7413093c0e
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-16 14:05:20 +0300

    Make a function that does arithmetic! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3385,4 +3385,40 @@ const sequenceSum = (begin, end, step) => {
 
 console.log(sequenceSum(2, 6, 2), 12)
 console.log(sequenceSum(1, 5, 1), 15)
-console.log(sequenceSum(1, 5, 3), 5) 
+console.log(sequenceSum(1, 5, 3), 5)
+
+// Make a function that does arithmetic! (7 kyu)
+
+/* 
+Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+A few examples:(Input1, Input2, Input3 --> Output)
+
+5, 2, "add"      --> 7
+5, 2, "subtract" --> 3
+5, 2, "multiply" --> 10
+5, 2, "divide"   --> 2.5
+Try to do it without using if statements!
+*/
+
+function arithmetic(a, b, operator) {
+	switch (operator) {
+		case "add":
+			return a + b;
+		case "subtract":
+			return a - b;
+		case "multiply":
+			return a * b;
+		case "divide":
+			return a / b;
+	}
+}
+
+console.log(arithmetic(1, 2, "add"), 3, "'add' should return the two numbers added together!");
+console.log(arithmetic(8, 2, "subtract"), 6, "'subtract' should return a minus b!");
+console.log(arithmetic(5, 2, "multiply"), 10, "'multiply' should return a multiplied by b!");
+console.log(arithmetic(8, 2, "divide"), 4, "'divide' should return a divided by b!");
