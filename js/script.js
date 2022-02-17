76f2c0dad44cd0fe6b05e61c0aa6f0325c1921f4
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-17 13:26:55 +0300

    Sort array by string length (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3422,3 +3422,27 @@ console.log(arithmetic(1, 2, "add"), 3, "'add' should return the two numbers add
 console.log(arithmetic(8, 2, "subtract"), 6, "'subtract' should return a minus b!");
 console.log(arithmetic(5, 2, "multiply"), 10, "'multiply' should return a multiplied by b!");
 console.log(arithmetic(8, 2, "divide"), 4, "'divide' should return a divided by b!");
+
+// Sort array by string length (7 kyu)
+
+/* 
+Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+*/
+
+function sortByLength(array) {
+	return array.sort((a, b) => a.length - b.length);
+}
+
+console.log(sortByLength(["Beg", "Life", "I", "To"]), ["I", "To", "Beg", "Life"]);
+console.log(sortByLength(["", "Moderately", "Brains", "Pizza"]), ["", "Pizza", "Brains", "Moderately"]);
+console.log(sortByLength(["Longer", "Longest", "Short"]), ["Short", "Longer", "Longest"]);
