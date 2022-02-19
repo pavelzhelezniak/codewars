2a7ead9cbd347903f89e39f18025102f74ac9894
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-19 18:33:15 +0300

    Duplicate Encoder (6 kyu)

--- js/script.js
+++ js/script.js
@@ -3488,3 +3488,32 @@ console.log(duplicateCount("aabbcde"), 2);
 console.log(duplicateCount("aabBcde"), 2, "should ignore case");
 console.log(duplicateCount("Indivisibility"), 1)
 console.log(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
+
+// Duplicate Encoder (6 kyu)
+
+/* 
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+Notes
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+*/
+
+function duplicateEncode(word) {
+	return word
+		.toLowerCase()
+		.split('')
+		.map(function (item, index, laters) {
+			return laters.indexOf(item) === laters.lastIndexOf(item) ? '(' : ')'
+		})
+		.join('');
+}
+
+console.log(duplicateEncode("din"), "(((");
+console.log(duplicateEncode("recede"), "()()()");
+console.log(duplicateEncode("Success"), ")())())", "should ignore case");
+console.log(duplicateEncode("(( @"), "))((");
