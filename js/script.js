2f505c908eb5e2176963857f0757904557625b44
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-20 20:56:23 +0300

    Round up to the next multiple of 5 (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3517,3 +3517,33 @@ console.log(duplicateEncode("din"), "(((");
 console.log(duplicateEncode("recede"), "()()()");
 console.log(duplicateEncode("Success"), ")())())", "should ignore case");
 console.log(duplicateEncode("(( @"), "))((");
+
+// Round up to the next multiple of 5 (7 kyu)
+
+/* 
+Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+*/
+
+function roundToNext5(n) {
+	return Math.ceil(n / 5) * 5;
+}
+
+console.log(roundToNext5(1));
+console.log(roundToNext5(7));
+console.log(roundToNext5(12));
