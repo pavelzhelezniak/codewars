63a05ee7e7c4fdbd8dbd163359a42df8501f507e
pavelzhelezniak <zhele3o1992@gmail.com>
2022-02-24 22:13:26 +0300

    Maximum Multiple (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3589,3 +3589,32 @@ const uniqueInOrder = function (iterable) {
 console.log(uniqueInOrder('AAAABBBCCDAABBB'), ['A', 'B', 'C', 'D', 'A', 'B']);
 console.log(uniqueInOrder('ABBCcAD'), ['A', 'B', 'C', 'c', 'A', 'D']);
 console.log(uniqueInOrder([1, 2, 2, 3, 3]), [1, 2, 3]);
+
+// Maximum Multiple (7 kyu)
+
+/* 
+Task
+Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+Conditions :
+N is divisible by divisor
+
+N is less than or equal to bound
+
+N is greater than 0.
+
+Notes
+The parameters (divisor, bound) passed to the function are only positive values .
+It's guaranteed that a divisor is Found .
+*/
+
+function maxMultiple(divisor, bound) {
+	return bound - (bound % divisor)
+}
+
+console.log(maxMultiple(2, 7), 6);
+console.log(maxMultiple(3, 10), 9);
+console.log(maxMultiple(7, 17), 14);
+console.log(maxMultiple(10, 50), 50);
+console.log(maxMultiple(37, 200), 185);
+console.log(maxMultiple(7, 100), 98);
