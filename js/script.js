2a3017b2ff80e5fe9177f346ec7637da092d871c
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-03 16:34:36 +0300

    Summing a number's digits (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3804,3 +3804,27 @@ console.log(reverseLetter("krishan"), "nahsirk")
 console.log(reverseLetter("ultr53o?n"), "nortlu")
 console.log(reverseLetter("ab23c"), "cba")
 console.log(reverseLetter("krish21an"), "nahsirk")
+
+// Summing a number's digits (7 kyu)
+
+/* 
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+*/
+
+function sumDigits(number) {
+	return Math.abs(number)
+		.toString()
+		.split('').
+		reduce((acc, item) => parseFloat(acc, 10) + parseFloat(item, 10), 0);
+}
+
+console.log(sumDigits(10), 1);
+console.log(sumDigits(99), 18);
+console.log(sumDigits(-32), 5);
