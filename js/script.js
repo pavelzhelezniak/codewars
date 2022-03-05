d449c029aee713ae0b766bf451f562af9e0cd03d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-05 10:57:12 +0300

    Check the exam (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3925,3 +3925,33 @@ const deleteNth = (arr, n) => {
 
 console.log(deleteNth([20, 37, 20, 21], 1), [20, 37, 21])
 console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
+
+// Check the exam (7 kyu)
+
+/* 
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+*/
+
+const checkExam = (array1, array2) => {
+	let count = 0;
+	array1.forEach((item, index) => {
+		array2[index] === item ? count += 4 : array2[index] === '' ? count += 0 : count -= 1;
+	});
+	return count > 0 ? count : 0;
+}
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]), 6);
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]), 7);
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]), 16);
+console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]), 0); 
