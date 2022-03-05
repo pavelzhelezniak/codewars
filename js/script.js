7e55201da62c3b1efaaa11229067d1c340d4b34f
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-05 10:27:48 +0300

    Delete occurrences of an element if it occurs more than n times(6 kyu)

--- js/script.js
+++ js/script.js
@@ -3880,3 +3880,48 @@ const repeater = (string, n) => {
 console.log(repeater('a', 5), 'aaaaa')
 console.log(repeater('Na', 16), 'NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa')
 console.log(repeater('Wub ', 6), 'Wub Wub Wub Wub Wub Wub ')
+
+// Delete occurrences of an element if it occurs more than n times(6 kyu)
+
+/* 
+Enough is enough!
+Alice and Bob were on a holiday.
+Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. 
+However, Charlie doesn't like these sessions, since the motive usually repeats. 
+He isn't fond of seeing the Eiffel tower 40 times. 
+He tells them that he will only sit during the session if they show the same motive at most N times. 
+Luckily, Alice and Bob are able to encode the motive as a number. 
+Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+Task
+Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. 
+For example if N = 2, and the input is [1,2,3,1,2,1,2,3], 
+you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, 
+and then take 3, which leads to [1,2,3,1,2,3].
+
+Example
+  deleteNth ([1,1,1,1],2) // return [1,1]
+  
+  deleteNth ([20,37,20,21],1) // return [20,37,21]
+*/
+
+const deleteNth = (arr, n) => {
+	const objNumbers = {};
+	const newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i];
+		if (objNumbers.hasOwnProperty(element)) {
+			objNumbers[element]++;
+			if (objNumbers[element] <= n) {
+				newArr.push(element);
+			}
+		} else {
+			objNumbers[element] = 1;
+			newArr.push(element);
+		}
+	}
+	return newArr;
+}
+
+console.log(deleteNth([20, 37, 20, 21], 1), [20, 37, 21])
+console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
