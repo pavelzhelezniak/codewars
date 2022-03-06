b2124f48563d3d45f86365fc1b6c81dc95584772
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-06 14:13:49 +0300

    Small enough? - Beginner (7 kyu)

--- js/script.js
+++ js/script.js
@@ -3954,4 +3954,23 @@ const checkExam = (array1, array2) => {
 console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]), 6);
 console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]), 7);
 console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]), 16);
-console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]), 0); 
+console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]), 0);
+
+// Small enough? - Beginner (7 kyu)
+
+/* 
+You will be given an array and a limit value. You must check that 
+all values in the array are below or equal to the limit value. 
+If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+*/
+
+const smallEnough = (a, limit) => {
+	return a.every(item => item <= limit);
+}
+
+console.log(smallEnough([66, 101], 200), true);
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100), false);
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107), true);
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120), true);
