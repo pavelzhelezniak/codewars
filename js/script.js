1046e0d0a016c79265ab2013cec4900d3d3c16fb
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-09 14:52:46 +0300

    Maximum Length Difference (7 kyu)

--- js/script.js
+++ js/script.js
@@ -4035,3 +4035,35 @@ console.log(capitalize("abcdef"), ['AbCdEf', 'aBcDeF']);
 console.log(capitalize("codewars"), ['CoDeWaRs', 'cOdEwArS']);
 console.log(capitalize("abracadabra"), ['AbRaCaDaBrA', 'aBrAcAdAbRa']);
 console.log(capitalize("codewarriors"), ['CoDeWaRrIoRs', 'cOdEwArRiOrS']);
+
+// Maximum Length Difference (7 kyu)
+
+/* 
+You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. 
+Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+Example:
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+Bash note:
+input : 2 strings with substrings separated by ,
+output: number as a string
+*/
+
+const mxdiflg = (a1, a2) => {
+	if (a1.length === 0 || a2.length === 0) {
+		return -1;
+	}
+	const wordLength1 = a1.map(word => word.length);
+	const wordLength2 = a2.map(word => word.length);
+	return Math.max(Math.max(...wordLength1) - Math.min(...wordLength2), Math.max(...wordLength2) - Math.min(...wordLength1));
+}
+
+const s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"];
+const s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"];
+console.log(mxdiflg(s1, s2), 13);
