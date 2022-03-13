43cfa7c7a579c403f42812ab3e0040815b0a53b5
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-13 15:02:18 +0300

    Write Number in Expanded Form (6 kyu)

--- js/script.js
+++ js/script.js
@@ -4178,3 +4178,29 @@ console.log(validBraces(")(}{][", false));
 console.log(validBraces("())({}}{()][][", false));
 console.log(validBraces("(((({{", false));
 console.log(validBraces("}}]]))}])", false));
+
+// Write Number in Expanded Form (6 kyu)
+
+/* 
+Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
+*/
+
+const expandedForm = (num) => {
+	return num.toString()
+		.split('')
+		.reverse()
+		.map((item, index) => item * Math.pow(10, index))
+		.filter(item => item > 0)
+		.reverse()
+		.join(' + ');
+}
+
+console.log(expandedForm(12), '10 + 2');
+console.log(expandedForm(42), '40 + 2');
+console.log(expandedForm(70304), '70000 + 300 + 4');
