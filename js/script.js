409fbc8fdac8dddf0662dbc59babc5a6ff3f8488
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-14 15:38:25 +0300

    Find The Parity Outlier (6 kyu)

--- js/script.js
+++ js/script.js
@@ -4204,3 +4204,36 @@ const expandedForm = (num) => {
 console.log(expandedForm(12), '10 + 2');
 console.log(expandedForm(42), '40 + 2');
 console.log(expandedForm(70304), '70000 + 300 + 4');
+
+// Find The Parity Outlier (6 kyu)
+
+/* 
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+*/
+
+const findOutlier = (integers) => {
+	const odd = [];
+	const even = [];
+	integers.forEach(item => {
+		if (item % 2 === 0) {
+			even.push(item);
+		} else {
+			odd.push(item);
+		}
+	});
+
+	return odd.length === 1 ? odd[0] : even[0];
+}
+
+console.log(findOutlier([0, 1, 2]), 1);
+console.log(findOutlier([1, 2, 3]), 2);
+console.log(findOutlier([2, 6, 8, 10, 3]), 3);
+console.log(findOutlier([0, 0, 3, 0, 0]), 3);
+console.log(findOutlier([1, 1, 0, 1, 1]), 0);
