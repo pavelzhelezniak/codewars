3a9ef1e6c72f9d52b18493b6de341b7c1c798511
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-15 13:32:31 +0300

    Equal Sides Of An Array (6 kyu)

--- js/script.js
+++ js/script.js
@@ -4288,3 +4288,59 @@ console.log(tribonacci([3, 2, 1], 10), [3, 2, 1, 6, 9, 16, 31, 56, 103, 190]);
 console.log(tribonacci([1, 1, 1], 1), [1]);
 console.log(tribonacci([300, 200, 100], 0), []);
 console.log(tribonacci([0.5, 0.5, 0.5], 30), [0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5, 1104.5, 2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5, 266079.5, 489396.5, 900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5]);
+
+// Equal Sides Of An Array (6 kyu)
+
+/* 
+You are going to be given an array of integers. 
+Your job is to take that array and find an index N where the sum of the integers 
+to the left of N is equal to the sum of the integers to the right of N. 
+If there is no index that would make this happen, return -1.
+
+For example:
+
+Let's say you are given the array {1,2,3,4,3,2,1}:
+Your function will return the index 3, because at the 3rd position of the array, 
+the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+Let's look at another one.
+You are given the array {1,100,50,-51,1,1}:
+Your function will return the index 1, because at the 1st position of the array, 
+the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+Last one:
+You are given the array {20,10,-80,10,10,15,35}
+At index 0 the left side is {}
+The right side is {10,-80,10,10,15,35}
+They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+Index 0 is the place where the left side and right side are equal.
+
+Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+
+Input:
+An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+
+Output:
+The lowest index N where the side to the left of N is equal to the side to the right of N. 
+If you do not find an index that fits these rules, then you will return -1.
+
+Note:
+If you are given an array with multiple answers, return the lowest correct index.
+*/
+
+const findEvenIndex = (arr) => {
+	const arrayIndex = [];
+	for (let i = 0; i < arr.length; i++) {
+		let sumLeftSide = arr.slice(0, i).reduce((acc, item) => acc + item, 0);
+		let sumRightSide = arr.slice(i + 1, arr.length).reduce((acc, item) => acc + item, 0);
+		if (sumLeftSide === sumRightSide) {
+			arrayIndex.push(i);
+		}
+	}
+	return arrayIndex.length === 0 ? -1 : arrayIndex[0];
+}
+
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]), 3, "The array was: [1,2,3,4,3,2,1] \n");
+console.log(findEvenIndex([1, 100, 50, -51, 1, 1]), 1, "The array was: [1,100,50,-51,1,1] \n");
+console.log(findEvenIndex([1, 2, 3, 4, 5, 6]), -1, "The array was: [1,2,3,4,5,6] \n");
+console.log(findEvenIndex([20, 10, 30, 10, 10, 15, 35]), 3, "The array was: [20,10,30,10,10,15,35] \n");
