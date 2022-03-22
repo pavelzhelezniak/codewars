50b712f325f9966feaceed3678ec09f4c48d4116
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-22 16:10:58 +0300

    Two Sum (6 kyu)

--- js/script.js
+++ js/script.js
@@ -4577,3 +4577,36 @@ const solutionss = (string) => {
 
 console.log(solutionss('camelCasing'), 'camel Casing', 'Unexpected result');
 console.log(solutionss('camelCasingTest'), 'camel Casing Test', 'Unexpected result');
+
+// Two Sum (6 kyu)
+
+/* 
+Write a function that takes an array of numbers (integers for the tests) and a target number. 
+It should find two different items in the array that, when added together, give the target value. 
+The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; 
+any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, 
+	and all of the items will be numbers; 
+	target will always be the sum of two different items from that array).
+
+Based on: http://oj.leetcode.com/problems/two-sum/
+
+twoSum [1, 2, 3] 4 === (0, 2)
+*/
+
+const twoSum = (numbers, total) => {
+	const previousValues = {};
+	for (let i = 0; i < numbers.length; i++) {
+		const complement = total - numbers[i];
+		if (previousValues[complement]) {
+			return [numbers.indexOf(complement), i];
+		}
+		previousValues[numbers[i]] = true;
+	}
+};
+
+console.log(twoSum([1, 2, 3], 4), [0, 2]);
+console.log(twoSum([3, 9, 12, 20], 21), [1, 2]); 
