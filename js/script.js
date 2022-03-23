fd1e79956d67d30432718e1ad7d65a9a5bd88517
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-23 16:18:52 +0300

    Scramblies (5 kyu)

--- js/script.js
+++ js/script.js
@@ -4609,4 +4609,54 @@ const twoSum = (numbers, total) => {
 };
 
 console.log(twoSum([1, 2, 3], 4), [0, 2]);
-console.log(twoSum([3, 9, 12, 20], 21), [1, 2]); 
+console.log(twoSum([3, 9, 12, 20], 21), [1, 2]);
+
+// Scramblies (5 kyu)
+
+/* 
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, 
+otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered.
+Examples
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+*/
+
+const scramble = (str1, str2) => {
+	const letterObj = {};
+	for (let letter of str1) {
+		if (letterObj[letter]) {
+			letterObj[letter]++;
+		}
+		else {
+			letterObj[letter] = 1;
+		}
+	}
+
+	for (let letter of str2) {
+		if (!letterObj[letter]) {
+			return false;
+		}
+		else {
+			letterObj[letter]--;
+		}
+	}
+	return true;
+}
+
+console.log(scramble('rkqodlw', 'world'), true);
+console.log(scramble('cedewaraaossoqqyt', 'codewars'), true);
+console.log(scramble('katas', 'steak'), false);
+console.log(scramble('scriptjavx', 'javascript'), false);
+console.log(scramble('scriptingjava', 'javascript'), true);
+console.log(scramble('scriptsjava', 'javascripts'), true);
+console.log(scramble('javscripts', 'javascript'), false);
+console.log(scramble('jscripts', 'javascript'), false);
+console.log(scramble('aabbcamaomsccdd', 'commas'), true);
+console.log(scramble('commas', 'commas'), true);
+console.log(scramble('sammoc', 'commas'), true)
