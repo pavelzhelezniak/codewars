d0a4194bff35f69672e4bd218a512cbf9fabc92c
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-28 12:03:17 +0300

    Title Case (6 kyu)

--- js/script.js
+++ js/script.js
@@ -4879,3 +4879,51 @@ console.log(rowWeights([0]), [0, 0]);
 console.log(rowWeights([100, 51, 50, 100]), [150, 151]);
 console.log(rowWeights([39, 84, 74, 18, 59, 72, 35, 61]), [207, 235]);
 console.log(rowWeights([0, 1, 0]), [0, 1]);
+
+// Title Case (6 kyu)
+
+/* 
+A string is considered to be in title case if each word in the string is either (a) capitalised (that is, 
+only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely
+into lower case unless it is the first word, which is always capitalised.
+
+Write a function that will convert a string into title case, given an optional list of exceptions (minor words). 
+The list of minor words will be given as a string with each word separated by a space. 
+Your function should ignore the case of the minor words string -- it should behave in the same way even 
+if the case of the minor word string is changed.
+
+###Arguments (Haskell)
+
+First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
+Second argument: the original string to be converted.
+###Arguments (Other languages)
+
+First argument (required): the original string to be converted.
+Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. 
+The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+###Example
+
+titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+*/
+
+const titleCase = (title, minorWords) => {
+	const arrMinorWords = typeof minorWords !== "undefined" ? minorWords.toLowerCase().split(' ') : [];
+	return title.toLowerCase()
+		.split(' ')
+		.map(function (item, index) {
+			if (item != "" && ((arrMinorWords.indexOf(item) === -1) || index == 0)) {
+				item = item.split('');
+				item[0] = item[0].toUpperCase();
+				item = item.join('');
+			}
+			return item;
+		})
+		.join(' ');
+}
+
+console.log(titleCase(''), '')
+console.log(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings')
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In'), 'The Wind in the Willows')
+console.log(titleCase('the quick brown fox'), 'The Quick Brown Fox')
