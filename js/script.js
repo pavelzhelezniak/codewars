2548682d39b3d73bee26e8a4ee1a7dd8f828410f
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-29 13:43:34 +0300

    Reverse or rotate? (6 kyu)

--- js/script.js
+++ js/script.js
@@ -4927,3 +4927,53 @@ console.log(titleCase(''), '')
 console.log(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings')
 console.log(titleCase('THE WIND IN THE WILLOWS', 'The In'), 'The Wind in the Willows')
 console.log(titleCase('the quick brown fox'), 'The Quick Brown Fox')
+
+// Reverse or rotate? (6 kyu)
+
+/* 
+The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+
+If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+If
+
+sz is <= 0 or if str is empty return ""
+sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+Examples:
+revrot("123456987654", 6) --> "234561876549"
+revrot("123456987653", 6) --> "234561356789"
+revrot("66443875", 4) --> "44668753"
+revrot("66443875", 8) --> "64438756"
+revrot("664438769", 8) --> "67834466"
+revrot("123456779", 8) --> "23456771"
+revrot("", 8) --> ""
+revrot("123456779", 0) --> "" 
+revrot("563000655734469485", 4) --> "0365065073456944"
+Example of a string rotated to the left by one position:
+s = "123456" gives "234561".
+*/
+
+const revrot = (str, sz) => {
+	const arr = [];
+	if (str.length === 0 || sz <= 0) {
+		return '';
+	}
+	for (let i = 0; i < str.length; i += sz) {
+		if (!(str.slice(i, i + sz).length < sz)) {
+			const substr = str.slice(i, i + sz).split('');
+			const sumSubstr = substr.reduce((acc, item) => acc + Math.pow(item, 3), 0);
+			sumSubstr % 2 === 0 ? arr.push(substr.reverse().join('')) : arr.push(substr.slice(1).join(''), substr[0]);
+		}
+	}
+	return arr.join('');
+}
+
+console.log(revrot("123456987654", 6), "234561876549");
+console.log(revrot("123456987653", 6), "234561356789");
+console.log(revrot("66443875", 4), "44668753");
+console.log(revrot("66443875", 8), "64438756");
+console.log(revrot("664438769", 8), "67834466");
+console.log(revrot("123456779", 8), "23456771");
+console.log(revrot("", 8), "");
+console.log(revrot("123456779", 0), "");
+console.log(revrot("563000655734469485", 4), "0365065073456944");
