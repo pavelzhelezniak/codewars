a163ab0827af18831f4af45d8b1114532e21a038
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-30 18:26:14 +0300

    Help the bookseller ! (6 kyu)

--- js/script.js
+++ js/script.js
@@ -4977,3 +4977,66 @@ console.log(revrot("123456779", 8), "23456771");
 console.log(revrot("", 8), "");
 console.log(revrot("123456779", 0), "");
 console.log(revrot("563000655734469485", 4), "0365065073456944");
+
+// Help the bookseller ! (6 kyu)
+
+/* 
+A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. 
+Each book has a code c of 3, 4, 5 or more characters. 
+The 1st character of a code is a capital letter which defines the book category.
+
+In the bookseller's stocklist each code c is followed by a space and by a positive integer 
+n (int n >= 0) which indicates the quantity of books of this code in stock.
+
+For example an extract of a stocklist could be:
+
+L = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}.
+or
+L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] or ....
+You will be given a stocklist (e.g. : L) and a list of categories in capital letters e.g :
+
+M = {"A", "B", "C", "W"} 
+or
+M = ["A", "B", "C", "W"] or ...
+and your task is to find all the books of L with codes belonging to 
+each category of M and to sum their quantity according to each category.
+
+For the lists L and M of example you have to return the string (in Haskell/Clojure/Racket a list of pairs):
+
+(A : 20) - (B : 114) - (C : 50) - (W : 0)
+where A, B, C, W are the categories, 20 is the sum of the unique book of category A, 
+114 the sum corresponding to "BKWRK" and "BTSQZ", 
+50 corresponding to "CDXEF" and 0 to category 'W' since there are no code beginning with W.
+
+If L or M are empty return string is "" (Clojure and Racket should return an empty array/list instead).
+
+Note:
+In the result codes and their values are in the same order as in M.
+*/
+
+const stockList = (listOfArt, listOfCat) => {
+	if (listOfArt.length === 0 || listOfCat.length === 0) {
+		return ''
+	}
+	let resultStr = '';
+	for (let i = 0; i < listOfCat.length; i++) {
+		const elem1 = listOfCat[i];
+		let sum = 0;
+		for (let k = 0; k < listOfArt.length; k++) {
+			const elem2 = listOfArt[k];
+			if (elem1 === elem2[0]) {
+				sum += parseInt(elem2.match(/\d/g).join(''), 10)
+			}
+		}
+		elem1 === listOfCat[listOfCat.length - 1] ? resultStr += `(${elem1} : ${sum})` : resultStr += `(${elem1} : ${sum}) - `;
+	}
+	return resultStr;
+}
+
+b1 = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"];
+c1 = ["A", "B"];
+console.log(stockList(b1, c1), "(A : 200) - (B : 1140)");
+
+b2 = ["CBART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"]
+c2 = ["A", "B", "C", "W"]
+console.log(stockList(b2, c2), "(A : 0) - (B : 114) - (C : 70) - (W : 0)")
