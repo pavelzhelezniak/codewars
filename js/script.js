51f5c564cd990947b3aaf7b7a6b1fdb85a6f332f
pavelzhelezniak <zhele3o1992@gmail.com>
2022-03-31 19:47:11 +0300

    Good vs Evil (6 kyu)

--- js/script.js
+++ js/script.js
@@ -5040,3 +5040,96 @@ console.log(stockList(b1, c1), "(A : 200) - (B : 1140)");
 b2 = ["CBART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"]
 c2 = ["A", "B", "C", "W"]
 console.log(stockList(b2, c2), "(A : 0) - (B : 114) - (C : 70) - (W : 0)")
+
+// Good vs Evil (6 kyu)
+
+/* 
+Description
+Middle Earth is about to go to war. The forces of good will have many battles with the forces of evil. Different races will certainly be involved. Each race has a certain worth when battling against others. On the side of good we have the following races, with their associated worth:
+
+Hobbits: 1
+Men: 2
+Elves: 3
+Dwarves: 3
+Eagles: 4
+Wizards: 10
+On the side of evil we have:
+
+Orcs: 1
+Men: 2
+Wargs: 2
+Goblins: 2
+Uruk Hai: 3
+Trolls: 5
+Wizards: 10
+Although weather, location, supplies and valor play a part in any battle, 
+if you add up the worth of the side of good and compare it with the worth of the side of evil, 
+the side with the larger worth will tend to win.
+
+Thus, given the count of each of the races on the side of good, 
+followed by the count of each of the races on the side of evil, determine which side wins.
+
+Input:
+The function will be given two parameters. 
+Each parameter will be a string of multiple integers separated by a single space. 
+Each string will contain the count of each race on the side of good and evil.
+
+The first parameter will contain the count of each race on the side of good in the following order:
+
+Hobbits, Men, Elves, Dwarves, Eagles, Wizards.
+The second parameter will contain the count of each race on the side of evil in the following order:
+
+Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards.
+All values are non-negative integers. The resulting sum of the worth for each side will not exceed the limit of a 32-bit integer.
+
+Output:
+Return "Battle Result: Good triumphs over Evil" if good wins, 
+"Battle Result: Evil eradicates all trace of Good" if evil wins, 
+or "Battle Result: No victor on this battle field" if it ends in a tie.
+*/
+
+const goodWorth = [
+	1, // Hobbits
+	2, // Men
+	3, // Elves
+	3, // Dwarves
+	4, // Eagles
+	10, // Wizards
+]
+
+const evilWorth = [
+	1, // Orcs
+	2, // Men
+	2, // Wargs
+	2, // Goblins
+	3, // Uruk Hai
+	5, // Trolls
+	10, // Wizards
+]
+
+const calculateWorth = (points, force) =>
+	points
+		.split(' ')
+		.reduce(
+			(total, value, index) =>
+				force === 'good'
+					? Number(total) + goodWorth[index] * Number(value)
+					: Number(total) + evilWorth[index] * Number(value),
+			0
+		)
+
+const goodVsEvil = (good, evil) => {
+	const goodTotalWorth = calculateWorth(good, 'good')
+	const evilTotalWorth = calculateWorth(evil, 'evil')
+
+	if (evilTotalWorth > goodTotalWorth)
+		return 'Battle Result: Evil eradicates all trace of Good'
+	else if (goodTotalWorth > evilTotalWorth)
+		return 'Battle Result: Good triumphs over Evil'
+	else return 'Battle Result: No victor on this battle field'
+}
+
+console.log(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'), 'Battle Result: Evil eradicates all trace of Good');
+console.log(goodVsEvil('0 0 0 0 0 10', '0 1 1 1 1 0 0'), 'Battle Result: Good triumphs over Evil');
+console.log(goodVsEvil('1 0 0 0 0 0', '1 0 0 0 0 0 0'), 'Battle Result: No victor on this battle field');
+
