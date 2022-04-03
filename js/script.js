50dbb685fcd4eeca2140aed2169a251a40dfbf5d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-03 09:23:39 +0300

    Data Reverse (6 kyu)

--- js/script.js
+++ js/script.js
@@ -5225,3 +5225,38 @@ console.log(partsSums([0, 1, 3, 6, 10], [20, 20, 19, 16, 10, 0]));
 console.log(partsSums([1, 2, 3, 4, 5, 6], [21, 20, 18, 15, 11, 6, 0]));
 console.log(partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358],
 	[10037855, 9293730, 9292795, 9292388, 9291934, 9291504, 9291414, 9291270, 2581057, 2580168, 2579358, 0]));
+
+// Data Reverse (6 kyu)
+
+/* 
+A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+*/
+
+const dataReverse = (data) => {
+	const newData = [];
+	for (let i = 0; i < data.length; i += 8) {
+		newData.unshift(...data.slice(i, i + 8));
+	}
+	return newData;
+}
+
+const data1 = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0];
+const data2 = [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1];
+console.log(dataReverse(data1), data2);
+const data3 = [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1];
+const data4 = [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0];
+console.log(dataReverse(data3), data4);
