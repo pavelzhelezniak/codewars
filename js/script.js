32e0cbd0a27ab8ef3bd4912005ae27e769d3617d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-06 13:53:11 +0300

    Valid Phone Number (6 kyu)

--- js/script.js
+++ js/script.js
@@ -5329,3 +5329,36 @@ const solution1 = (input, markers) => {
 
 console.log(solution1("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]), "apples, plums\npears\noranges");
 console.log(solution1("Q @b\nu\ne -e f g", ["@", "-"]), "Q\nu\ne");
+
+// Valid Phone Number (6 kyu)
+
+/* 
+Write a function that accepts a string, and returns true if it is in the form of a phone number.
+Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+
+Only worry about the following format:
+(123) 456-7890 (don't forget the space after the close parentheses)
+
+Examples:
+
+"(123) 456-7890"  => true
+"(1111)555 2345"  => false
+"(098) 123 4567"  => false
+*/
+
+const validPhoneNumber = (phoneNumber) => {
+	// return phoneNumber[0] === "(" &&
+	// 	phoneNumber[4] === ")" &&
+	// 	phoneNumber[5] === " " &&
+	// 	phoneNumber[9] === "-" &&
+	// 	phoneNumber.substring(1, 4).length === 3 &&
+	// 	phoneNumber.substring(6, 9).length === 3 &&
+	// 	phoneNumber.substring(10).length === 4;
+
+	const regExp = /^\(\d{3}\) \d{3}-\d{4}$/gm;
+	return regExp.test(phoneNumber);
+}
+
+console.log(validPhoneNumber("(123) 456-7890"), true);
+console.log(validPhoneNumber("(1111)555 2345"), false);
+console.log(validPhoneNumber("(098) 123 4567"), false);
