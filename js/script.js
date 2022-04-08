589391b1f210258c7af7e14000e6748e2c09f4e9
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-08 19:32:43 +0300

    Meeting (6 kyu)

--- js/script.js
+++ js/script.js
@@ -5429,3 +5429,49 @@ console.log(nbDig(5750, 0), 4700, "n = 5750, d = 0");
 console.log(nbDig(11011, 2), 9481, "n = 11011, d = 2");
 console.log(nbDig(12224, 8), 7733, "n = 12224, d = 8");
 console.log(nbDig(11549, 1), 11905, "n = 11549, d = 1");
+
+// Meeting (6 kyu)
+
+/* 
+John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. 
+Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.
+
+Notes
+You can see another examples in the "Sample tests".
+*/
+
+const meeting = (s) => {
+	let newString = '';
+
+	const newS = s.split(';')
+		.map(item => item.toUpperCase()
+			.split(':')
+			.reverse()
+			.join(', '))
+		.sort()
+		.forEach(item => {
+			const newStrSplit = item.split(', ');
+			newString += `(${newStrSplit[0]}, ${newStrSplit[1]})`
+		});
+
+	return newString;
+}
+
+console.log(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn"),
+	"(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)");
+console.log(meeting("John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell"),
+	"(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)");
+console.log(meeting("Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern"),
+	"(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)");
