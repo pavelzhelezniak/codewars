4d5147c905bf8948bd5bae8368e41b001844e8ee
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-09 17:25:02 +0300

    Fold an array (6 kyu)

--- js/script.js
+++ js/script.js
@@ -5475,3 +5475,51 @@ console.log(meeting("John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny
 	"(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)");
 console.log(meeting("Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern"),
 	"(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)");
+
+// Fold an array (6 kyu)
+
+/* 
+In this kata you have to write a method that folds a given array of integers by the middle x-times.
+
+An example says more than thousand words:
+
+Fold 1-times:
+[1,2,3,4,5] -> [6,6,3]
+
+A little visualization (NOT for the algorithm but for the idea of folding):
+
+ Step 1         Step 2        Step 3       Step 4       Step5
+							5/           5|         5\          
+						  4/            4|          4\      
+1 2 3 4 5      1 2 3/         1 2 3|       1 2 3\       6 6 3
+----*----      ----*          ----*        ----*        ----*
+
+
+Fold 2-times:
+[1,2,3,4,5] -> [9,6]
+As you see, if the count of numbers is odd, the middle number will stay. Otherwise the fold-point is between the middle-numbers, so all numbers would be added in a way.
+
+The array will always contain numbers and will never be null. The parameter runs will always be a positive integer greater than 0 and says how many runs of folding your method has to do.
+
+If an array with one element is folded, it stays as the same array.
+
+The input array should not be modified!
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have created other katas. Have a look if you like coding and challenges.
+*/
+
+const foldArray = (array, runs) => {
+	const newArr = [...array];
+	const resultArray = [];
+
+	while (newArr.length) {
+		resultArray.push(newArr.pop() + (newArr.shift() || 0));
+	}
+
+	return runs - 1 ? foldArray(resultArray, runs - 1) : resultArray;
+}
+
+console.log(foldArray([1, 2, 3, 4, 5], 3), [15]);
+console.log(foldArray([-9, 9, -8, 8, 66, 23], 1), [14, 75, 0]);
