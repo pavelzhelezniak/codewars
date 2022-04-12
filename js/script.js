b853c006e5914c122730b0765a7f016d9731409d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-12 16:10:21 +0300

    +1 Array (6 kyu)

--- js/script.js
+++ js/script.js
@@ -5554,3 +5554,45 @@ const angle = (n) => {
 
 console.log(angle(3), 180);
 console.log(angle(4), 360);
+
+// +1 Array (6 kyu)
+
+/* 
+Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+the array can't be empty
+only non-negative, single digit integers are allowed
+Return nil (or your language's equivalent) for invalid inputs.
+
+Examples
+For example the array [2, 3, 9] equals 239, adding one would return the array [2, 4, 0].
+
+[4, 3, 2, 5] would return [4, 3, 2, 6]
+*/
+
+const upArray = (arr) => {
+	if (!arr.length) return null;
+
+	const newArr = [...arr];
+	let carry = 1;
+
+	for (let i = newArr.length - 1; i > -1; --i) {
+		if (newArr[i] < 0 || newArr[i] > 9) return null;
+
+		if (newArr[i] + carry === 10) {
+			newArr[i] = 0;
+			continue;
+		}
+		if (carry === 1) {
+			newArr[i] += carry;
+			carry--;
+		}
+	}
+
+	if (carry) newArr.unshift(carry)
+	return newArr;
+}
+
+console.log(upArray([2, 3, 9]), [2, 4, 0]);
+console.log(upArray([4, 3, 2, 5]), [4, 3, 2, 6]);
+console.log(upArray([1, -9]), null);
