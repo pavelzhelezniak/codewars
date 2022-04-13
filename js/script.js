8bcd468bfa55ab74764b8db27c7bed10be1cfdc5
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-13 16:43:44 +0300

    Array Helpers (6 kyu)

--- js/script.js
+++ js/script.js
@@ -5639,3 +5639,108 @@ console.log(highestRank(arr123), 12);
 console.log(highestRank(arr234), 12);
 console.log(highestRank(arr345), 3);
 console.log(highestRank(arr456), 3);
+
+// Array Helpers (6 kyu)
+
+/* 
+This kata is designed to test your ability to extend the functionality of built-in classes. 
+In this case, we want you to extend the built-in Array class with the following methods: 
+square(), cube(), average(), sum(), even() and odd().
+
+Explanation:
+
+square() must return a copy of the array, containing all values squared
+cube() must return a copy of the array, containing all values cubed
+average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+sum() must return the sum of all array values
+even() must return an array of all even numbers
+odd() must return an array of all odd numbers
+Note: the original array must not be changed in any case!
+
+Example
+var numbers = [1, 2, 3, 4, 5];
+
+numbers.square();  // must return [1, 4, 9, 16, 25]
+numbers.cube();    // must return [1, 8, 27, 64, 125]
+numbers.average(); // must return 3
+numbers.sum();     // must return 15
+numbers.even();    // must return [2, 4]
+numbers.odd();     // must return [1, 3, 5]
+*/
+
+Array.prototype.square = function () {
+	const newArr = [];
+	for (let i = 0; i < this.length; i++) {
+		const elem = this[i];
+		newArr.push(elem ** 2)
+	}
+
+	return newArr;
+}
+
+Array.prototype.cube = function () {
+	const newArr = [];
+	for (let i = 0; i < this.length; i++) {
+		const elem = this[i];
+		newArr.push(elem ** 3)
+	}
+
+	return newArr;
+}
+
+Array.prototype.sum = function () {
+	let resultSum = 0;
+	for (let i = 0; i < this.length; i++) {
+		const elem = this[i];
+		resultSum += elem;
+	}
+
+	return resultSum;
+}
+
+Array.prototype.average = function () {
+	let resultSum = 0;
+	if (this.length === 0) {
+		return NaN;
+	}
+	for (let i = 0; i < this.length; i++) {
+		const elem = this[i];
+		resultSum += elem;
+	}
+
+	return resultSum / this.length;
+}
+
+Array.prototype.even = function () {
+	const newArr = [];
+
+	for (let i = 0; i < this.length; i++) {
+		const elem = this[i];
+		if (elem % 2 === 0) {
+			newArr.push(elem);
+		}
+	}
+
+	return newArr;
+}
+
+Array.prototype.odd = function () {
+	const newArr = [];
+
+	for (let i = 0; i < this.length; i++) {
+		const elem = this[i];
+		if (elem % 2 !== 0) {
+			newArr.push(elem);
+		}
+	}
+
+	return newArr;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers.square(), [1, 4, 9, 16, 25]);
+console.log(numbers.cube(), [1, 8, 27, 64, 125]);
+console.log(numbers.sum(), 15, 'Wrong sum');
+console.log(numbers.average(), 3, 'Wrong average');
+console.log(numbers.even(), [2, 4], 'Wrong result for even()');
+console.log(numbers.odd(), [1, 3, 5], 'Wrong result for odd()');
