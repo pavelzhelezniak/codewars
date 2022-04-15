85022a8e2ddabf8e620b7de0a42a12bfa05c8316
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-15 10:08:24 +0300

    Length of missing array  (6 kyu)

--- js/script.js
+++ js/script.js
@@ -5762,3 +5762,49 @@ const reverse = (str) => {
 
 console.log(reverse("Reverse this string, please!"), "Reverse siht string, !esaelp");
 console.log(reverse("I really don't like reversing strings!"), "I yllaer don't ekil reversing !sgnirts");
+
+// Length of missing array  (6 kyu)
+
+/* 
+You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+
+
+You have to write a method, that return the length of the missing array.
+
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+
+If the array of arrays is null/nil or empty, the method should return 0.
+
+When an array in the array is null or empty, the method should return 0 too!
+There will always be a missing element and its length will be always between the given arrays.
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have created other katas. Have a look if you like coding and challenges.
+*/
+
+const getLengthOfMissingArray = (arrayOfArrays) => {
+
+	const lengthArrays = (arrayOfArrays || []).map(item => item ? item.length : 0).sort((a, b) => a - b)
+	if (lengthArrays.includes(0)) {
+		return 0;
+	}
+
+	for (let i = 0; i < lengthArrays.length; i++) {
+		const elem = lengthArrays[i];
+		if (elem + 1 !== lengthArrays[i + 1]) {
+			return elem + 1;
+		}
+	}
+
+	return 0;
+}
+
+console.log(getLengthOfMissingArray([[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]]), 3);
+console.log(getLengthOfMissingArray([[5, 2, 9], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]]), 2);
+console.log(getLengthOfMissingArray([[null], [null, null, null]]), 2);
+console.log(getLengthOfMissingArray([['a', 'a', 'a'], ['a', 'a'], ['a', 'a', 'a', 'a'], ['a'], ['a', 'a', 'a', 'a', 'a', 'a']]), 5);
+console.log(getLengthOfMissingArray([]), 0);
