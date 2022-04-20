19a3459f67c3023d40eee483af9d396d01e588dd
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-20 20:57:33 +0300

    Predict your age! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -5882,3 +5882,28 @@ const repeater1 = (string, n) => string.repeat(n);
 console.log(repeater1('a', 5), 'aaaaa');
 console.log(repeater1('Na', 16), 'NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa');
 console.log(repeater1('Wub ', 6), 'Wub Wub Wub Wub Wub Wub ');
+
+// Predict your age! (7 kyu)
+
+/* 
+My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+In honor of my grandfather's memory we will write a function using his formula!
+
+Take a list of ages when each of your great-grandparent died.
+Multiply each number by itself.
+Add them all together.
+Take the square root of the result.
+Divide by two.
+Example
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+Note: the result should be rounded down to the nearest integer.
+
+Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.
+*/
+
+const predictAge = (age1, age2, age3, age4, age5, age6, age7, age8) => {
+	return Math.floor(Math.sqrt(age1 ** 2 + age2 ** 2 + age3 ** 2 + age4 ** 2 + age5 ** 2 + age6 ** 2 + age7 ** 2 + age8 ** 2) / 2)
+}
+
+console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45), 86);   
