63b83865ef0b92cbc9a8a841252bb29c3f2dff0e
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-23 23:42:52 +0300

    Most digits (7 kyu)

--- js/script.js
+++ js/script.js
@@ -5993,3 +5993,16 @@ console.log(getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshake
 	"Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke");
 console.log(getOrder("pizzachickenfriesburgercokemilkshakefriessandwich"),
 	"Burger Fries Fries Chicken Pizza Sandwich Milkshake Coke");
+
+// Most digits (7 kyu)
+
+/* 
+Find the number with the most digits.
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+*/
+
+const findLongest = (array) => array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
+
+console.log(findLongest([1, 10, 100]), 100)
+console.log(findLongest([9000, 8, 800]), 9000)
+console.log(findLongest([8, 900, 500]), 900)
