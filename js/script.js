612c262ffb5e531f05389955de0812dc846197ad
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-24 17:32:23 +0300

    Two Joggers (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6006,3 +6006,57 @@ const findLongest = (array) => array.reduce((a, b) => (`${b}`.length > `${a}`.le
 console.log(findLongest([1, 10, 100]), 100)
 console.log(findLongest([9000, 8, 800]), 9000)
 console.log(findLongest([8, 900, 500]), 900)
+
+// Two Joggers (6 kyu)
+
+/* 
+Description
+Bob and Charles are meeting for their weekly jogging tour. 
+They both start at the same spot called "Start" and they each run a different lap, 
+which may (or may not) vary in length. 
+Since they know each other for a long time already, they both run at the exact same speed.
+
+Illustration
+Example where Charles (dashed line) runs a shorter lap than Bob:
+
+Example laps
+
+Task
+Your job is to complete the function nbrOfLaps(x, y) that, given the length of the laps for Bob and Charles, 
+finds the number of laps that each jogger has to complete before they meet each other again, at the same time, at the start.
+
+The function takes two arguments:
+
+The length of Bob's lap (larger than 0)
+The length of Charles' lap (larger than 0)
+
+The function should return an array (Tuple<int, int> in C#) containing exactly two numbers:
+
+The first number is the number of laps that Bob has to run
+The second number is the number of laps that Charles has to run
+
+Examples:
+
+nbrOfLaps(5, 3); // returns [3, 5]
+nbrOfLaps(4, 6); // returns [3, 2]
+*/
+
+const nbrOfLaps = (x, y) => {
+	if (x === y) {
+		return [1, 1];
+	}
+
+	function gcd(n, m) {
+		return m == 0 ? n : gcd(m, n % m);
+	}
+
+	function nok(n, m) {
+		return n * m / gcd(n, m);
+	}
+
+	return [nok(x, y) / x, nok(x, y) / y]
+}
+
+console.log(nbrOfLaps(5, 3), [3, 5]);
+console.log(nbrOfLaps(4, 6), [3, 2]);
+console.log(nbrOfLaps(5, 5), [1, 1]);
