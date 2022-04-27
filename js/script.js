9f799320320acfddf26d62cd9dfe73db0cb6876c
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-27 21:54:19 +0300

    String average (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6176,3 +6176,35 @@ console.log(dup(["adanac", "soonness", "toolless", "ppellee"]), ['adanac', 'sone
 console.log(dup(["callalloo", "feelless", "heelless"]), ['calalo', 'feles', 'heles']);
 console.log(dup(["putteellinen", "keenness"]), ['putelinen', 'kenes']);
 console.log(dup(["kelless", "voorraaddoosspullen", "achcha"]), ['keles', 'voradospulen', 'achcha']);
+
+// String average (6 kyu)
+
+/* 
+You are given a string of numbers between 0-9. 
+Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
+
+"zero nine five two" -> "four"
+
+If the string is empty or includes a number greater than 9, return "n/a"
+*/
+
+const averageString = (str) => {
+	const num = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }
+	const word = { 0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine' }
+	if (!str.length) return 'n/a';
+	let n = 0;
+	const length = str.split(' ').length;
+	const arr = str.split(' ')
+		.map(item => num.hasOwnProperty(item) ? item = num[item] : n = 1)
+		.reduce((a, b) => a + b, 0);
+
+	return n === 0 ? word[Math.floor(arr / length)] : 'n/a';
+}
+
+console.log(averageString("zero nine five two"), "four");
+console.log(averageString("four six two three"), "three");
+console.log(averageString("one two three four five"), "three");
+console.log(averageString("five four"), "four");
+console.log(averageString("zero zero zero zero zero"), "zero");
+console.log(averageString("one one eight one"), "two");
+console.log(averageString(""), "n/a");
