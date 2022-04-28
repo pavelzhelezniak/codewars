aec2da4eb458f7035aa8c9d424b46032bb956e49
pavelzhelezniak <zhele3o1992@gmail.com>
2022-04-28 16:31:27 +0300

    Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6208,3 +6208,69 @@ console.log(averageString("five four"), "four");
 console.log(averageString("zero zero zero zero zero"), "zero");
 console.log(averageString("one one eight one"), "two");
 console.log(averageString(""), "n/a");
+
+// Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer (6 kyu)
+
+/* 
+You will be given a sequence of objects representing data about developers who have 
+signed up to attend the next coding meetup that you are organising.
+
+Your task is to return a sequence which includes the developer who is the oldest. 
+In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+*/
+
+const findSenior = (list) => {
+	const ageList = [];
+	list.forEach(item => {
+		ageList.push(item.age);
+	});
+	const maxAge = Math.max(...ageList);
+
+	return list.filter(item => item.age === maxAge);
+}
+
+const list1 = [
+	{ firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+	{ firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+	{ firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+	{ firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+
+const answer1 = [
+	{ firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+	{ firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+
+
+const list2 = [
+	{ firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+	{ firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+	{ firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+];
+
+const answer2 = [
+	{ firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+];
+
+
+const list3 = [
+	{ firstName: 'Alexander', lastName: 'F.', country: 'Russia', continent: 'Europe', age: 89, language: 'Java' },
+	{ firstName: 'Fatima', lastName: 'K.', country: 'Saudi Arabia', continent: 'Asia', age: 21, language: 'Clojure' },
+	{ firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+	{ firstName: 'Nikola', lastName: 'H.', country: 'Serbia', continent: 'Europe', age: 29, language: 'Python' },
+	{ firstName: 'Jakub', lastName: 'I.', country: 'Slovakia', continent: 'Europe', age: 28, language: 'Java' },
+	{ firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 89, language: 'JavaScript' },
+	{ firstName: 'Luka', lastName: 'J.', country: 'Slovenia', continent: 'Europe', age: 29, language: 'Clojure' },
+	{ firstName: 'Mariam', lastName: 'B.', country: 'Egypt', continent: 'Africa', age: 89, language: 'Python' },
+];
+
+const answer3 = [
+	{ firstName: 'Alexander', lastName: 'F.', country: 'Russia', continent: 'Europe', age: 89, language: 'Java' },
+	{ firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 89, language: 'JavaScript' },
+	{ firstName: 'Mariam', lastName: 'B.', country: 'Egypt', continent: 'Africa', age: 89, language: 'Python' },
+];
+
+
+console.log(findSenior(list1), answer1);
+console.log(findSenior(list2), answer2);
+console.log(findSenior(list3), answer3);
