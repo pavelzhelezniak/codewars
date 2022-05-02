4c98bde6675cef49b02ebe84e824a9614be4be73
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-02 14:13:59 +0300

    Lottery Ticket (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6387,3 +6387,42 @@ console.log(formatWords(['one', 'two', '']), 'one and two', "formatWords(['one',
 console.log(formatWords([]), '', 'formatWords([]) should return ""');
 console.log(formatWords(null), '', 'formatWords(null) should return ""');
 console.log(formatWords(['']), '', 'formatWords([""]) should return ""');
+
+// Lottery Ticket (6 kyu)
+
+/* 
+Time to win the lottery!
+
+Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+Example ticket:
+
+[ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+To do this, you must first count the 'mini-wins' on your ticket. 
+Each subarray has both a string and a number within it. 
+If the character code of any of the characters in the string matches the number, you get a mini win. 
+Note you can only have one mini win per sub array.
+
+Once you have counted all of your mini wins, compare that number to the other input provided (win). 
+If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+All inputs will be in the correct format. Strings on tickets are not always the same length.
+*/
+
+const bingo = (ticket, win) => {
+	let count = 0;
+	ticket.forEach(item => {
+		let [lineCode, value] = item;
+		lineCode = [...new Set(lineCode.split(''))].join('');
+
+		for (let i = 0; i < lineCode.length; i++) {
+			lineCode.charCodeAt(i) === value ? count++ : null;
+		}
+	})
+
+	return count >= win ? 'Winner!' : 'Loser!';
+}
+
+console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2), 'Loser!');
+console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1), 'Winner!');
+console.log(bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3), 'Loser!');
