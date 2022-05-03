19fa23f8833bbea1065b95f4ac1d104406ad6023
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-03 20:48:32 +0300

    Count letters in string (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6426,3 +6426,24 @@ const bingo = (ticket, win) => {
 console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2), 'Loser!');
 console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1), 'Winner!');
 console.log(bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3), 'Loser!');
+
+// Count letters in string (6 kyu)
+
+/* 
+In this kata, you've to count lowercase letters in a given string and 
+return the letter count in a hash with 'letter' as key and count as 'value'. 
+The key must be 'symbol' instead of string in Ruby and 'char' instead of string in Crystal.
+*/
+
+const letterCount = (s) => {
+	const resultHash = {};
+	s.split('').sort().map(item => resultHash[item] = resultHash[item] + 1 || 1);
+
+	return resultHash;
+}
+
+console.log(letterCount("codewars"), { "a": 1, "c": 1, "d": 1, "e": 1, "o": 1, "r": 1, "s": 1, "w": 1 });
+console.log(letterCount("activity"), { "a": 1, "c": 1, "i": 2, "t": 2, "v": 1, "y": 1 });
+console.log(letterCount("arithmetics"), { "a": 1, "c": 1, "e": 1, "h": 1, "i": 2, "m": 1, "r": 1, "s": 1, "t": 2 });
+console.log(letterCount("traveller"), { "a": 1, "e": 2, "l": 2, "r": 2, "t": 1, "v": 1 });
+console.log(letterCount("daydreamer"), { "a": 2, "d": 2, "e": 2, "m": 1, "r": 2, "y": 1 });
