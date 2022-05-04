0232a807f73f53854adcaa9d28dfc06a591d354b
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-04 17:45:56 +0300

    Prefill an Array (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6447,3 +6447,53 @@ console.log(letterCount("activity"), { "a": 1, "c": 1, "i": 2, "t": 2, "v": 1, "
 console.log(letterCount("arithmetics"), { "a": 1, "c": 1, "e": 1, "h": 1, "i": 2, "m": 1, "r": 1, "s": 1, "t": 2 });
 console.log(letterCount("traveller"), { "a": 1, "e": 2, "l": 2, "r": 2, "t": 1, "v": 1 });
 console.log(letterCount("daydreamer"), { "a": 2, "d": 2, "e": 2, "m": 1, "r": 2, "y": 1 });
+
+// Prefill an Array (6 kyu)
+
+/* 
+Create the function prefill that returns an array of n elements that all have the same value v. 
+See if you can do this without using a loop.
+
+You have to validate input:
+
+v can be anything (primitive or otherwise)
+if v is ommited, fill the array with undefined
+if n is 0, return an empty array
+if n is anything other than an integer or integer-formatted 
+string (e.g. '123') that is >=0, throw a TypeError
+When throwing a TypeError, the message should be n is invalid, 
+where you replace n for the actual value passed to the function.
+
+Code Examples
+
+prefill(3,1) --> [1,1,1]
+
+prefill(2,"abc") --> ['abc','abc']
+
+prefill("1", 1) --> [1]
+
+prefill(3, prefill(2,'2d'))
+--> [['2d','2d'],['2d','2d'],['2d','2d']]
+
+prefill("xyz", 1)
+--> throws TypeError with message "xyz is invalid"
+*/
+
+const prefill = (n, v) => {
+	if (n === 0 || n === '0') return [];
+
+	if (n < 0 || !Number.isInteger(n)) {
+		let typeError = new TypeError();
+		typeError.name = "TypeError";
+		typeError.message = n + " is invalid";
+		throw typeError
+	}
+
+	return new Array(n).fill(v);
+}
+
+console.log(prefill(3, 1), [1, 1, 1]);
+console.log(prefill(2, 'abc'), ['abc', 'abc']);
+//console.log(prefill('1', 1), [1]);
+console.log(prefill(3, prefill(2, '2d')), [['2d', '2d'], ['2d', '2d'], ['2d', '2d']]);
+//console.log(prefill('xyz', 1));
