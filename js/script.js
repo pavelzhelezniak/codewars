9239fc99a3bfbd5bc7a30c55fe100f72a31ad63e
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-05 13:33:20 +0300

    Twisted Sum (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6500,3 +6500,33 @@ console.log(prefill(2, 'abc'), ['abc', 'abc']);
 //console.log(prefill('1', 1), [1]);
 console.log(prefill(3, prefill(2, '2d')), [['2d', '2d'], ['2d', '2d'], ['2d', '2d']]);
 //console.log(prefill('xyz', 1));
+
+// Twisted Sum (6 kyu)
+
+/* 
+Find the sum of the digits of all the numbers from 1 to N (both ends included).
+
+Examples
+# N = 4
+1 + 2 + 3 + 4 = 10
+
+# N = 10
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) = 46
+
+# N = 12
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
+*/
+
+const twistedSum = (n) => {
+	let result = 0;
+	for (let i = 1; i <= n; i++) {
+		result += i.toString().split('').reduce((acc, item) => acc + parseInt(item, 10), 0)
+	}
+
+	return result;
+}
+
+console.log(twistedSum(3), 6, "n = 3");
+console.log(twistedSum(10), 46, "n = 10");
+console.log(twistedSum(11), 48, "n = 11");
+console.log(twistedSum(12), 51, "n = 12");
