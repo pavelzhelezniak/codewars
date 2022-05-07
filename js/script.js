a2c7ede0c83deadc8f7b00d414a9b46e22d94937
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-07 15:45:34 +0300

    Sum two arrays (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6564,3 +6564,43 @@ const findChildren = (dancingBrigade) => {
 
 console.log(findChildren("beeeEBb"), "BbbEeee");
 console.log(findChildren("uwwWUueEe"), "EeeUuuWww");
+
+// Sum two arrays (6 kyu)
+
+/* 
+Your task is to create a function called addArrays, 
+which takes two arrays consisting of integers, 
+and returns the sum of those two arrays.
+
+The twist is that (for example) [3,2,9] does not equal 3 + 2 + 9, 
+it would equal '3' + '2' + '9' converted to an integer for this kata, 
+meaning it would equal 329. The output should be an array of the sum in a similar fashion to the input (for example, 
+if the sum is 341, you would return [3,4,1]). Examples are given below of what two arrays should return.
+
+[3,2,9],[1,2] --> [3,4,1]
+[4,7,3],[1,2,3] --> [5,9,6]
+[1],[5,7,6] --> [5,7,7]
+If both arrays are empty, return an empty array.
+
+In some cases, there will be an array containing a negative number as the first index in the array. 
+In this case treat the whole number as a negative number. See below:
+
+[3,2,6,6],[-7,2,2,8] --> [-3,9,6,2] # 3266 + (-7228) = -3962
+*/
+
+const addArrays = (array1, array2) => {
+	if (!array1.length && !array2.length) return [];
+	if (!array1.length || !array2.length) return array1.length > array2.length ? array1 : array2;
+
+	const num1 = parseInt(array1.join(''));
+	const num2 = parseInt(array2.join(''));
+	const result = num1 + num2;
+	const sign = result > 0 ? 1 : -1;
+
+	return `${Math.abs(result)}`.split('').map((char, idx) => idx ? char * 1 : char * sign);
+
+}
+
+console.log(addArrays([6, 7], [6, 7]), [1, 3, 4]);
+console.log(addArrays([4, 7, 3], [1, 2, 3]), [5, 9, 6]);
+console.log(addArrays([3, 2, 6, 6], [-7, 2, 2, 8]), [-3, 9, 6, 2]);
