8c0f395f9cd2fe617c62e63343e7c45371420a5d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-08 18:17:16 +0300

    zipWith (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6604,3 +6604,44 @@ const addArrays = (array1, array2) => {
 console.log(addArrays([6, 7], [6, 7]), [1, 3, 4]);
 console.log(addArrays([4, 7, 3], [1, 2, 3]), [5, 9, 6]);
 console.log(addArrays([3, 2, 6, 6], [-7, 2, 2, 8]), [-3, 9, 6, 2]);
+
+// zipWith (6 kyu)
+
+/* 
+Implement zipWith
+zipWith takes a function and two arrays and zips the arrays together, applying the function to every pair of values.
+The function value is one new array.
+
+If the arrays are of unequal length, the output will only be as long as the shorter one.
+(Values of the longer array are simply not used.)
+
+Inputs should not be modified.
+
+Examples
+zipWith( Math.pow, [10,10,10,10], [0,1,2,3] )      =>  [1,10,100,1000]
+zipWith( Math.max, [1,4,7,1,4,7], [4,7,1,4,7,1] )  =>  [4,7,7,4,7,7]
+
+zipWith( function(a,b) { return a+b; }, [0,1,2,3], [0,1,2,3] )  =>  [0,2,4,6]  Both forms are valid.
+zipWith( (a,b) => a+b,                  [0,1,2,3], [0,1,2,3] )  =>  [0,2,4,6]  Both are functions.
+Input validation
+Assume all input is valid.
+*/
+
+const zipWith = (fn, a0, a1) => {
+	const minLength = Math.min(a0.length, a1.length);
+	const result = [];
+	for (let i = 0; i < minLength; i += 1) {
+		result.push(fn(a0[i], a1[i]));
+	}
+
+	return result;
+}
+
+const plus = (a, b) => a + b;
+console.log(zipWith(plus, [0, 1, 2, 3, 4], [6, 5, 4, 3, 2, 1]), [6, 6, 6, 6, 6]);
+console.log(zipWith(plus, [0, 1, 2, 3, 4, 5], [6, 5, 4, 3, 2, 1]), [6, 6, 6, 6, 6, 6]);
+console.log(zipWith(plus, [0, 1, 2, 3, 4, 5], [6, 5, 4, 3, 2]), [6, 6, 6, 6, 6]);
+console.log(zipWith(Math.pow, [10, 10, 10, 10], [0, 1, 2, 3]), [1, 10, 100, 1000]);
+console.log(zipWith(Math.max, [1, 4, 7, 1, 4, 7], [4, 7, 1, 4, 7, 1]), [4, 7, 7, 4, 7, 7]);
+console.log(zipWith(function (a, b) { return a + b; }, [0, 1, 2, 3], [0, 1, 2, 3]), [0, 2, 4, 6]);
+console.log(zipWith((a, b) => a + b, [0, 1, 2, 3], [0, 1, 2, 3]), [0, 2, 4, 6]);
