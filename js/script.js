d9e24719f1cd572f14d0b49172c844555bec247d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-15 17:57:02 +0300

    The Vowel Code (6 kyu)

--- js/script.js
+++ js/script.js
@@ -6868,3 +6868,32 @@ console.log(solvee("strength"), 57);
 console.log(solvee("catchphrase"), 73);
 console.log(solvee("twelfthstreet"), 103);
 console.log(solvee("mischtschenkoana"), 80);
+
+// The Vowel Code (6 kyu)
+
+/* 
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+For example, encode("hello") would return "h2ll4". 
+There is no need to worry about uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+*/
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const v4w2ls = ['1', '2', '3', '4', '5'];
+
+const encode = str => [...str].map(e => vowels.includes(e) ? e = vowels.indexOf(e) + 1 : e).join('');
+const decode = str => [...str].map(e => v4w2ls.includes(e) ? e = vowels[v4w2ls.indexOf(e)] : e).join('');
+
+console.log(encode('hello'), 'h2ll4');
+console.log(encode('How are you today?'), 'H4w 1r2 y45 t4d1y?');
+console.log(encode('This is an encoding test.'), 'Th3s 3s 1n 2nc4d3ng t2st.');
+console.log(decode('h2ll4'), 'hello');
