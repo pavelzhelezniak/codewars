b9be3d6490226f6b6b17109be48ba4e10bc8acc8
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-19 11:59:50 +0300

    Sum consecutives (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7014,3 +7014,44 @@ console.log(FileNameExtractor.extractFileName("1_FILE_NAME.EXTENSION.OTHEREXTENS
 console.log(FileNameExtractor.extractFileName("1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION"), "FILE_NAME.EXTENSION");
 console.log(FileNameExtractor.extractFileName("1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34"), "This_is_an_otherExample.mpg");
 console.log(FileNameExtractor.extractFileName("1231231223123131_myFile.tar.gz2"), "myFile.tar");
+
+// Sum consecutives (6 kyu)
+
+/* 
+You are given a list/array which contains only integers (positive and negative). 
+Your job is to sum only the numbers that are the same and consecutive. 
+The result should be one list.
+
+Extra credit if you solve it in one line. 
+You can assume there is never an empty list/array and there will always be an integer.
+Same meaning: 1 == 1
+1 != -1
+
+#Examples:
+[1,4,4,4,0,4,3,3,1] # should return [1,12,0,4,6,1]
+"""So as you can see sum of consecutives 1 is 1 
+sum of 3 consecutives 4 is 12 
+sum of 0... and sum of 2 
+consecutives 3 is 6 ..."""
+[1,1,7,7,3] # should return [2,14,3]
+[-5,-5,7,7,12,0] # should return [-10,14,12,0]
+*/
+
+const sumConsecutives = (s) => {
+	const resArr = [];
+	let count = 0;
+	for (let i = 0; i < s.length; i++) {
+		count += s[i];
+		if (s[i] !== s[i + 1]) {
+			resArr.push(count);
+			count = 0;
+		}
+	}
+
+	return resArr;
+}
+
+console.log(sumConsecutives([1, 4, 4, 4, 0, 4, 3, 3, 1]), [1, 12, 0, 4, 6, 1], "on list [1,4,4,0,4,3,3,1] you get ")
+console.log(sumConsecutives([1, 1, 7, 7, 3]), [2, 14, 3], "on list [1,1,7,7,3] you get ")
+console.log(sumConsecutives([-5, -5, 7, 7, 12, 0]), [-10, 14, 12, 0], "on list [-5,-5,7,7,12,0] you get ")
+console.log(sumConsecutives([3, 3, 3, 3, 1]), [12, 1], "on list [3,3,3,3,1] you get ")
