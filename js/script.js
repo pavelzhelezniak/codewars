487a3b72ffa80c645cbf11f92b58e2d933d86377
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-20 09:57:04 +0300

    English beggars (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7055,3 +7055,40 @@ console.log(sumConsecutives([1, 4, 4, 4, 0, 4, 3, 3, 1]), [1, 12, 0, 4, 6, 1], "
 console.log(sumConsecutives([1, 1, 7, 7, 3]), [2, 14, 3], "on list [1,1,7,7,3] you get ")
 console.log(sumConsecutives([-5, -5, 7, 7, 12, 0]), [-10, 14, 12, 0], "on list [-5,-5,7,7,12,0] you get ")
 console.log(sumConsecutives([3, 3, 3, 3, 1]), [12, 1], "on list [3,3,3,3,1] you get ")
+
+// English beggars (6 kyu)
+
+/*
+Description:
+Born a misinterpretation of this kata, your task here is pretty simple: 
+given an array of values and an amount of beggars, 
+you are supposed to return an array with the sum of what each beggar brings home, 
+assuming they all take regular turns, from the first to the last.
+For example: [1,2,3,4,5] for 2 beggars will return a result of [9,6], 
+as the first one takes [1,3,5], the second collects [2,4].
+The same array with 3 beggars would have in turn have produced a better out come for the second beggar: [5,7,3], 
+as they will respectively take [1,4], [2,5] and [3].
+Also note that not all beggars have to take the same amount of "offers", 
+meaning that the length of the array is not necessarily a multiple of n; 
+length can be even shorter, in which case the last beggers will of course take nothing (0).
+Note: in case you don't get why this kata is about English beggars, 
+then you are not familiar on how religiously queues are taken in the kingdom ;)
+*/
+
+const beggars = (values, n) => {
+	let arr = [];
+	for (let i = 1; i <= n; i++) {
+		arr[i - 1] = 0;
+		for (let j = i - 1; j < values.length;) {
+			arr[i - 1] += values[j];
+			j = j + n;
+		}
+	}
+	return arr;
+}
+
+console.log(beggars([1, 2, 3, 4, 5], 1), [15]);
+console.log(beggars([1, 2, 3, 4, 5], 2), [9, 6]);
+console.log(beggars([1, 2, 3, 4, 5], 3), [5, 7, 3]);
+console.log(beggars([1, 2, 3, 4, 5], 6), [1, 2, 3, 4, 5, 0]);
+console.log(beggars([1, 2, 3, 4, 5], 0), []);
