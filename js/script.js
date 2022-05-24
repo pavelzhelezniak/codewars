15478a3a2f38b3dc7c55d4966ea5c79f0735d7e0
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-24 12:17:34 +0300

    Arrh, grabscrab! (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7148,3 +7148,30 @@ const stringTransformer = (str) => {
 
 console.log(stringTransformer('Example string'), 'STRING eXAMPLE');
 console.log(stringTransformer('Gas Etp Myx   Ebxv   Ewtu'), 'STRING eXAMPLE');
+
+// Arrh, grabscrab! (6 kyu)
+
+/* 
+Pirates have notorious difficulty with enunciating. 
+They tend to blur all the letters together and scream at people.
+At long last, we need a way to unscramble what these pirates are saying.
+Write a function that will accept a jumble of letters as well as a dictionary, 
+and output a list of words that the pirate might have meant.
+
+For example:
+grabscrab( "ortsp", ["sport", "parrot", "ports", "matey"] )
+Should return ["sport", "ports"].
+
+Return matches in the same order as in the dictionary. Return an empty array if there are no matches.
+Good luck!
+*/
+
+const sortStr = str => str.split('').sort().join('');
+const grabscrab = (anagram, dictionary) => [...dictionary].filter(item => sortStr(item) === sortStr(anagram));
+
+console.log(grabscrab("trisf", ["first"]), ["first"], "Should have found 'first'");
+console.log(grabscrab("oob", ["bob", "baobab"]), [], "Should not have found anything");
+console.log(grabscrab("ainstuomn", ["mountains", "hills", "mesa"]), ["mountains"], "Should have found 'mountains'");
+console.log(grabscrab("oolp", ["donkey", "pool", "horse", "loop"]), ["pool", "loop"], "Should have found 'pool' and 'loop'");
+console.log(grabscrab("ortsp", ["sport", "parrot", "ports", "matey"]), ["sport", "ports"], "Should have found 'sport' and 'ports'");
+console.log(grabscrab("ourf", ["one", "two", "three"]), [], "Should not have found anything");
