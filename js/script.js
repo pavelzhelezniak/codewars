2a2fbf57b15a4e6b829aae03f169278413a1b940
pavelzhelezniak <zhele3o1992@gmail.com>
2022-05-27 10:33:48 +0300

    Validate Credit Card Number (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7248,3 +7248,56 @@ console.log(numericals("Hello, World! It's me, JomoPipi!"), "1112111121311211113
 console.log(numericals("hello hello"), "11121122342");
 console.log(numericals("Hello"), "11121");
 console.log(numericals("aaaaaaaaaaaa"), "123456789101112");
+
+// Validate Credit Card Number (6 kyu)
+
+/* 
+In this Kata, you will implement the Luhn Algorithm, which is used to help validate credit card numbers.
+Given a positive integer of up to 16 digits, return true if it is a valid credit card number, and false if it is not
+Here is the algorithm:
+Double every other digit, scanning from right to left, starting from the second digit (from the right).
+Another way to think about it is: if there are an even number of digits,
+double every other digit starting with the first; if there are an odd number of digits, 
+double every other digit starting with the second:
+1714 ==> [1*, 7, 1*, 4] ==> [2, 7, 2, 4]
+12345 ==> [1, 2*, 3, 4*, 5] ==> [1, 4, 3, 8, 5]
+891 ==> [8, 9*, 1] ==> [8, 18, 1]
+If a resulting number is greater than 9, 
+replace it with the sum of its own digits (which is the same as subtracting 9 from it):
+[8, 18*, 1] ==> [8, (1+8), 1] ==> [8, 9, 1]
+
+or:
+[8, 18*, 1] ==> [8, (18-9), 1] ==> [8, 9, 1]
+Sum all of the final digits:
+
+[8, 9, 1] ==> 8 + 9 + 1 = 18
+Finally, take that sum and divide it by 10. If the remainder equals zero, 
+the original credit card number is valid.
+
+18 (modulus) 10 ==> 8 , which is not equal to 0, so this is not a valid credit card number
+*/
+
+const validate = (n) => {
+	let sum = 0;
+
+	while (n > 0) {
+		const a = n % 10;
+		n = Math.floor(n / 10);
+
+		let b = (n % 10) * 2;
+		n = Math.floor(n / 10);
+
+		if (b > 9) {
+			b -= 9;
+		}
+
+		sum += a + b;
+	}
+
+	return sum % 10 === 0;
+}
+
+console.log(validate(123), false);
+console.log(validate(1), false);
+console.log(validate(2121), true);
+console.log(validate(1230), true);
