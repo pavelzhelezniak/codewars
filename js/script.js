5b9f6b4bafb47bc3aec1d16ed072c43854712fa6
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-02 09:58:55 +0300

    Throwing Darts (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7517,3 +7517,48 @@ const alphabetized = (s) => {
 }
 
 console.log(alphabetized('The Holy Bible'), 'BbeehHilloTy')
+
+// Throwing Darts (6 kyu)
+
+/* 
+You've just recently been hired to calculate scores for a Dart Board game!
+
+Scoring specifications:
+
+0 points - radius above 10
+5 points - radius between 5 and 10 inclusive
+10 points - radius less than 5
+If all radii are less than 5, award 100 BONUS POINTS!
+
+Write a function that accepts an array of radii (can be integers and/or floats), and returns a total score using the above specification.
+
+An empty array should return 0.
+
+Examples:
+scoreThrows( [1, 5, 11] )    =>  15
+scoreThrows( [15, 20, 30] )  =>  0
+scoreThrows( [1, 2, 3, 4] )  =>  140
+*/
+
+const scoreThrows = (radii) => {
+	let res = 0;
+
+	if (radii.length === 0) return res;
+
+	radii.forEach(item => {
+		item < 5 ? res += 10 : (item >= 5 && item <= 10) ? res += 5 : res += 0;
+	})
+
+	if (radii.every(item => item < 5)) {
+		res += 100;
+	}
+
+	return res
+}
+
+console.log(scoreThrows([1, 5, 11]), 15);
+console.log(scoreThrows([15, 20, 30, 31, 32, 44, 100]), 0);
+console.log(scoreThrows([1, 2, 3, 4]), 140);
+console.log(scoreThrows([]), 0, 'Empty list');
+console.log(scoreThrows([1, 2, 3, 4, 5, 6, 7, 8, 9]), 65);
+console.log(scoreThrows([0, 5, 10, 10.5, 4.5]), 30);
