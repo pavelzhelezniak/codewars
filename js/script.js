1be5fe0e375745da1ca8fdaef39593ad6b3f4af7
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-03 15:05:38 +0300

    Coding Meetup #10 - Higher-Order Functions Series - Create usernames (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7562,3 +7562,61 @@ console.log(scoreThrows([1, 2, 3, 4]), 140);
 console.log(scoreThrows([]), 0, 'Empty list');
 console.log(scoreThrows([1, 2, 3, 4, 5, 6, 7, 8, 9]), 65);
 console.log(scoreThrows([0, 5, 10, 10.5, 4.5]), 30);
+
+// Coding Meetup #10 - Higher-Order Functions Series - Create usernames (6 kyu)
+
+/* 
+Given the following input array:
+
+var list1 = [
+  { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+  { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
+];
+write a function that adds the username property to each object in the input array:
+
+[
+  { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby', 
+	 username: 'emilyn1990' },
+  { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure', 
+	 username: 'nore2000' }
+]
+The value of the username property is composed by concatenating:
+
+firstName in lower-case;
+first letter of the lastName in lower-case; and
+the birth year which for the purpose of this kata is calculated simply by subtracting age from the current year. 
+Please make sure that your function delivers the correct birth year irrespective of when it will be executed, 
+for example it should also work correctly for a meetup organised in 10-years-time. 
+The example above assumes that the function is run in year 2020.
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer.
+Lastname will always be one upper-cased letter followed by dot, e.g. 'N.'
+Order of the objects in the array should be maintained but order of the properties in the individual objects does not matter.
+*/
+
+function addUsername(list) {
+	const resultList = []
+	list.forEach(item => {
+		const firstName = item.firstName.toLowerCase();
+		const lastName = item.lastName[0].toLowerCase();
+		const year = (new Date().getFullYear() - item.age).toString();
+		let username = `${firstName}${lastName}${year}`;
+		item.username = username;
+		resultList.push(item);
+	})
+
+	return resultList;
+}
+
+const list123 = [
+	{ firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+	{ firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
+];
+
+console.log(addUsername(list123),
+	[
+		{ firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby', username: 'emilyn1990' },
+		{ firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure', username: 'nore2000' }
+	]);
