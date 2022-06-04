a04e81fbef8b4368b0dcb1dae4039f6121492fc5
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-04 18:13:43 +0300

    String Breakers (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7620,3 +7620,32 @@ console.log(addUsername(list123),
 		{ firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby', username: 'emilyn1990' },
 		{ firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure', username: 'nore2000' }
 	]);
+
+// String Breakers (6 kyu)
+
+/* 
+I will give you an integer (N) and a string. Break the string up into as many 
+substrings of N as you can without spaces. If there are leftover characters, include those as well.
+Example: 
+N = 5;
+String = "This is an example string";
+Return value:
+Thisi
+sanex
+ample
+strin
+g
+Return value as a string: 'Thisi'+'\n'+'sanex'+'\n'+'ample'+'\n'+'strin'+'\n'+'g'
+*/
+
+const stringBreakers = (n, string) => {
+	string = string.replace(/\s/g, '');
+	let arr = [];
+	for (let i = 0; i < string.length; i += n) {
+		arr.push(string.slice(i, i + n));
+	}
+
+	return arr.join('\n');
+}
+
+console.log(stringBreakers(5, 'This is an example string'), 'Thisi' + '\n' + 'sanex' + '\n' + 'ample' + '\n' + 'strin' + '\n' + 'g');
