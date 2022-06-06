3376ce83c04e8a8b4efbf207773fd64ef3cbc1a2
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-06 15:55:14 +0300

    Sorting on planet Twisted-3-7 (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7687,3 +7687,43 @@ console.log(moreZeros('abcde'), ['a', 'b', 'd']);
 console.log(moreZeros('Great job!'), ['a', ' ', 'b', '!']);
 console.log(moreZeros('DIGEST'), ['D', 'I', 'E', 'T']);
 console.log(moreZeros('abcdeabcde'), ['a', 'b', 'd'], 'Should not return duplicates values');
+
+// Sorting on planet Twisted-3-7 (6 kyu)
+
+/* 
+There is a planet... in a galaxy far far away. It is exactly like our planet, but it has one difference:
+ #The values of the digits 3 and 7 are twisted. Our 3 means 7 on the planet Twisted-3-7. And 7 means 3.
+
+Your task is to create a method, that can sort an array the way it would be sorted on Twisted-3-7.
+
+7 Examples from a friend from Twisted-3-7:
+
+[1,2,3,4,5,6,7,8,9] -> [1,2,7,4,5,6,3,8,9]
+[12,13,14] -> [12,14,13]
+[9,2,4,7,3] -> [2,7,4,3,9]
+There is no need for a precheck. The array will always be not null and will always contain at least one number.
+
+You should not modify the input array!
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+*/
+
+const sortTwisted37 = (array) => {
+	const change = (val) => {
+		let result = val.toString().split('');
+		for (let i = 0; i < result.length; i++) {
+			if (result[i] === '3') result[i] = '7';
+			else if (result[i] === '7') result[i] = '3';
+		}
+		return Number(result.join(''));
+	}
+
+	const newArr = [...array].map(a => change(a));
+	return newArr.sort((a, b) => a - b).map(a => change(a))
+}
+
+console.log(sortTwisted37([1, 2, 3, 4, 5, 6, 7, 8, 9]), [1, 2, 7, 4, 5, 6, 3, 8, 9]);
+console.log(sortTwisted37([12, 13, 14]), [12, 14, 13]);
+console.log(sortTwisted37([9, 2, 4, 7, 3]), [2, 7, 4, 3, 9]);
