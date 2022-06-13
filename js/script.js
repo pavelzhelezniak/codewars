06fc8b6cada5a5980bc35b5808e4d851fda26571
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-13 11:27:46 +0300

    Prime Factors (6 kyu)

--- js/script.js
+++ js/script.js
@@ -7842,3 +7842,41 @@ console.log(solve123("abbaab"), "not possible");
 console.log(solve123("madmam"), "remove one");
 console.log(solve123("raydarm"), "not possible");
 console.log(solve123("hannah"), "OK");
+
+// Prime Factors (6 kyu)
+
+/* 
+Prime Factors
+Inspired by one of Uncle Bob's TDD Kata
+
+Write a function that generates factors for a given number.
+
+The function takes an integer on the standard input and returns a list of integers (ObjC: 
+array of NSNumbers representing integers). That list contains the prime factors in numerical sequence.
+
+Examples
+1  ==>  []
+3  ==>  [3]
+8  ==>  [2, 2, 2]
+9  ==>  [3, 3]
+12 ==>  [2, 2, 3]
+*/
+
+const prime_factors = n => {
+	const factorsArr = [];
+
+	for (let i = 2; i <= n; i++) {
+		while (n % i === 0) {
+			factorsArr.push(i);
+			n /= i;
+		}
+	}
+
+	return factorsArr;
+}
+
+console.log(prime_factors(1), []);
+console.log(prime_factors(3), [3]);
+console.log(prime_factors(8), [2, 2, 2]);
+console.log(prime_factors(9), [3, 3]);
+console.log(prime_factors(12), [2, 2, 3]);
