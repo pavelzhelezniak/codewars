e21d3da4e3a74e046b6b90d4282eb5dccc78102f
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-15 14:20:30 +0300

    Find the nth Digit of a Number (7 kyu)

--- js/script.js
+++ js/script.js
@@ -7880,3 +7880,37 @@ console.log(prime_factors(3), [3]);
 console.log(prime_factors(8), [2, 2, 2]);
 console.log(prime_factors(9), [3, 3]);
 console.log(prime_factors(12), [2, 2, 3]);
+
+// Find the nth Digit of a Number (7 kyu)
+
+/* 
+Complete the function that takes two numbers as input, 
+num and nth and return the nth digit of num (counting from right to left).
+
+Note
+If num is negative, ignore its sign and treat it as a positive value
+If nth is not positive, return -1
+Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
+Examples(num, nth --> output)
+5673, 4 --> 5
+129, 2 --> 2
+-2825, 3 --> 8
+-456, 4 --> 0
+0, 20 --> 0
+65, 0 --> -1
+24, -8 --> -1
+*/
+
+const findDigit = function (num, nth) {
+	if (nth <= 0) return -1;
+	let nums = ('' + Math.abs(num));
+	return nums.slice(nums.length - nth, nums.length - nth + 1) * 1;
+}
+
+console.log(findDigit(5673, 4), 5);
+console.log(findDigit(129, 2), 2);
+console.log(findDigit(-2825, 3), 8);
+console.log(findDigit(-456, 4), 0);
+console.log(findDigit(0, 20), 0);
+console.log(findDigit(65, 0), -1);
+console.log(findDigit(24, -8), -1);
