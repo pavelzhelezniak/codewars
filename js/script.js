250933373ae1a6e829237c8b5b04cc664238b2bb
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-23 12:31:10 +0300

    Sort the Gift Code (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8120,3 +8120,31 @@ console.log(firstNSmallest([1, 2, 3, 4, 2], 4), [1, 2, 3, 2]);
 console.log(firstNSmallest([2, 1, 2, 3, 4, 2], 2), [2, 1]);
 console.log(firstNSmallest([2, 1, 2, 3, 4, 2], 3), [2, 1, 2]);
 console.log(firstNSmallest([2, 1, 2, 3, 4, 2], 4), [2, 1, 2, 2]);
+
+// Sort the Gift Code (7 kyu)
+
+/* 
+Happy Holidays fellow Code Warriors!
+Santa's senior gift organizer Elf developed a way to represent up to 26 gifts by assigning a unique alphabetical character to each gift. 
+After each gift was assigned a character, the gift organizer Elf then joined the characters to form the gift ordering code.
+
+Santa asked his organizer to order the characters in alphabetical order, 
+but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+
+Sort the Gift Code
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, 
+and returns a string containing the same characters in alphabetical order.
+
+Examples (Input -- => Output):
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+*/
+
+const sortGiftCode = (code) => {
+	return code.split('').sort().join('');
+}
+
+console.log(sortGiftCode('abcdef'), 'abcdef');
+console.log(sortGiftCode('pqksuvy'), 'kpqsuvy');
+console.log(sortGiftCode('zyxwvutsrqponmlkjihgfedcba'), 'abcdefghijklmnopqrstuvwxyz');
