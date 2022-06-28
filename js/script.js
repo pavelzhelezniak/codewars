847833f623b5c52e0319b7b16549b3aeaae7b8bb
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-28 19:27:06 +0300

    Simple remove duplicates (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8240,3 +8240,35 @@ console.log(automorphic(76), "Automorphic");
 console.log(automorphic(95), "Not!!");
 console.log(automorphic(625), "Automorphic");
 console.log(automorphic(225), "Not!!");
+
+// Simple remove duplicates (7 kyu)
+
+/* 
+Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+Expected output: [4, 6, 3]
+
+More examples can be found in the test cases.
+
+Good luck!
+*/
+
+const solve = (arr) => {
+	let newArr = arr.slice().filter((item, i) => arr.indexOf(item) !== i)
+	for (let i = 0; i < newArr.length; i++) {
+		arr.splice(arr.indexOf(newArr[i]), 1)
+	}
+	return arr
+}
+
+console.log(solve([3, 4, 4, 3, 6, 3]), [4, 6, 3]);
+console.log(solve([1, 2, 1, 2, 1, 2, 3]), [1, 2, 3]);
+console.log(solve([1, 2, 3, 4]), [1, 2, 3, 4]);
+console.log(solve([1, 1, 4, 5, 1, 2, 1]), [4, 5, 2, 1]);
+console.log(solve([1, 2, 1, 2, 1, 1, 3]), [2, 1, 3]);
