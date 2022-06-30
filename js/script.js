1795b7b72333e8739b8df57a4a2833b60051b362
pavelzhelezniak <zhele3o1992@gmail.com>
2022-06-30 21:00:34 +0300

    max diff - easy (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8272,3 +8272,27 @@ console.log(solve([1, 2, 1, 2, 1, 2, 3]), [1, 2, 3]);
 console.log(solve([1, 2, 3, 4]), [1, 2, 3, 4]);
 console.log(solve([1, 1, 4, 5, 1, 2, 1]), [4, 5, 2, 1]);
 console.log(solve([1, 2, 1, 2, 1, 1, 3]), [2, 1, 3]);
+
+// max diff - easy (7 kyu)
+
+/* 
+You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+lst contains integers, that means it may contain some negative numbers
+if lst is empty or contains a single element, return 0
+lst is not sorted
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+Have fun!
+*/
+
+const maxDiff = (list) => {
+	if (list.length < 2) return 0;
+	return Math.max(...list) - Math.min(...list);
+};
+
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 6]), 6);
+console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6]), 11);
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 16]), 16);
+console.log(maxDiff([16]), 0);
+console.log(maxDiff([]), 0);
