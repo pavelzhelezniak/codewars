34f5c02ca033a6b05d83e15d6644d8f650c4097d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-07-08 19:40:34 +0300

    Disarium Number (Special Numbers Series #3) (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8482,4 +8482,37 @@ const divCon = x => x.filter(item => typeof item === 'number').reduce((acc, item
 
 console.log(divCon([9, 3, '7', '3']), 2);
 console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 14);
-console.log(divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']), 13); 
+console.log(divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']), 13);
+
+// Disarium Number (Special Numbers Series #3) (7 kyu)
+
+/* 
+Definition
+Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+
+Task
+Given a number, Find if it is Disarium or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+Number passed is always Positive .
+Return the result as String
+Input >> Output Examples
+disariumNumber(89) ==> return "Disarium !!"
+Explanation:
+Since , 81 + 92 = 89 , thus output is "Disarium !!"
+disariumNumber(564) ==> return "Not !!"
+Explanation:
+Since , 51 + 62 + 43 = 105 != 564 , thus output is "Not !!"
+*/
+
+const disariumNumber = n => n === n.toString().split('').reduce((acc, item, index) => acc + parseInt(item, 10) ** (index + 1), 0) ?
+	"Disarium !!" : "Not !!";
+
+
+console.log(disariumNumber(89), "Disarium !!");
+console.log(disariumNumber(564), "Not !!");
+console.log(disariumNumber(1024), "Not !!");
+console.log(disariumNumber(135), "Disarium !!");
+console.log(disariumNumber(136586), "Not !!");
