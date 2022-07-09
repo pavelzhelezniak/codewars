e6353a061521a44d19fadfdfaaf420c0e36d7bb3
pavelzhelezniak <zhele3o1992@gmail.com>
2022-07-09 17:17:41 +0300

    Incrementer (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8516,3 +8516,27 @@ console.log(disariumNumber(564), "Not !!");
 console.log(disariumNumber(1024), "Not !!");
 console.log(disariumNumber(135), "Disarium !!");
 console.log(disariumNumber(136586), "Not !!");
+
+// Incrementer (7 kyu)
+
+/* 
+Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
+
+Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+
+Notes:
+return an empty array if your array is empty
+arrays will only contain numbers so don't worry about checking that
+Examples:
+[1, 2, 3]  -->  [2, 4, 6]   #  [1+1, 2+2, 3+3]
+
+[4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
+													#  9+3 = 12  -->  2
+*/
+
+const incrementer = num => num.map((item, index) => (item + index + 1) % 10);
+
+console.log(incrementer([]), []);
+console.log(incrementer([1, 2, 3]), [2, 4, 6]);
+console.log(incrementer([4, 6, 7, 1, 3]), [5, 8, 0, 5, 8]);
+console.log(incrementer([3, 6, 9, 8, 9]), [4, 8, 2, 2, 4]);
