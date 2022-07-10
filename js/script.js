b3c4359b3738c596b9757715b313cf9193aab2da
pavelzhelezniak <zhele3o1992@gmail.com>
2022-07-10 17:59:30 +0300

    Odd-Even String Sort (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8540,3 +8540,38 @@ console.log(incrementer([]), []);
 console.log(incrementer([1, 2, 3]), [2, 4, 6]);
 console.log(incrementer([4, 6, 7, 1, 3]), [5, 8, 0, 5, 8]);
 console.log(incrementer([3, 6, 9, 8, 9]), [4, 8, 2, 2, 4]);
+
+// Odd-Even String Sort (7 kyu)
+
+/* 
+Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+
+Note: 
+0 is considered to be an even index. 
+All input strings are valid with no spaces
+input: 'CodeWars'
+output 'CdWr oeas'
+
+S[0] = 'C'
+S[1] = 'o'
+S[2] = 'd'
+S[3] = 'e'
+S[4] = 'W'
+S[5] = 'a'
+S[6] = 'r'
+S[7] = 's'
+Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+And the final string to return is 'Cdwr oeas'
+
+Enjoy.
+*/
+
+const sortMyString = s => {
+	let even = s.split('').filter((v, i) => i % 2 === 0).join('')
+	let odd = s.split('').filter((v, i) => i % 2 !== 0).join('')
+	return even + ' ' + odd
+}
+
+console.log(sortMyString("CodeWars"), "CdWr oeas");
+console.log(sortMyString("YCOLUE'VREER"), "YOU'RE CLEVER");
