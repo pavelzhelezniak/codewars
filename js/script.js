1f8dc4c3eb1427b29349369af84d96a9c1de1dab
pavelzhelezniak <zhele3o1992@gmail.com>
2022-07-16 13:00:22 +0300

    V A P O R C O D E (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8668,3 +8668,23 @@ The character can be any alphanumeric character.
 const countChar = (string, char) => string.toLowerCase().split('').filter(item => item === char.toLowerCase()).length;
 
 console.log(countChar("fizzbuzz", "z"), 4);
+
+//	V A P O R C O D E (7 kyu)
+
+/* 
+ASC Week 1 Challenge 4 (Medium #1)
+
+Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, 
+and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+Note that spaces should be ignored in this case.
+
+Examples
+"Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+"Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
+*/
+
+const vaporcode = string => string.toUpperCase().split('').filter(item => item !== ' ').join('  ');
+
+console.log(vaporcode("Lets go to the movies"), "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S");
+console.log(vaporcode("Why isnt my code working"), "W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G");
