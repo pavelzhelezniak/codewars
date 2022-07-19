65df1af3a33ce281fac63bf1dfa0e37919092273
pavelzhelezniak <zhele3o1992@gmail.com>
2022-07-19 16:44:20 +0300

    Filter the number (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8744,3 +8744,22 @@ const differenceOfSquares = n => {
 console.log(differenceOfSquares(5), 170);
 console.log(differenceOfSquares(10), 2640);
 console.log(differenceOfSquares(100), 25164150);
+
+// Filter the number (7 kyu)
+
+/* 
+Filter the number
+Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+Task
+Your task is to return a number from a string.
+
+Details
+You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+*/
+
+const FilterString = value => parseInt(value.match(/\d+/g).join(''), 10);
+
+console.log(FilterString("123"), 123, 'Just return the numbers');
+console.log(FilterString("a1b2c3"), 123, 'Just return the numbers');
+console.log(FilterString("aa1bb2cc3dd"), 123, 'Just return the numbers');
