942b57d8a92ea76cb6ae306aeecdcff68e032c69
pavelzhelezniak <zhele3o1992@gmail.com>
2022-07-28 18:02:05 +0300

    Evens and Odds (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8877,3 +8877,22 @@ const explode = s => s.split('').map(item => item.repeat(Number(item))).join('')
 
 console.log(explode('12345'), '122333444455555');
 console.log(explode('1347'), '133344447777777');
+
+// Evens and Odds (7 kyu)
+
+/* 
+This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased.
+*/
+
+const evensAndOdds = num => num % 2 === 0 ? num.toString(2) : num.toString(16);
+
+console.log(evensAndOdds(2), '10');
+console.log(evensAndOdds(13), 'd');
+console.log(evensAndOdds(47), '2f');
+console.log(evensAndOdds(0), '0');
+console.log(evensAndOdds(12800), '11001000000000');
+console.log(evensAndOdds(8172381723), '1e71ca61b')
