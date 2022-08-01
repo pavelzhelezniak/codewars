4cbacb33a56820691ff955970ddc59024165d33e
pavelzhelezniak <zhele3o1992@gmail.com>
2022-08-01 16:24:10 +0300

    The old switcheroo (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8896,3 +8896,32 @@ console.log(evensAndOdds(47), '2f');
 console.log(evensAndOdds(0), '0');
 console.log(evensAndOdds(12800), '11001000000000');
 console.log(evensAndOdds(8172381723), '1e71ca61b')
+
+// The old switcheroo (7 kyu)
+
+/* 
+Write a function
+
+vowel2index(str)
+that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+E.g:
+
+vowel2index('this is my string') == 'th3s 6s my str15ng'
+vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+vowel2index('') == ''
+Your function should be case insensitive to the vowels.
+*/
+
+const vowel2index = str => str.split('').map((item, i) => {
+	if (item === 'a' || item === 'e' || item === 'i' || item === 'o' || item === 'u' || item === 'A' || item === 'E' || item === 'I' || item === 'O' || item === 'U') {
+		return ++i;
+	}
+	return item;
+
+	// return str.replace(/[aeiou]/ig, (m, i) => i + 1);
+}).join('');
+
+console.log(vowel2index('this is my string'), 'th3s 6s my str15ng');
+console.log(vowel2index('Codewars is the best site in the world'), 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld');
+console.log(vowel2index('Tomorrow is going to be raining'), 'T2m4rr7w 10s g1415ng t20 b23 r2627n29ng');
+console.log(vowel2index(''), '');
