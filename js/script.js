08f8872b5acf3a883d2a085c94476a8ec8cd56d2
pavelzhelezniak <zhele3o1992@gmail.com>
2022-08-03 17:46:42 +0300

    Smallest value of an array (7 kyu)

--- js/script.js
+++ js/script.js
@@ -8948,3 +8948,29 @@ const generateIntegers = (m, n) => {
 }
 
 console.log(generateIntegers(2, 5), [2, 3, 4, 5]);
+
+// Smallest value of an array (7 kyu)
+
+/* 
+Write a function that can return the smallest value of an array or the index of that value. 
+The function's 2nd parameter will tell whether it should return the value or the index.
+
+Assume the first parameter will always be an array filled with at least 1 number and no duplicates. 
+Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+min([1,2,3,4,5], 'value') // => 1
+min([1,2,3,4,5], 'index') // => 0
+*/
+
+const min = (arr, toReturn) => {
+	const minVal = Math.min(...arr);
+	switch (toReturn) {
+		case 'value':
+			return minVal;
+		case 'index':
+			return arr.indexOf(minVal)
+	}
+}
+
+console.log(min([1, 2, 3, 4, 5], 'value'), 1);
+console.log(min([1, 2, 3, 4, 5], 'index'), 0);
