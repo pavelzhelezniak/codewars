7fcf6188ff1a5a21253ca95216bbaa869360e095
pavelzhelezniak <zhele3o1992@gmail.com>
2022-08-07 17:45:20 +0300

    Find the lucky numbers (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9027,3 +9027,19 @@ console.log(sumOfIntegersInString("2 + 3 = "), 5);
 console.log(sumOfIntegersInString("The Great Depression lasted from 1929 to 1939."), 3868);
 console.log(sumOfIntegersInString("C4t5 are 4m4z1ng."), 18);
 
+// Find the lucky numbers (7 kyu)
+
+/* 
+Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
+
+For example,
+
+ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
+[7,70,17]
+Don't worry about bad input, you will always receive a finite list of integers.
+*/
+
+const filterLucky = x => x.filter(item => item.toString().split('').includes('7'));
+
+console.log(filterLucky([1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17]), [7, 70, 17]);
+console.log(filterLucky([71, 9907, 69]), [71, 9907]);
