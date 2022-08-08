f538f05f55f312464139000ca6d7db5da480f600
pavelzhelezniak <zhele3o1992@gmail.com>
2022-08-08 17:51:26 +0300

    Inspiring Strings (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9043,3 +9043,30 @@ const filterLucky = x => x.filter(item => item.toString().split('').includes('7'
 
 console.log(filterLucky([1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17]), [7, 70, 17]);
 console.log(filterLucky([71, 9907, 69]), [71, 9907]);
+
+// Inspiring Strings (7 kyu)
+
+/* 
+When given a string of space separated words, return the word with the longest length. 
+If there are multiple words with the longest length, return the last instance of the word with the longest length.
+
+Example:
+
+'red white blue' //returns string value of white
+
+'red blue gold' //returns gold
+*/
+
+const longestWord = stringOfWords => {
+	let maxLengthStr = '';
+	stringOfWords.split(' ').forEach(item => {
+		if (item.length >= maxLengthStr.length) {
+			maxLengthStr = item;
+		}
+	});
+	return maxLengthStr;
+}
+
+console.log(longestWord('a b c d e fgh'), "fgh");
+console.log(longestWord('one two three'), "three");
+console.log(longestWord('red blue grey'), "grey");
