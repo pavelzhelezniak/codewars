4c2acf4d868cf0de2676de8975fa8af85e9f710f
pavelzhelezniak <zhele3o1992@gmail.com>
2022-08-16 13:45:49 +0300

    Say hello! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9186,3 +9186,24 @@ console.log(lastSurvivor('zbk', [2, 1]), 'z');
 console.log(lastSurvivor('c', []), 'c');
 console.log(lastSurvivor('foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr',
 	[8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23, 56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55, 24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39, 36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7, 0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1]), 'd');
+
+
+// Say hello! (7 kyu)
+
+/* 
+Say hello!
+
+Write a function to greet a person. Function will take name as input and greet the person by saying hello. 
+Return null/nil/None if input is empty string or null/nil/None.
+
+Example:
+
+greet("Niks") === "hello Niks!";
+greet("") === null; // Return null if input is empty string
+greet(null) === null; // Return null if input is null
+*/
+
+const greet = name => name ? `hello ${name}!` : null;
+
+console.log(greet("Niks"), "hello Niks!", "try again");
+console.log(greet(null), null, "try again");
