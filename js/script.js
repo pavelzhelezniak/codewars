0f2b4dd1f303b9cbc74d6aee77fb0bc6494eb751
pavelzhelezniak <zhele3o1992@gmail.com>
2022-08-19 12:56:21 +0300

    Sum of numerous arguments (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9258,3 +9258,25 @@ const palindrome = num => {
 	return 'Not valid';
 
 }
+
+// Sum of numerous arguments (7 kyu)
+
+/* 
+After calling the function findSum() with any number of non-negative integer arguments, it should return the sum of all those arguments. If no arguments are given, the function should return 0, if negative arguments are given, it should return -1.
+
+Example
+findSum(1,2,3,4); // returns 10 
+findSum(1,-2);    // returns -1 
+findSum();        // returns 0 
+*/
+
+function findSum() {
+	const arr = [...arguments].sort((a, b) => a - b);
+	return arr[0] < 0 ? -1 : arr.reduce((acc, item) => acc + item, 0)
+}
+
+console.log(findSum(1, 3, 5), 9, "1 + 3 + 5 = 9");
+console.log(findSum(0, 3, 9, 2), 14, "0+3+9+2 = 14");
+console.log(findSum(), 0, "If no arguments, function should return 0");
+console.log(findSum(1, -2, 4), -1, "If negative arguments are passed, function should return -1");
+console.log(findSum(0), 0, "The sum of 0 is 0");
