715fea870fd35d2c87dd290e6cc801623eff9f82
pavelzhelezniak <zhele3o1992@gmail.com>
2022-08-25 13:33:08 +0300

    Find all occurrences of an element in an array (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9379,3 +9379,29 @@ console.log(summy("1 2 3 4"), 10);
 console.log(summy("1 2 3 4 5"), 15);
 console.log(summy("10 10"), 20);
 console.log(summy("0 0"), 0);
+
+
+// Find all occurrences of an element in an array (7 kyu)
+
+/* 
+Given an array (a list in Python) of integers and an integer n, find all occurrences of n 
+in the given array and return another array containing all the index positions of n in the given array.
+
+If n is not in the given array, return an empty array [].
+
+Assume that n and all values in the given array will always be integers.
+
+Example:
+
+findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+*/
+
+const findAll = (array, n) => {
+	const arrFind = [];
+	array.map((item, index) => item === n ? arrFind.push(index) : null);
+	return arrFind;
+}
+
+console.log(findAll([6, 9, 3, 4, 3, 82, 11], 3), [2, 4]);
+console.log(findAll([10, 16, 20, 6, 14, 11, 20, 2, 17, 16, 14], 16), [1, 9]);
+console.log(findAll([20, 20, 10, 13, 15, 2, 7, 2, 20, 3, 18, 2, 3, 2, 16, 10, 9, 9, 7, 5, 15, 5], 20), [0, 1, 8]);
