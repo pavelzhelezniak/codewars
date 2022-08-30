af8b7d5425125bb6cbab30e913b9d5dcc9fdf208
pavelzhelezniak <zhele3o1992@gmail.com>
2022-08-30 18:14:12 +0300

    Formatting decimal places #1 (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9545,3 +9545,26 @@ console.log(bandNameGenerator('knife'), 'The Knife');
 console.log(bandNameGenerator('tart'), 'Tartart');
 console.log(bandNameGenerator('sandles'), 'Sandlesandles');
 console.log(bandNameGenerator('bed'), 'The Bed');
+
+// Formatting decimal places #1 (7 kyu)
+
+/* 
+Each floating-point number should be formatted that only the first two decimal places are returned. 
+You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Don't round the numbers! Just cut them after two decimal places!
+
+Right examples:  
+32.8493 is 32.84  
+14.3286 is 14.32
+
+Incorrect examples (e.g. if you round the numbers):  
+32.8493 is 32.85  
+14.3286 is 14.33
+*/
+
+const twoDecimalPlaces = number => parseInt(number * 100) / 100;
+
+console.log(twoDecimalPlaces(10.1289767789), 10.12, "didn't work for 10.1289767789");
+console.log(twoDecimalPlaces(-7488.83485834983), -7488.83, "didn't work for -7488.83485834983");
+console.log(twoDecimalPlaces(4.653725356), 4.65, "didn't work for 4.653725356");
