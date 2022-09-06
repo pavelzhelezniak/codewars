7b70ca16778a442d96a39cab70c7998df93a1a22
pavelzhelezniak <zhele3o1992@gmail.com>
2022-09-06 15:44:07 +0300

    Covfefe (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9619,3 +9619,25 @@ console.log(arr.numberOfOccurrences(0), 1);
 console.log(arr.numberOfOccurrences(4), 0);
 console.log(arr.numberOfOccurrences(2), 2);
 console.log(arr.numberOfOccurrences("a"), 0);
+
+// Covfefe (7 kyu)
+
+/* 
+Your are given a string. You must replace any occurence of the sequence coverage by covfefe, however, 
+if you don't find the word coverage in the string, you must add covfefe at the end of the string with a leading space.
+
+For the languages where the string is not immutable (such as ruby), don't modify the given string, otherwise this will break the test cases.
+*/
+
+const covfefe = str => {
+	if (str.includes("coverage")) {
+		return str.replace(/coverage/g, "covfefe");
+	}
+	return str + " covfefe";
+}
+
+console.log(covfefe("coverage"), "covfefe");
+console.log(covfefe("coverage coverage"), "covfefe covfefe");
+console.log(covfefe("nothing"), "nothing covfefe");
+console.log(covfefe("double space "), "double space  covfefe");
+console.log(covfefe("covfefe"), "covfefe covfefe");
