1b41ac060134eba07e11047452224fff34c10a45
pavelzhelezniak <zhele3o1992@gmail.com>
2022-09-08 08:26:26 +0300

    Refactored Greeting (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9641,3 +9641,34 @@ console.log(covfefe("coverage coverage"), "covfefe covfefe");
 console.log(covfefe("nothing"), "nothing covfefe");
 console.log(covfefe("double space "), "double space  covfefe");
 console.log(covfefe("covfefe"), "covfefe covfefe");
+
+// Refactored Greeting (7 kyu)
+
+/* 
+The following code could use a bit of object-oriented artistry. 
+While it's a simple method and works just fine as it is, in a larger system it's best to organize methods into classes/objects. 
+(Or, at least, something similar depending on your language)
+
+Refactor the following code so that it belongs to a Person class/object. 
+Each Person instance will have a greet method. The Person instance should be instantiated with 
+a name so that it no longer has to be passed into each greet method call.
+
+Here is how the final refactored code would be used:
+
+var joe = new Person('Joe');
+joe.greet('Kate'); // should return 'Hello Kate, my name is Joe'
+joe.name           // should == 'Joe'
+*/
+
+class Person {
+	constructor(name) {
+		this.name = name
+	}
+
+	greet(yourName) {
+		return `Hello ${yourName}, my name is ${this.name}`
+	}
+}
+
+const joe = new Person('Joe');
+console.log(joe.greet('Kate'), 'Hello Kate, my name is Joe');
