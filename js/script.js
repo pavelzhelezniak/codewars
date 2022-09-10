418a352435d018c391bb9b160c252c59a875381e
pavelzhelezniak <zhele3o1992@gmail.com>
2022-09-10 14:55:23 +0300

    Who's Online? (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9672,3 +9672,49 @@ class Person {
 
 const joe = new Person('Joe');
 console.log(joe.greet('Kate'), 'Hello Kate, my name is Joe');
+
+// Who's Online? (7 kyu)
+
+/* 
+	 You have a group chat application, but who is online!?
+	 You want to show your users which of their friends are online and available to chat!
+	 Given an input of an array of objects containing usernames, status and time since last activity (in mins), create a function to work out who is online, offline and away.
+	 If someone is online but their lastActivity was more than 10 minutes ago they are to be considered away.
+	 The input data has the following structure:
+	 [{
+		username: 'David',
+		status: 'online',
+		lastActivity: 10
+	 }, {
+		username: 'Lucy',
+		status: 'offline',
+		lastActivity: 22
+	 }, {
+		username: 'Bob',
+		status: 'online',
+		lastActivity: 104
+	 }]
+	 username will always be a string, status will always be either 'online' or 'offline' (UserStatus enum in C#) and lastActivity will always be number >= 0.
+	 Finally, if you have no friends in your chat application, the input will be an empty array [].
+	 In this case you should return an empty object {} (empty Dictionary in C#).
+*/
+
+const whosOnline = friends =>
+	friends.reduce((activities, { username, status, lastActivity }) => {
+		const realStatus = status === 'online' && lastActivity > 10 ? 'away' : status
+		return { ...activities, [realStatus]: activities[realStatus] ? [...activities.realStatus, username] : [username] }
+	}, {})
+
+console.log(whosOnline([{
+	username: 'David',
+	status: 'online',
+	lastActivity: 10
+}, {
+	username: 'Lucy',
+	status: 'offline',
+	lastActivity: 22
+}, {
+	username: 'Bob',
+	status: 'online',
+	lastActivity: 104
+}]))
