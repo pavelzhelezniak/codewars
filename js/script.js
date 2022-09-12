669a95785d25610099d21fde60772e6bf8fa2c71
pavelzhelezniak <zhele3o1992@gmail.com>
2022-09-12 17:31:16 +0300

    Flatten (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9718,3 +9718,23 @@ console.log(whosOnline([{
 	status: 'online',
 	lastActivity: 104
 }]))
+
+// Flatten (7 kyu)
+
+/* 
+Write a function that flattens an Array of Array objects into a flat Array. 
+Your function must only do one level of flattening.
+
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]
+*/
+
+const flatten = array => [].concat(...array);
+// const flatten = array => array.flat();
+
+console.log(flatten([]), []);
+console.log(flatten([1, 2, 3]), [1, 2, 3]);
+console.log(flatten([[1, 2, 3], ["a", "b", "c"], [1, 2, 3]]), [1, 2, 3, "a", "b", "c", 1, 2, 3]);
+console.log(flatten([[3, 4, 5], [[9, 9, 9]], ["a,b,c"]]), [3, 4, 5, [9, 9, 9], "a,b,c"]);
+console.log(flatten([[[3], [4], [5]], [9], [9], [8], [[1, 2, 3]]]), [[3], [4], [5], 9, 9, 8, [1, 2, 3]]);
