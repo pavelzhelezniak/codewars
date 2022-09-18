316a8661f2922b50d11385fa363f388750470e0d
pavelzhelezniak <zhele3o1992@gmail.com>
2022-09-18 11:13:50 +0300

    Longest vowel chain (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9751,3 +9751,22 @@ console.log(args_count(1, 2), 2);
 console.log(args_count(), 0);
 console.log(args_count('A', 'B', 'C'), 3);
 console.log(args_count(["foo", "bar"]), 1);
+
+// Longest vowel chain (7 kyu)
+
+/*
+The vowel substrings in the word codewarriors are o,e,a,io. 
+The longest of these has a length of 2. 
+Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. 
+Vowels are any of aeiou.
+*/
+
+const solve = x => Math.max(...x.split(/[^aoeiu]/i).map(x => x.length))
+
+console.log(solve("codewarriors"), 2);
+console.log(solve("suoidea"), 3);
+console.log(solve("ultrarevolutionariees"), 3);
+console.log(solve("strengthlessnesses"), 1);
+console.log(solve("cuboideonavicuare"), 2);
+console.log(solve("chrononhotonthuooaos"), 5);
+console.log(solve("iiihoovaeaaaoougjyaw"), 8);
