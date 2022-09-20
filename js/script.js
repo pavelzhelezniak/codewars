3cb515b29b48cc37ff0e9b077a43d03383b4ad54
pavelzhelezniak <zhele3o1992@gmail.com>
2022-09-20 22:16:19 +0300

    Clean up after your dog (7 kyu)

--- js/script.js
+++ js/script.js
@@ -9788,3 +9788,46 @@ const catMouse = x => x.replace(/[\C\m]/gi, '').length < 4 ? "Caught!" : "Escape
 console.log(catMouse('C....m'), "Escaped!");
 console.log(catMouse('C..m'), "Caught!");
 console.log(catMouse('C.....m'), "Escaped!");
+
+// Clean up after your dog (7 kyu)
+
+/* 
+You have stumbled across the divine pleasure that is owning a dog and a garden. Now time to pick up all the cr@p! :D
+
+Given a 2D array to represent your garden, you must find and collect all of the dog cr@p - represented by '@'.
+
+You will also be given the number of bags you have access to (bags), and the capactity of a bag (cap). 
+If there are no bags then you can't pick anything up, so you can ignore cap.
+
+You need to find out if you have enough capacity to collect all the cr@p and make your garden clean again.
+
+If you do, return 'Clean', else return 'Cr@p'.
+
+Watch out though - if your dog is out there ('D'), he gets very touchy about being watched. If he is there you need to return 'Dog!!'.
+
+For example:
+
+x=
+[[_,_,_,_,_,_]
+[_,_,_,_,@,_]
+[@,_,_,_,_,_]]
+
+bags = 2, cap = 2
+
+return --> 'Clean'
+*/
+
+const crap = (x, bags, cap) => {
+	const totalCapacity = bags * cap;
+	const gardenElements = x.join('');
+
+	if (gardenElements.includes('D')) {
+		return 'Dog!!';
+	}
+
+	return (gardenElements.match(/@/g) || []).length <= totalCapacity ? 'Clean' : 'Cr@p';
+}
+
+console.log(crap([['_', '_', '_', '_'], ['_', '_', '_', '@'], ['_', '_', '@', '_']], 2, 2), "Clean");
+console.log(crap([['_', '_', '_', '_'], ['_', '_', '_', '@'], ['_', '_', '@', '_']], 1, 1), "Cr@p");
+console.log(crap([['_', '_'], ['_', '@'], ['D', '_']], 2, 2), "Dog!!");
