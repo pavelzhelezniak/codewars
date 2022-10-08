10fa50540e853353f5f217fda669d736582c3562
pavelzhelezniak <zhele3o1992@gmail.com>
2022-10-08 20:29:29 +0300

    Consecutive items (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10045,3 +10045,19 @@ console.log(compare(null, "BC"), false);
 console.log(compare(null, null), true);
 console.log(compare(null, ""), true);
 console.log(compare("", ""), true);
+
+// Consecutive items (7 kyu)
+
+/* 
+You are given a list of unique integers arr, and two integers a and b. 
+Your task is to find out whether or not a and b appear consecutively in arr, 
+and return a boolean value (True if a and b are consecutive, False otherwise).
+
+It is guaranteed that a and b are both present in arr.
+*/
+
+const consecutive = (arr, a, b) => Math.abs(arr.indexOf(a) - arr.indexOf(b)) === 1;
+
+console.log(consecutive([1, 3, 5, 7], 3, 7), false);
+console.log(consecutive([1, 3, 5, 7], 3, 1), true);
+console.log(consecutive([1, 6, 9, -3, 4, -78, 0], -3, 4), true);
