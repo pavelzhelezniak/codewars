c56936df1d86377ebb4e489062d7e75e17b0ed3b
pavelzhelezniak <zhele3o1992@gmail.com>
2022-10-16 21:22:10 +0300

    Alphabet symmetry (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10080,3 +10080,29 @@ l = new Array()
 const integer_list = [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8];
 const values_list = [1, 3, 4, 2];
 console.log(l.remove_(integer_list, values_list), [5, 6, 7, 8]);
+
+// Alphabet symmetry (7 kyu)
+
+/* 
+Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. 
+Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+See test cases for more examples.
+
+Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+
+Good luck!
+*/
+
+const solveAlphabet = arr => {
+	const alphabeth = "abcdefghijklmnopqrstuvwxyz";
+	return arr.map(word => word.toLowerCase().split('').filter((letter, i) => i === alphabeth.indexOf(letter)).length);
+};
+
+console.log(solveAlphabet(["abode", "ABc", "xyzD"]), [4, 3, 1]);
+console.log(solveAlphabet(["abide", "ABc", "xyz"]), [4, 3, 0]);
+console.log(solveAlphabet(["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"]), [6, 5, 7]);
+console.log(solveAlphabet(["encode", "abc", "xyzD", "ABmD"]), [1, 3, 1, 3]);
