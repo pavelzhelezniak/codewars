24eade66349c1ed61b4a63b8c5f9d77b25dc8a55
pavelzhelezniak <zhele3o1992@gmail.com>
2022-10-29 09:02:58 +0300

    Sum of Odd Cubed Numbers (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10179,3 +10179,19 @@ console.log(sumCubes(1), 1);
 console.log(sumCubes(2), 9);
 console.log(sumCubes(3), 36);
 console.log(sumCubes(4), 100);
+
+// Sum of Odd Cubed Numbers (7 kyu)
+
+/* 
+Find the sum of the odd numbers within an array, after cubing the initial integers. 
+The function should return undefined if any of the values aren't numbers.
+*/
+
+const cubeOdd = arr => arr.length === arr.filter(item => typeof item === 'number').length ?
+	arr.filter(item => item % 2 !== 0).reduce((acc, item) => acc += item ** 3, 0) :
+	undefined
+
+
+console.log(cubeOdd([1, 2, 3, 4]), 28);
+console.log(cubeOdd([-3, -2, 2, 3]), 0);
+console.log(cubeOdd(["a", 12, 9, "z", 42]), undefined);
