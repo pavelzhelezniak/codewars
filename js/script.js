d13666beaf1bef73157ca00f240880a653320ef9
pavelzhelezniak <zhele3o1992@gmail.com>
2022-11-13 14:33:14 +0300

    Find the vowels (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10231,3 +10231,34 @@ console.log(calculate(-3, "-", 0), -3, "-3 - 0 = -3");
 console.log(calculate(-3, "/", 0), null, "-3 / 0 = null");
 console.log(calculate(-3, "*", 0), 0, "-3 * 0 = 0");
 console.log(calculate(-3, "w", 0), null, "-3 w 0 = null");
+
+// Find the vowels (7 kyu)
+
+/* 
+We want to know the index of the vowels in a given word, for example, 
+there are two vowels in the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+Vowels in this context refers to: a e i o u y (including upper case)
+This is indexed from [1..n] (not zero indexed!)
+*/
+
+const vowelIndices = word => {
+	const vowels = 'aeoiuy';
+	const indicate = [];
+	word.toLowerCase().split('').forEach((item, i) => { if (vowels.includes(item)) { indicate.push(i + 1) } });
+	return indicate;
+}
+
+console.log(vowelIndices("mmm"), []);
+console.log(vowelIndices("apple"), [1, 5]);
+console.log(vowelIndices("super"), [2, 4]);
+console.log(vowelIndices("orange"), [1, 3, 6]);
+console.log(vowelIndices("supercalifragilisticexpialidocious"), [2, 4, 7, 9, 12, 14, 16, 19, 21, 24, 25, 27, 29, 31, 32, 33]);
