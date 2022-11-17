ff49e112f3d6cdc7858c3ac28d357e5c175254fd
pavelzhelezniak <zhele3o1992@gmail.com>
2022-11-17 22:48:25 +0300

    Sum of numbers from 0 to N (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10323,3 +10323,52 @@ console.log(palindromeChainLength(88), 0);
 console.log(palindromeChainLength(87), 4);
 console.log(palindromeChainLength(89), 24);
 console.log(palindromeChainLength(10), 1);
+
+// Sum of numbers from 0 to N (7 kyu)
+
+/* 
+Description:
+
+We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+Example:
+Input:
+
+> 6
+Output:
+
+0+1+2+3+4+5+6 = 21
+
+Input:
+
+> -15
+Output:
+
+-15<0
+
+Input:
+
+> 0
+Output:
+
+0=0
+*/
+
+let SequenceSum = (function () {
+	function SequenceSum() { }
+
+	SequenceSum.showSequence = function (count) {
+		let arr = [];
+		for (let i = 0; i <= count; i++) { arr.push(i) };
+		let b = arr.reduce((total, next) => total + next, 0)
+		let a = arr.join('+');
+		if (count < 0) return `${count}<0`;
+		if (count == 0) return `${count}=0`;
+		return `${a} = ${b}`
+	};
+
+	return SequenceSum;
+
+})();
+
+console.log(SequenceSum.showSequence(6), "0+1+2+3+4+5+6 = 21");
