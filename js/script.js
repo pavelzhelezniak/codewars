0e0956369abafdc4dd26f1becfb5d62d346bca5f
pavelzhelezniak <zhele3o1992@gmail.com>
2022-11-26 11:47:40 +0300

    Nth Smallest Element (Array Series #4) (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10418,3 +10418,25 @@ const getFirstPython = list => {
 }
 
 console.log(getFirstPython(list12), 'Victoria, Puerto Rico');
+
+
+// Nth Smallest Element (Array Series #4) (7 kyu)
+
+/* 
+Task
+Given an array/list of integers, find the Nth smallest element in the array.
+
+Notes
+Array/list size is at least 3.
+Array/list's numbers could be a mixture of positives , negatives and zeros.
+Repetition in array/list's numbers could occur, so don't remove duplications.
+*/
+
+const nthSmallest = (arr, pos) => arr.sort((a, b) => a - b)[pos - 1];
+
+console.log(nthSmallest([3, 1, 2], 2), 2);
+console.log(nthSmallest([15, 20, 7, 10, 4, 3], 3), 7);
+console.log(nthSmallest([-5, -1, -6, -18], 4), -1);
+console.log(nthSmallest([-102, -16, -1, -2, -367, -9], 5), -2);
+console.log(nthSmallest([2, 169, 13, -5, 0, -1], 4), 2);
+console.log(nthSmallest([2, 1, 3, 3, 1, 2], 3), 2);
