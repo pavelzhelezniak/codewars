d9a73a8f430638996fe614a237937208fb2bf704
pavelzhelezniak <zhele3o1992@gmail.com>
2022-11-29 17:30:05 +0300

    Numbers to Letters (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10476,4 +10476,57 @@ const alternateCase = (s) => s.split('')
 
 console.log(alternateCase("abc"), "ABC");
 console.log(alternateCase("ABC"), "abc");
-console.log(alternateCase("Hello World"), "hELLO wORLD"); 
+console.log(alternateCase("Hello World"), "hELLO wORLD");
+
+// Numbers to Letters (7 kyu)
+
+/* 
+Given an array of numbers (in string format), you must return a string. 
+The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. 
+You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+All inputs will be valid.
+*/
+
+const switcher = (x) => {
+	let word = '';
+	let alphabet = {
+		29: ' ',
+		28: '?',
+		27: '!',
+		26: 'a',
+		25: 'b',
+		24: 'c',
+		23: 'd',
+		22: 'e',
+		21: 'f',
+		20: 'g',
+		19: 'h',
+		18: 'i',
+		17: 'j',
+		16: 'k',
+		15: 'l',
+		14: 'm',
+		13: 'n',
+		12: 'o',
+		11: 'p',
+		10: 'q',
+		9: 'r',
+		8: 's',
+		7: 't',
+		6: 'u',
+		5: 'v',
+		4: 'w',
+		3: 'x',
+		2: 'y',
+		1: 'z',
+	};
+	for (let i = 0; i < x.length; i++) {
+		word += alphabet[x[i]];
+	}
+	return word;
+}
+
+console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8']), 'codewars');
+console.log(switcher(['25', '7', '8', '4', '14', '23', '8', '25', '23', '29', '16', '16', '4']), 'btswmdsbd kkw');
+console.log(switcher(['4', '24']), 'wc'); 
