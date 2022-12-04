1f8cd30feecc746f628b01787b7446f49464dd55
pavelzhelezniak <zhele3o1992@gmail.com>
2022-12-04 19:47:16 +0300

    String Merge! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10619,3 +10619,25 @@ console.log(validSpacing(' Hello world'), false);
 console.log(validSpacing('Hello  world '), false);
 console.log(validSpacing('Hello'), true);
 console.log(validSpacing('Helloworld'), true);
+
+// String Merge! (7 kyu)
+
+/* 
+Given two words and a letter, return a single word that's a combination of both words, 
+merged at the point where the given letter first appears in each word. 
+The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. 
+You can assume both words will contain the dividing letter.
+*/
+
+const stringMerge = (string1, string2, letter) => {
+	const index1 = string1.indexOf(letter);
+	const index2 = string2.indexOf(letter);
+	return string1.slice(0, index1) + string2.slice(index2)
+}
+
+console.log(stringMerge("person", "here", "e"), "pere");
+console.log(stringMerge("apowiejfoiajsf", "iwahfeijouh", "j"), "apowiejouh");
+console.log(stringMerge("abcdefxxxyzz", "abcxxxyyyxyzz", "x"), "abcdefxxxyyyxyzz");
+console.log(stringMerge("12345654321", "123456789", "6"), "123456789");
+console.log(stringMerge("JiOdIdA4", "oopopopoodddasdfdfsd", "d"), "JiOdddasdfdfsd");
+console.log(stringMerge("incredible", "people", "e"), "increople");
