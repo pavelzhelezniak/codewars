3774734582f99961190747b16d176b860aa5872b
pavelzhelezniak <zhele3o1992@gmail.com>
2022-12-14 14:04:39 +0300

    Double Sort (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10763,3 +10763,32 @@ const checkThreeAndTwo = array => {
 console.log(checkThreeAndTwo(["a", "a", "a", "b", "b"]), true);
 console.log(checkThreeAndTwo(["a", "c", "a", "c", "b"]), false);
 console.log(checkThreeAndTwo(["a", "a", "a", "a", "a"]), false);
+
+// Double Sort (7 kyu)
+
+/* 
+Simple enough this one - you will be given an array. 
+The values in the array will either be numbers or strings, or a mix of both. 
+You will not get an empty array, nor a sparse one.
+
+Your job is to return a single array that has first the numbers sorted in ascending order, 
+followed by the strings sorted in alphabetic order. The values must maintain their original type.
+
+Note that numbers written as strings are strings and must be sorted with the other strings.
+*/
+
+const dbSort = a => {
+	const num = [];
+	const str = [];
+	a.forEach(item => {
+		typeof item === 'number' ? num.push(item) : str.push(item);
+	})
+	return num.sort((a, b) => a - b).concat(str.sort());
+}
+
+console.log(dbSort([6, 2, 3, 4, 5]), [2, 3, 4, 5, 6]);
+console.log(dbSort([14, 32, 3, 5, 5]), [3, 5, 5, 14, 32]);
+console.log(dbSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
+console.log(dbSort(["Banana", "Orange", "Apple", "Mango", 0, 2, 2]), [0, 2, 2, "Apple", "Banana", "Mango", "Orange"]);
+console.log(dbSort(["C", "W", "W", "W", 1, 2, 0]), [0, 1, 2, "C", "W", "W", "W"]);
+console.log(dbSort(["Apple", 46, "287", 574, "Peach", "3", "69", 78, "Grape", "423"]), [46, 78, 574, '287', '3', '423', '69', 'Apple', 'Grape', 'Peach'])
