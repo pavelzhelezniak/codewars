831fa706282ab52be808bb5cc73ec5a5529d2e61
pavelzhelezniak <zhele3o1992@gmail.com>
2022-12-17 16:22:36 +0300

    Find all pairs (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10814,4 +10814,36 @@ questions.forEach(item => {
 	item.usersAnswer = null
 });
 
-console.log(questions[0].usersAnswer === null);  
+console.log(questions[0].usersAnswer === null);
+
+// Find all pairs (7 kyu)
+
+/* 
+You are given array of integers, your task will be to count all pairs in that array and return their count.
+
+Notes:
+
+Array can be empty or contain only one value; in this case return 0
+If there are more pairs of a certain number, count each pair only once. E.g.: for [0, 0, 0, 0] the return value is 2 (= 2 pairs of 0s)
+Random tests: maximum array length is 1000, range of values in array is between 0 and 1000
+*/
+
+const duplicates = array => {
+	array = [...array].sort((a, b) => a - b);
+	let count = 0;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === array[i + 1]) {
+			count++;
+			array[i] = '';
+			array[i + 1] = '';
+		}
+	}
+	return count;
+}
+
+console.log(duplicates([1, 2, 5, 6, 5, 2]), 2);
+console.log(duplicates([1, 2, 2, 20, 6, 20, 2, 6, 2]), 4);
+console.log(duplicates([0, 0, 0, 0, 0, 0, 0]), 3);
+console.log(duplicates([1000, 1000]), 1);
+console.log(duplicates([]), 0);
+console.log(duplicates([54]), 0);
