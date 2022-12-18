a0df7c2e7492fee74626318c6f757d443e0b5c7b
pavelzhelezniak <zhele3o1992@gmail.com>
2022-12-18 15:27:39 +0300

    Find how many times did a team from a given country win the Champions League? (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10847,3 +10847,53 @@ console.log(duplicates([0, 0, 0, 0, 0, 0, 0]), 3);
 console.log(duplicates([1000, 1000]), 1);
 console.log(duplicates([]), 0);
 console.log(duplicates([54]), 0);
+
+// Find how many times did a team from a given country win the Champions League? (7 kyu)
+
+/* 
+Create a function that takes two arguments:
+
+An array of objects which feature the season, the team and the country of the Champions League winner.
+
+Country (as a string, for example, 'Portugal')
+
+You function should then return the number which represents the number of times a team from a given country has won. 
+Return 0 if there have been no wins.
+*/
+
+const winnerList1 = [
+	{ season: '1996–97', team: 'Borussia Dortmund', country: 'Germany' },
+	{ season: '1997–98', team: 'Real Madrid', country: 'Spain' },
+	{ season: '1998–99', team: 'Manchester United', country: 'England' },
+	{ season: '1999–00', team: 'Real Madrid', country: 'Spain' },
+	{ season: '2000–01', team: 'Bayern Munich', country: 'Germany' },
+	{ season: '2001–02', team: 'Real Madrid', country: 'Spain' },
+	{ season: '2002–03', team: 'Milan', country: 'Italy' },
+	{ season: '2003–04', team: 'Porto', country: 'Portugal' },
+	{ season: '2004–05', team: 'Liverpool', country: 'England' },
+	{ season: '2005–06', team: 'Barcelona', country: 'Spain' },
+	{ season: '2006–07', team: 'Milan', country: 'Italy' },
+	{ season: '2007–08', team: 'Manchester United', country: 'England' },
+	{ season: '2008–09', team: 'Barcelona', country: 'Spain' },
+	{ season: '2009–10', team: 'Internazionale', country: 'Italy' },
+	{ season: '2010–11', team: 'Barcelona', country: 'Spain' },
+	{ season: '2011–12', team: 'Chelsea', country: 'England' },
+	{ season: '2012–13', team: 'Bayern', country: 'Germany' },
+	{ season: '2013–14', team: 'Real Madrid', country: 'Spain' },
+	{ season: '2014–15', team: 'Barcelona', country: 'Spain' },
+	{ season: '2015–16', team: 'Real Madrid', country: 'Spain' }
+];
+
+const countWins = (winnerList, country) => {
+	let countWinner = 0;
+	for (let i = 0; i < winnerList.length; i++) {
+		if (winnerList[i].country === country) {
+			countWinner++;
+		}
+	}
+	return countWinner;
+}
+
+console.log(countWins(winnerList1, 'Portugal'), 1);
+console.log(countWins(winnerList1, 'FootLand'), 0);
+console.log(countWins(winnerList1, 'Spain'), 9);
