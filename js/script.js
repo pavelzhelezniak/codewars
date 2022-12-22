e4e201d514c9f09e8cbf45bdb40126c1c7f20e29
pavelzhelezniak <zhele3o1992@gmail.com>
2022-12-22 10:14:03 +0300

    Spoonerize Me (7 kyu)

--- js/script.js
+++ js/script.js
@@ -10948,3 +10948,33 @@ console.log(sum(1, 2), 3)
 console.log(sum(5, 7, 9), 21)
 console.log(sum(12, 1, 1, 1, 1), 16)
 console.log(sum(12, 1, 1, 1, 1, 1, 1), 18)
+
+// Spoonerize Me (7 kyu)
+
+/* 
+A spoonerism is a spoken phrase in which the first letters of two of the words are swapped around, often with amusing results.
+
+In its most basic form a spoonerism is a two word phrase in which only the first letters of each word are swapped:
+
+"not picking" --> "pot nicking"
+
+Your task is to create a function that takes a string of two words, separated by a space: 
+words and returns a spoonerism of those words in a string, as in the above example. 
+A "word" in the context of this kata can contain any of the letters A through Z in upper or lower case, 
+and the numbers 0 to 9. Though spoonerisms are about letters in words in the domain of written 
+and spoken language, numbers are included in the inputs for the random test cases and they require no special treatment.
+
+NOTE: All input strings will contain only two words. Spoonerisms can be more complex. 
+For example, three-word phrases in which the first letters of the first and last words are swapped: 
+"pack of lies" --> "lack of pies" or more than one letter from a word is swapped: 
+"flat battery --> "bat flattery" You are NOT expected to account for these, or any other nuances involved in spoonerisms.
+*/
+
+const spoonerize = words => {
+	const wordsArr = words.split(' ');
+	return `${wordsArr[1][0]}${wordsArr[0].slice(1)} ${wordsArr[0][0]}${wordsArr[1].slice(1)}`
+}
+
+console.log(spoonerize("nit picking"), "pit nicking");
+console.log(spoonerize("wedding bells"), "bedding wells");
+console.log(spoonerize("jelly beans"), "belly jeans");
