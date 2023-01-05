abab9271a11f46424ee8449fbc7447608e651e3f
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-05 15:32:18 +0300

    Filter unused digits (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11222,3 +11222,26 @@ console.log(smaller([1, 2, 3]), [0, 0, 0]);
 console.log(smaller([1, 2, 0]), [1, 1, 0]);
 console.log(smaller([1, 2, 1]), [0, 1, 0]);
 console.log(smaller([1, 1, -1, 0, 0]), [3, 3, 0, 0, 0]);
+
+// Filter unused digits (7 kyu)
+
+/* 
+Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+Example:
+
+[12, 34, 56, 78]  =>  "09"
+[2015, 8, 26]     =>  "3479"
+Note: the digits in the resulting string should be sorted.
+*/
+
+const unusedDigits = (...arr) => {
+	const digits = arr.join();
+
+	return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		.filter(x => !digits.includes(x))
+		.join('');
+};
+
+console.log(unusedDigits(12, 34, 56, 78), "09");
+console.log(unusedDigits(2015, 8, 26), "3479");
