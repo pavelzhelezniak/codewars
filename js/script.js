86b9ba58ea574d269d73095b222504aaf426206e
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-06 14:28:17 +0300

    The Office III - Broken Photocopier (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11245,3 +11245,17 @@ const unusedDigits = (...arr) => {
 
 console.log(unusedDigits(12, 34, 56, 78), "09");
 console.log(unusedDigits(2015, 8, 26), "3479");
+
+// The Office III - Broken Photocopier (7 kyu)
+
+/* 
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+Given a string of binary, return the version the photocopier gives you as a string.
+*/
+
+const broken = x => x.split('').map(item => item === '1' ? '0' : '1').join('');
+
+console.log(broken("1"), "0");
+console.log(broken("10000000101101111110011001000"), "01111111010010000001100110111");
+console.log(broken("100010"), "011101");
