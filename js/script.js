600acf9f441f9f30f5745f00438188f3c370bbfb
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-08 21:40:42 +0300

    Very Even Numbers. (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11259,3 +11259,36 @@ const broken = x => x.split('').map(item => item === '1' ? '0' : '1').join('');
 console.log(broken("1"), "0");
 console.log(broken("10000000101101111110011001000"), "01111111010010000001100110111");
 console.log(broken("100010"), "011101");
+
+// "Very Even" Numbers. (7 kyu)
+
+/* 
+Write a function that returns true if the number is a "Very Even" number.
+
+If a number is a single digit, then it is simply "Very Even" if it itself is even.
+
+If it has 2 or more digits, it is "Very Even" if the sum of its digits is "Very Even".
+
+Examples
+number = 88 => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd 
+
+number = 222 => returns true -> 2 + 2 + 2 = 6 => 6 is even
+
+number = 5 => returns false
+
+number = 841 => returns true -> 8 + 4 + 1 = 13 -> 1 + 3 => 4 is even
+Note: The numbers will always be 0 or positive integers!
+*/
+
+const isVeryEvenNumber = n => n < 10 ?
+	n % 2 === 0 :
+	isVeryEvenNumber(n.toString().split('').reduce((acc, item) => acc + parseInt(item, 10), 0));
+
+console.log(isVeryEvenNumber(0), true);
+console.log(isVeryEvenNumber(4), true);
+console.log(isVeryEvenNumber(12), false);
+console.log(isVeryEvenNumber(222), true);
+console.log(isVeryEvenNumber(400000220), true);
+console.log(isVeryEvenNumber(5), false);
+console.log(isVeryEvenNumber(88), false);
+console.log(isVeryEvenNumber(45), false);
