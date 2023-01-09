ac6ae24eeaa969fd1daf4e387acc35af0fa8cfaa
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-09 15:15:46 +0300

    Javascript filter - 1 (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11292,3 +11292,25 @@ console.log(isVeryEvenNumber(400000220), true);
 console.log(isVeryEvenNumber(5), false);
 console.log(isVeryEvenNumber(88), false);
 console.log(isVeryEvenNumber(45), false);
+
+// Javascript filter - 1 (7 kyu)
+
+/* 
+While developing a website, you detect that some of the members have troubles logging in. 
+Searching through the code you find that all logins ending with a "_" make problems. 
+So you want to write a function that takes an array of pairs of login-names and e-mails, 
+and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".
+
+If you have the input-array:
+
+[ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ]
+it should output
+
+[ [ "bar_", "bar@bar.com" ] ]
+You have to use the filter-method which returns each element of the array for which the filter-method returns true.
+*/
+
+const searchNames = logins => logins.filter(a => a[0][a[0].length - 1] === '_');
+// const searchNames = logins => logins.filter(a => a[0].endsWith('_'));
+
+console.log(searchNames([["foo", "foo@foo.com"], ["bar_", "bar@bar.com"]]), [["bar_", "bar@bar.com"]]);
