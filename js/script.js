fdd550029a79bfbc3ffb615534bf3c06c8cf996a
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-13 11:56:19 +0300

    Largest Elements (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11386,3 +11386,16 @@ console.log(consecutive12([4, 8, 6]), 2);
 console.log(consecutive12([1, 2, 3, 4]), 0);
 console.log(consecutive12([]), 0);
 console.log(consecutive12([1]), 0);
+
+// Largest Elements (7 kyu)
+
+/* 
+Write a program that outputs the top n elements from a list.
+*/
+
+const largest = (n, xs) => xs.sort((a, b) => b - a).slice(0, n).reverse();
+
+console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), [9, 10]);
+console.log(largest(3, [5, 1, 5, 2, 3, 1, 2, 3, 5]), [5, 5, 5]);
+console.log(largest(7, [9, 1, 50, 22, 3, 13, 2, 63, 5]), [3, 5, 9, 13, 22, 50, 63]);
+console.log(largest(0, [1, 2, 3, 4, 8, 7, 6, 5]), []);
