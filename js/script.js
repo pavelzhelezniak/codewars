bd32d12cc7e77006659d7f7e4f64f126fe4423f9
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-15 18:40:41 +0300

    filterEvenLengthWords (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11399,3 +11399,18 @@ console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), [9, 10]);
 console.log(largest(3, [5, 1, 5, 2, 3, 1, 2, 3, 5]), [5, 5, 5]);
 console.log(largest(7, [9, 1, 50, 22, 3, 13, 2, 63, 5]), [3, 5, 9, 13, 22, 50, 63]);
 console.log(largest(0, [1, 2, 3, 4, 8, 7, 6, 5]), []);
+
+// filterEvenLengthWords (7 kyu)
+
+/* 
+Write a function called "filterEvenLengthWords".
+Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements
+of the given array whose length is an even number.
+var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+console.log(output); // --> ['word', 'word']
+*/
+
+const filterEvenLengthWords = words => words.filter(wordele => wordele.length % 2 === 0);
+
+console.log(filterEvenLengthWords(['Hello', 'World']));
+console.log(filterEvenLengthWords(['One', 'Two', 'Three', 'Four']));
