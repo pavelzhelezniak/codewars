e2b6c7072ef24744f0e76a430bf8d5c3a0b5c2d5
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-16 16:12:04 +0300

    Plus - minus - plus - plus - ... - Count (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11414,3 +11414,28 @@ const filterEvenLengthWords = words => words.filter(wordele => wordele.length % 
 
 console.log(filterEvenLengthWords(['Hello', 'World']));
 console.log(filterEvenLengthWords(['One', 'Two', 'Three', 'Four']));
+
+// Plus - minus - plus - plus - ... - Count (7 kyu)
+
+/* 
+Count how often sign changes in array.
+*/
+
+const catchSignChange = arr => {
+	let count = 0;
+	if (arr.length < 2) {
+		return 0;
+	} else {
+		for (let i = 1; i < arr.length; i++) {
+			if ((arr[i] < 0 && arr[i - 1] >= 0) || (arr[i] >= 0 && arr[i - 1] < 0)) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
+
+console.log(catchSignChange([1, 3, 4, 5]), 0);
+console.log(catchSignChange([1, -3, -4, 0, 5]), 2);
+console.log(catchSignChange([]), 0);
+console.log(catchSignChange([-47, 84, -30, -11, -5, 74, 77]), 3);
