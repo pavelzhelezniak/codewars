6a6c861ab7785a0d66590095cb3cb1ac03327201
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-18 17:20:28 +0300

    Find the calculation type (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11465,3 +11465,29 @@ console.log(scrollingText("CODEWARS"), ["CODEWARS",
 	"RSCODEWA",
 	"SCODEWAR"]);
 
+// Find the calculation type (7 kyu)
+
+/*
+	You have to create a function calcType, which receives 3 arguments: 
+	2 numbers, and the result of an unknown operation performed on them (also a number).
+Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
+The possible return strings are: "addition", "subtraction", "multiplication", "division".
+*/
+
+const calcType = (a, b, res) => {
+	switch (res) {
+		case a + b:
+			return 'addition';
+		case a * b:
+			return 'multiplication';
+		case a - b:
+			return 'subtraction';
+		case a / b:
+			return 'division';
+	}
+};
+
+console.log(calcType(1, 2, 3), 'addition');
+console.log(calcType(10, 4, 40), 'multiplication');
+console.log(calcType(10, 5, 5), 'subtraction');
+console.log(calcType(9, 5, 1.8), 'division'); 
