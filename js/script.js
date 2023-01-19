bd0cee074273fbd8a4a7976b033dc66ec7a64a7c
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-19 15:00:40 +0300

    Number-Star ladder (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11490,4 +11490,44 @@ const calcType = (a, b, res) => {
 console.log(calcType(1, 2, 3), 'addition');
 console.log(calcType(10, 4, 40), 'multiplication');
 console.log(calcType(10, 5, 5), 'subtraction');
-console.log(calcType(9, 5, 1.8), 'division'); 
+console.log(calcType(9, 5, 1.8), 'division');
+
+// Number-Star ladder (7 kyu)
+
+/* 
+Using n as a parameter in the function pattern, where n should be a natural number, 
+complete the codes to get the pattern (take the help of examples):
+
+Note: There is no newline in the end (after the pattern ends)
+
+Examples
+pattern(3) should return `"1\n12\n1*3", e.g. the following:
+
+1
+1*2
+1**3
+pattern(10): should return the following:
+
+1
+1*2
+1**3
+1***4
+1****5
+1*****6
+1******7
+1*******8
+1********9
+1*********10
+Note that there shouldn't be any trailing newlines.
+*/
+
+const pattern1 = n => {
+	let arr = [];
+	for (let i = 1; i < n + 1; i++) {
+		arr.push('1' + '*'.repeat(i - 1) + i)
+	}
+	return arr.join('\n').replace('1', '');
+}
+
+console.log(pattern1(3), "1\n1*2\n1**3");
+console.log(pattern1(7), "1\n1*2\n1**3\n1***4\n1****5\n1*****6\n1******7");
