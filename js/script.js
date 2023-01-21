207ae0957edc3e440231764408004af81489211f
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-21 13:56:11 +0300

    Is every value in the array an array? (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11554,3 +11554,22 @@ console.log(solve("abc"), true);
 console.log(solve("abd"), false);
 console.log(solve("dabc"), true);
 console.log(solve("abbc"), false);
+
+// Is every value in the array an array? (7 kyu)
+
+/* 
+Is every value in the array an array?
+This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+Examples:
+[[1],[2]] => true
+['1','2'] => false
+[{1:1},{2:2}] => false
+*/
+
+const arrCheck = value => value.every(Array.isArray);
+
+console.log(arrCheck([]), true);
+console.log(arrCheck([['string']]), true);
+console.log(arrCheck([[], {}]), false);
+console.log(arrCheck([[1], [2], [3]]), true);
+console.log(arrCheck(['A', 'R', 'R', 'A', 'Y']), false);
