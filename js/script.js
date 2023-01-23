ae9b5446ec53d723672b3f124c9003fa67fdbeab
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-23 13:43:58 +0300

    Debug Basic Calculator (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11590,3 +11590,43 @@ const reverse12 = str => str.split('').reverse().join('');
 
 console.log(multi([5, 1, 5]), 25);
 console.log(add([9, 8, 5]), 22);
+
+// Debug Basic Calculator (7 kyu)
+
+/* 
+Debug a function called calculate that takes 3 values. The first and third values are numbers. 
+The second value is a character. If the character is "+" , "-", "*", or "/", 
+the function will return the result of the corresponding mathematical function on the two numbers. 
+If the string is not one of the specified characters, the function should return null.
+
+calculate(2,"+", 4); //Should return 6
+calculate(6,"-", 1.5); //Should return 4.5
+calculate(-4,"*", 8); //Should return -32
+calculate(49,"/", -7); //Should return -7
+calculate(8,"m", 2); //Should return null
+calculate(4,"/",0) //should return null
+Kind of a fork (not steal :)) of Basic Calculator kata by TheDoctor.
+*/
+
+const calculate123 = (a, o, b) => {
+	switch (o) {
+		case "+":
+			return a + b;
+		case "-":
+			return a - b;
+		case "*":
+			return a * b;
+		case "/":
+			return b !== 0 ? a / b : null;
+
+		default:
+			return null;
+	}
+};
+
+console.log(calculate123(2, "+", 4), 6);
+console.log(calculate123(6, "-", 1.5), 4.5);
+console.log(calculate123(-4, "*", 8), -32);
+console.log(calculate123(49, "/", -7), -7);
+console.log(calculate123(8, "m", 2), null);
+console.log(calculate123(4, "/", 0), null);
