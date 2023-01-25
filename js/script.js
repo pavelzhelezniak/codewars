fef5e0eebb6c8dbd0a27b8bfee73786204a78107
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-25 18:23:05 +0300

    Calculate mean and concatenate string (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11657,3 +11657,28 @@ const multiples = (s1, s2, s3) => {
 console.log(multiples(2, 4, 40), [4, 8, 12, 16, 20, 24, 28, 32, 36]);
 console.log(multiples(13, 5, 800), [65, 130, 195, 260, 325, 390, 455, 520, 585, 650, 715, 780]);
 console.log(multiples(13, 15, 800), [195, 390, 585, 780]);
+
+// Calculate mean and concatenate string (7 kyu)
+
+/* 
+You will be given an array which will include both integers and characters.
+
+Return an array of length 2 with a[0] representing the mean of the ten integers as a floating point number. 
+There will always be 10 integers and 10 characters. 
+Create a single string with the characters and return it as a[1] while maintaining the original order.
+
+lst = ['u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0']
+Here is an example of your return
+
+[3.6, "udiwstagwo"]
+In C# and Java the mean return is a double.
+*/
+
+const mean = lst => {
+	let a = lst.filter(value => value.match(/[a-z]/gi)).join('');
+	let b = lst.filter(value => value.match(/[0-9]/gi)).reduce((acc, cur) => acc + parseFloat(cur), 0) / 10
+	return [b, a]
+}
+
+console.log(mean(["u", "6", "d", "1", "i", "w", "6", "s", "t", "4", "a", "6", "g", "1", "2", "w", "8", "o", "2", "0"]), [3.6, "udiwstagwo"]);
+console.log(mean(["0", "c", "7", "x", "6", "2", "3", "5", "w", "7", "0", "y", "v", "u", "h", "i", "n", "u", "0", "0"]), [3.0, "cxwyvuhinu"]);
