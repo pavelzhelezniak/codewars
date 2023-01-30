b519133d369765b78fd83882e7e1678d7b95b3a3
pavelzhelezniak <zhele3o1992@gmail.com>
2023-01-30 16:14:06 +0300

    Password maker (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11731,3 +11731,33 @@ const howManyTimes = (annualPrice, individualPrice) => {
 console.log(howManyTimes(40, 15), 3);
 console.log(howManyTimes(30, 10), 3);
 console.log(howManyTimes(80, 15), 6);
+
+// Password maker (7 kyu)
+
+/* 
+One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and make 
+a password by extracting the first letter of each word.
+
+Even better is to replace some of those letters with numbers (e.g., the letter O can be replaced with the number 0):
+
+instead of including i or I put the number 1 in the password;
+instead of including o or O put the number 0 in the password;
+instead of including s or S put the number 5 in the password.
+*/
+
+const makePassword = phrase => phrase.split(' ').map(item => {
+	switch (item[0].toLowerCase()) {
+		case 'i':
+			return '1';
+		case 'o':
+			return '0';
+		case 's':
+			return '5';
+
+		default:
+			return item[0];
+	}
+}).join('');
+
+console.log(makePassword("Give me liberty or give me death"), "Gml0gmd", "Wrong output for 'Give me liberty or give me death'");
+console.log(makePassword("Keep Calm and Carry On"), "KCaC0", "Wrong output for 'Keep Calm and Carry On'");
