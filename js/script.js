31bf3b39dcf576158f5f36670ef47555f917deb4
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-03 18:19:14 +0300

    Chain me (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11850,3 +11850,32 @@ const rakeGarden = garden => garden.split(' ')
 
 const garden1 = 'slug spider rock gravel gravel gravel gravel gravel gravel gravel';
 console.log(rakeGarden(garden1), 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel');
+
+// Chain me (7 kyu)
+
+/* 
+Write a generic function chainer
+Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+The input for each function is the output of the previous function (except the first function, 
+	which takes the starting value as its input). Return the final value after execution is complete.
+*/
+
+const chain = (input, fs) => {
+	let res = input;;
+
+	for (let i = 0; i < fs.length; i++) {
+		res = fs[i](res);
+	}
+	return res;
+}
+
+function add(x) {
+	return x + 10;
+}
+
+function mult(x) {
+	return x * 30;
+}
+
+console.log(chain(2, [add, mult]), 360, "Error: chain function does not work");
