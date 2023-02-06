f2433900556faf1d5884067ac9874949644d5859
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-06 17:45:30 +0300

    The Poet And The Pendulum (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11895,7 +11895,41 @@ state_capitals = [{ state: 'Maine', capital: 'Augusta' }];
 console.log(capital(state_capitals)[0], "The capital of Maine is Augusta");
 
 country_capitals = [{ 'country': 'Spain', 'capital': 'Madrid' }];
-console.log(capital(country_capitals)[0], "The capital of Spain is Madrid")
+console.log(capital(country_capitals)[0], "The capital of Spain is Madrid");
 
 mixed_capitals = [{ "state": 'Maine', capital: 'Augusta' }, { country: 'Spain', "capital": "Madrid" }];
-console.log(capital(mixed_capitals)[1], "The capital of Spain is Madrid")
+console.log(capital(mixed_capitals)[1], "The capital of Spain is Madrid");
+
+// The Poet And The Pendulum (7 kyu)
+
+/* 
+Given an array/list [] of n integers , Arrange them in a way similar to the to-and-fro movement of a Pendulum
+
+The Smallest element of the list of integers , must come in center position of array/list.
+
+The Higher than smallest , goes to the right .
+The Next higher number goes to the left of minimum number and So on , in a to-and-fro manner similar to that of a Pendulum.
+*/
+
+const pendulum = values => {
+	values.sort((a, b) => a - b);
+	const result = [values[0]];
+	let right = true;
+
+	for (let i = 1; i < values.length; i++) {
+		if (right) {
+			result.push(values[i]);
+		} else {
+			result.unshift(values[i]);
+		}
+		right = !right;
+	}
+
+	return result;
+};
+
+console.log(pendulum([33, 38, 38, 36, 43, 48, 32, 40, 47, 50, 33]), [50, 47, 40, 38, 33, 32, 33, 36, 38, 43, 48]);
+console.log(pendulum([49, 40, 41, 41, 39, 43, 40, 46, 30, 47, 46, 40]), [47, 46, 41, 40, 40, 30, 39, 40, 41, 43, 46, 49]);
+console.log(pendulum([48, 41, 38, 35, 50, 46, 38, 42, 37, 49, 44, 32, 37]), [50, 48, 44, 41, 38, 37, 32, 35, 37, 38, 42, 46, 49]);
+console.log(pendulum([49, 30, 39, 30, 40, 44, 43, 48, 47, 50, 42, 48, 33]), [50, 48, 47, 43, 40, 33, 30, 30, 39, 42, 44, 48, 49]);
+console.log(pendulum([48, 43, 35, 47, 39, 38, 38, 46, 49, 32, 42]), [49, 47, 43, 39, 38, 32, 35, 38, 42, 46, 48]);
