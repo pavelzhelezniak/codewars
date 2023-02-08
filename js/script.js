b6b70228406da1f577d37af244042b56cf93d0f0
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-08 11:08:46 +0300

    Find Duplicates (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11954,3 +11954,17 @@ console.log(evenChars("abcdefghijklm"), ["b", "d", "f", "h", "j", "l"]);
 console.log(evenChars("aiqbuwbjqwbckjdwbwkqbefhglqhfjbwqejbcadn.bcaw.jbhwefjbwqkvbweevkj.bwvwbhvjk.dsvbajdv.hwuvghwuvfhgw.vjhwncv.wecnaw.ecnvw.kejvhnw.evjkhweqv.kjhwqeev.kjbhdjk.vbaewkjva"), "invalid string")
 console.log(evenChars("a"), "invalid string");
 console.log(evenChars(""), "invalid string");
+
+// Find Duplicates (7 kyu)
+
+/* 
+Given an array, find the duplicates in that array, and return a new array of those duplicates. 
+The elements of the returned array should appear in the order when they first appeared as duplicates.
+
+Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., "1" != 1).
+*/
+
+const duplicates1 = arr => [...new Set(arr.filter((v, i, arr) => arr.indexOf(v) !== i))];
+
+console.log(duplicates1([1, 2, 4, 4, 3, 3, 1, 5, 3, '5']), [4, 3, 1]);
+console.log(duplicates1([0, 1, 2, 3, 4, 5]), []);
