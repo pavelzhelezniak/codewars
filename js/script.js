e9d8acad39fcab034ee134281be920127daae164
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-09 16:47:46 +0300

    Vampire Numbers (7 kyu)

--- js/script.js
+++ js/script.js
@@ -11968,3 +11968,28 @@ const duplicates1 = arr => [...new Set(arr.filter((v, i, arr) => arr.indexOf(v) 
 
 console.log(duplicates1([1, 2, 4, 4, 3, 3, 1, 5, 3, '5']), [4, 3, 1]);
 console.log(duplicates1([0, 1, 2, 3, 4, 5]), []);
+
+// Vampire Numbers (7 kyu)
+
+/* 
+Our loose definition of Vampire Numbers can be described as follows:
+
+6 * 21 = 126
+# 6 and 21 would be valid 'fangs' for a vampire number as the 
+# digits 6, 1, and 2 are present in both the product and multiplicands
+
+10 * 11 = 110
+# 110 is not a vampire number since there are three 1's in the
+# multiplicands, but only two 1's in the product
+Create a function that can receive two 'fangs' and determine if the product of the two is a valid vampire number.
+*/
+
+const vampire_test = (a, b) => (a.toString() + b.toString()).split('').sort().join() ===
+	(a * b).toString().split('').sort().join();
+
+console.log(vampire_test(21, 6), true, "Basic: 21 * 6 = 126 should return true");
+console.log(vampire_test(204, 615), true, "Basic: 204 * 615 = 125460 should return true");
+console.log(vampire_test(30, -51), true, "One Negative: 30 * -51 = -1530 should return true");
+console.log(vampire_test(-246, -510), false, "Double Negatives: -246 * -510 = 125460 should return false (The negative signs aren't present on the product)");
+console.log(vampire_test(2947050, 8469153), true, "Large: 2947050 * 8469153 = 24959017348650 should return true");
+console.log(vampire_test(2947051, 8469153), false, "Large: 2947051 * 8469153 = 24959025817803 should return false");
