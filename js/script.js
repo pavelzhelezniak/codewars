b091c8f035940ab352527a941b7972e41fd2efca
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-10 13:10:33 +0300

    Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!! (6 kyu)

--- js/script.js
+++ js/script.js
@@ -11993,3 +11993,57 @@ console.log(vampire_test(30, -51), true, "One Negative: 30 * -51 = -1530 should 
 console.log(vampire_test(-246, -510), false, "Double Negatives: -246 * -510 = 125460 should return false (The negative signs aren't present on the product)");
 console.log(vampire_test(2947050, 8469153), true, "Large: 2947050 * 8469153 = 24959017348650 should return true");
 console.log(vampire_test(2947051, 8469153), false, "Large: 2947051 * 8469153 = 24959025817803 should return false");
+
+// Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!! (6 kyu)
+
+/* 
+The number 
+89
+89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. 
+What's the use of saying "Eureka"? Because this sum gives the same number: 
+89=81+92
+89=8 1 +9 2 
+
+The next number in having this property is 135
+135:
+
+See this property again: 
+135=11+32+53
+135=1 1 +3 2 +5 3 
+
+Task
+We need a function to collect these numbers, that may receive two integers 
+�a, �b that defines the range 
+[�,�]
+[a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+Examples
+Let's see some cases (input -> output):
+
+1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+If there are no numbers of this kind in the range 
+[�,�]
+[a,b] the function should output an empty list.
+
+90, 100 --> []
+Enjoy it!!
+*/
+
+const sumDigPow = (a, b) => {
+	var res = [];
+	while (a <= b) {
+		if (a.toString().split('').reduce((acc, item, i) => acc + parseInt(item, 10) ** (i + 1), 0) === a)
+			res.push(a);
+		a++;
+	}
+	return res;
+}
+
+console.log(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]);
+console.log(sumDigPow(10, 100), [89]);
+console.log(sumDigPow(90, 100), []);
+console.log(sumDigPow(90, 150), [135]);
+console.log(sumDigPow(50, 150), [89, 135]);
+console.log(sumDigPow(10, 150), [89, 135]);
