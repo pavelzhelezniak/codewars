5afe4c39ebe07cf9bd1e7d9e5fedfcd4af7fe0bd
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-14 19:29:53 +0300

    Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented? (6 kyu)

--- js/script.js
+++ js/script.js
@@ -12158,3 +12158,41 @@ console.log(numPrimorial(4), 210);
 console.log(numPrimorial(5), 2310);
 console.log(numPrimorial(8), 9699690);
 console.log(numPrimorial(9), 223092870);
+
+// Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented? (6 kyu)
+
+/* 
+You will be given a sequence of objects (associative arrays in PHP) representing data about developers 
+who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if all of the following continents / geographic zones will be represented by at least one developer: 
+'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'.
+false otherwise.
+*/
+
+const allContinents = list => {
+	const continents = [];
+	list.forEach((item) => {
+		if (!continents.includes(item.continent)) {
+			continents.push(item.continent);
+		}
+	});
+	return continents.length === 5;
+}
+
+const list11 = [
+	{ firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
+	{ firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+	{ firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+	{ firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+	{ firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' }
+];
+
+const list21 = [
+	{ firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' }
+];
+
+console.log(allContinents(list11), true);
+console.log(allContinents(list21), false);
