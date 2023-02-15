ef75d6b063c581f388ce71a555935b019577964c
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-15 14:24:41 +0300

    Calculate String Rotation (6 kyu)

--- js/script.js
+++ js/script.js
@@ -12196,3 +12196,30 @@ const list21 = [
 
 console.log(allContinents(list11), true);
 console.log(allContinents(list21), false);
+
+// Calculate String Rotation (6 kyu)
+
+/* 
+Write a function that receives two strings and returns n, where n is equal to the number of 
+characters we should shift the first string forward to match the second. The check should be case sensitive.
+
+For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has 
+been rotated 5 characters forward to produce the second string, so 5 would be returned.
+
+If the second string isn't a valid rotation of the first string, the method returns -1.
+*/
+
+const shiftedDiff = (first, second) => {
+	if (first.length !== second.length) {
+		return -1;
+	}
+	return (second + second).indexOf(first);
+};
+
+console.log(shiftedDiff("eecoff", "coffee"), 4);
+console.log(shiftedDiff("Moose", "moose"), -1);
+console.log(shiftedDiff("isn't", "'tisn"), 2);
+console.log(shiftedDiff("Esham", "Esham"), 0);
+console.log(shiftedDiff(" ", " "), 0);
+console.log(shiftedDiff("hoop", "pooh"), -1);
+console.log(shiftedDiff("  ", " "), -1);
