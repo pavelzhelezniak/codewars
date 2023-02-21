6656bd181000fc29daacf3e60d7f18232ecf60f8
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-21 14:13:13 +0300

    Coding Meetup #15 - Higher-Order Functions Series - Find the odd names (6 kyu)

--- js/script.js
+++ js/script.js
@@ -12308,3 +12308,46 @@ const add1 = a => a + 1;
 const addAll3 = (a, b, c) => a + b + c;
 
 console.log(compose(add1, addAll3)(1, 2, 3), 7);
+
+// Coding Meetup #15 - Higher-Order Functions Series - Find the odd names (6 kyu)
+
+/* 
+You will be given an array of objects representing data about developers who have signed 
+up to attend the next coding meetup that you are organising.
+
+Given the following input array:
+
+var list1 = [
+  { firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
+  { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+];
+write a function that when executed as findOddNames(list1) returns only the developers where if you add the ASCII 
+representation of all characters in their first names, the result will be an odd number:
+
+[
+  { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+]
+Explanation of the above:
+
+Sum of ASCII codes of letters in 'Aba' is: 65 + 98 + 97 = 260 which is an even number
+Sum of ASCII codes of letters in 'Abb' is: 65 + 98 + 98 = 261 which is an odd number
+Notes:
+
+Preserve the order of the original list.
+Return an empty array [] if there is no developer with an "odd" name.
+The input array and first names will always be valid and formatted as in the example above.
+*/
+
+const findOddNames = list => list.filter(item => item.firstName.split('')
+	.reduce((acc, item) => acc + item.charCodeAt(), 0) % 2 !== 0);
+
+const list1234 = [
+	{ firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
+	{ firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+];
+
+const answer123 = [
+	{ firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+];
+
+console.log(findOddNames(list1234), answer123);
