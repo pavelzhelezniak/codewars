18b068774fe07846e2d074465f5eddec23c53df2
pavelzhelezniak <zhele3o1992@gmail.com>
2023-02-22 13:10:36 +0300

    first character that repeats (6 kyu)

--- js/script.js
+++ js/script.js
@@ -12351,3 +12351,35 @@ const answer123 = [
 ];
 
 console.log(findOddNames(list1234), answer123);
+
+// first character that repeats (6 kyu)
+
+/* 
+Find the first character that repeats in a String and return that character.
+
+firstDup('tweet') => 't'
+firstDup('like') => undefined
+This is not the same as finding the character that repeats first. In that case, an input of 'tweet' would yield 'e'.
+
+Another example:
+
+In 'translator' you should return 't', not 'a'.
+
+v      v  
+translator
+  ^   ^
+While second 'a' appears before second 't', the first 't' is before the first 'a'.
+*/
+
+const firstDup = s => s.split('').filter((item, i, arr) => {
+	if (i !== arr.lastIndexOf(item)) {
+		return item;
+	}
+})[0];
+
+console.log(firstDup('tweet'), 't');
+console.log(firstDup('Ode to Joy'), ' ');
+console.log(firstDup('ode to joy'), 'o');
+console.log(firstDup('bar'), undefined);
+console.log(firstDup('123123'), '1');
+console.log(firstDup('!@#$!@#$'), '!');
