d258650f31c8a076fecc85f2a17a81bd415a7e56
pavelzhelezniak <zhele3o1992@gmail.com>
2023-03-01 13:43:27 +0300

    Simple Substitution Cipher Helper (6 kyu)

--- js/script.js
+++ js/script.js
@@ -12509,3 +12509,56 @@ There is a global var that holds the numeric ID to which wire to cut. Find that 
 const wireCode = Object.keys(global);
 const numVariable = wireCode.filter(key => (typeof global[key] === 'number'))[0]
 Bomb.CutTheWire(global[numVariable]);
+
+// Simple Substitution Cipher Helper (6 kyu)
+
+/* 
+A simple substitution cipher replaces one character from an alphabet with a character from an alternate alphabet, 
+where each character's position in an alphabet is mapped to the alternate alphabet for encoding or decoding.
+
+E.g.
+
+var abc1 = "abcdefghijklmnopqrstuvwxyz";
+var abc2 = "etaoinshrdlucmfwypvbgkjqxz";
+   
+var sub = new SubstitutionCipher(abc1, abc2);
+sub.encode("abc") // => "eta"
+sub.encode("xyz") // => "qxz"
+sub.encode("aeiou") // => "eirfg"
+   
+sub.decode("eta") // => "abc"
+sub.decode("qxz") // => "xyz"
+sub.decode("eirfg") // => "aeiou"
+If a character provided is not in the opposing alphabet, simply leave it as be.
+*/
+
+function SubstitutionCipher(abc1, abc2) {
+	this.encode = function (str) {
+		let newStr = '';
+		for (let i = 0; i < str.length; i++) {
+			const j = abc1.indexOf(str[i]);
+			j !== -1 ? newStr += abc2[j] : newStr += str[i];
+		}
+		return newStr;
+	}
+	this.decode = function (str) {
+		let newStr = '';
+		for (let i = 0; i < str.length; i++) {
+			const j = abc2.indexOf(str[i]);
+			j !== -1 ? newStr += abc1[j] : newStr += str[i];
+		}
+		return newStr;
+	}
+}
+
+const abc1 = "abcdefghijklmnopqrstuvwxyz";
+const abc2 = "etaoinshrdlucmfwypvbgkjqxz";
+const sub = new SubstitutionCipher(abc1, abc2);
+
+console.log(sub.encode("xyz"), "qxz");
+console.log(sub.encode("abc"), "eta");
+console.log(sub.encode("aeiou"), "eirfg");
+
+console.log(sub.decode("eta"), "abc");
+console.log(sub.decode("qxz"), "xyz");
+console.log(sub.decode("eirfg"), "aeiou");
