2c49dd718deec28944e119adc76b57b707b712f0
pavelzhelezniak <zhele3o1992@gmail.com>
2023-03-05 19:22:52 +0300

    Sum of odd numbers (7 kyu)

--- js/script.js
+++ js/script.js
@@ -12634,3 +12634,26 @@ console.log(isIsogram("aba"), false, "same chars may not be adjacent");
 console.log(isIsogram("moOse"), false, "same chars may not be same case");
 console.log(isIsogram("isIsogram"), false);
 console.log(isIsogram(""), true, "an empty string is a valid isogram");
+
+// Sum of odd numbers (7 kyu)
+
+/* 
+Given the triangle of consecutive odd numbers:
+
+				 1
+			 3     5
+		 7     9    11
+	13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+*/
+
+const rowSumOddNumbers = n => Math.pow(n, 3);
+
+console.log(rowSumOddNumbers(1), 1);
+console.log(rowSumOddNumbers(4), 64);
+console.log(rowSumOddNumbers(42), 74088);
