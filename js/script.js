28513b3b81f8411c5ce588117c72ad3c8f6ac81d
pavelzhelezniak <zhele3o1992@gmail.com>
2023-03-06 18:41:12 +0300

    Find the divisors! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -12657,3 +12657,32 @@ const rowSumOddNumbers = n => Math.pow(n, 3);
 console.log(rowSumOddNumbers(1), 1);
 console.log(rowSumOddNumbers(4), 64);
 console.log(rowSumOddNumbers(42), 74088);
+
+// Find the divisors! (7 kyu)
+
+/* 
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's 
+divisors(except for 1 and the number itself), from smallest to largest. 
+If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, 
+String> in Rust).
+
+Example:
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+*/
+
+const divisors = integer => {
+	let divisors = [];
+	for (let i = 2; i < integer; i++) {
+		if (integer % i == 0) {
+			divisors.push(i);
+		}
+	}
+
+	return divisors.length !== 0 ? divisors : `${integer} is prime`;
+}
+
+console.log(divisors(15), [3, 5]);
+console.log(divisors(12), [2, 3, 4, 6]);
+console.log(divisors(13), "13 is prime");
