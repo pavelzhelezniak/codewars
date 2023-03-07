ef1ed161922554464b285f4119e63506baa4cf50
pavelzhelezniak <zhele3o1992@gmail.com>
2023-03-07 19:36:13 +0300

    Breaking chocolate problem (7 kyu)

--- js/script.js
+++ js/script.js
@@ -12686,3 +12686,20 @@ const divisors = integer => {
 console.log(divisors(15), [3, 5]);
 console.log(divisors(12), [2, 3, 4, 6]);
 console.log(divisors(13), "13 is prime");
+
+// Breaking chocolate problem (7 kyu)
+
+/* 
+Your task is to split the chocolate bar of given dimension n x m into small squares. 
+Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, 
+but for size 3 x 1 you must do two breaks.
+If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). 
+Input will always be a non-negative integer.
+*/
+
+const breakChocolate = (n, m) => n > 0 && m > 0 ? (n - 1) + n * (m - 1) : 0;
+
+console.log(breakChocolate(5, 5), 24);
+console.log(breakChocolate(1, 1), 0);
+console.log(breakChocolate(5, 0), 0);
