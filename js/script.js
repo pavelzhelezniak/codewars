d5d7e222b798326c868801455f08bd0410e91a57
pavelzhelezniak <zhele3o1992@gmail.com>
2023-03-10 15:16:08 +0300

    Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe (7 kyu)

--- js/script.js
+++ js/script.js
@@ -12735,3 +12735,45 @@ console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 
 console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"), "Harald");
 console.log(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry"), "Harald");
 console.log(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald"), "Harald");
+
+// Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe (7 kyu)
+
+/* 
+You will be given an array of objects (hashes in ruby) representing data about developers who 
+have signed up to attend the coding meetup that you are organising for the first time.
+Your task is to return the number of JavaScript developers coming from Europe.
+
+For example, given the following list:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+your function should return number 1.
+
+If, there are no JavaScript developers from Europe then your function should return 0.
+
+Notes:
+
+The format of the strings will always be Europe and JavaScript.
+All data will always be valid and uniform as in the example above.
+*/
+
+const countDevelopers = list => list.filter(dev => dev.continent === 'Europe' && dev.language === 'JavaScript').length;
+
+const list12345 = [
+	{ firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+	{ firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+	{ firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+	{ firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+const list234 = [
+	{ firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 19, language: 'HTML' },
+	{ firstName: 'Lukas', lastName: 'R.', country: 'Austria', continent: 'Europe', age: 89, language: 'HTML' }
+];
+
+console.log(countDevelopers(list12345), 1, "Noah is the only JavaScript developer from Europe");
+console.log(countDevelopers(list234), 0, "Lukas is not a JS devolper");
