a13738dbe40e7ddb57c02971cadd2b52c2d57e6b
pavelzhelezniak <zhele3o1992@gmail.com>
2023-03-15 18:28:03 +0300

    Array Deep Count (6 kyu)

--- js/script.js
+++ js/script.js
@@ -12901,3 +12901,24 @@ console.log(getParticipants(7), 5);
 console.log(getParticipants(12), 6);
 console.log(getParticipants(19), 7);
 console.log(getParticipants(27), 8);
+
+// Array Deep Count (6 kyu)
+
+/* 
+You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+
+Examples
+[]                   -->  0
+[1, 2, 3]            -->  3
+["x", "y", ["z"]]    -->  4
+[1, 2, [3, 4, [5]]]  -->  7
+The input will always be an array.
+*/
+
+const deepCount = arr => arr.reduce((res, item) => Array.isArray(item) ? res += deepCount(item) : res, arr.length);
+
+console.log(deepCount([]), 0, "Expected 0");
+console.log(deepCount([1, 2, 3]), 3, "Expected 3");
+console.log(deepCount(["x", "y", ["z"]]), 4, "Expected 4");
+console.log(deepCount([1, 2, [3, 4, [5]]]), 7, "Expected 7");
+console.log(deepCount([[[[[[[[[]]]]]]]]]), 8, "Expected 8");
