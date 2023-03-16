795f0a58a6c1e09ec8b5938ec3d1050b9a8ad06d
pavelzhelezniak <zhele3o1992@gmail.com>
2023-03-16 18:02:56 +0300

    Let's Recycle! (6 kyu)

--- js/script.js
+++ js/script.js
@@ -13081,3 +13081,65 @@ console.log(head([5, 1]), 5);
 console.log(tail([1]), []);
 console.log(init([1, 5, 7, 9]), [1, 5, 7]);
 console.log(lastF([7, 2]), 2);
+
+// Let's Recycle! (6 kyu)
+
+/* 
+You will be given a list of objects. Each object has type, material, and possibly secondMaterial. 
+The existing materials are: paper, glass, organic, and plastic.
+
+Your job is to sort these objects across the 4 recycling bins according to their material 
+(and secondMaterial if it's present), by listing the type's of objects that should go into those bins.
+
+Notes
+The bins should come in the same order as the materials listed above
+All bins should be listed in the output, even if some of them are empty
+If an object is made of two materials, its type should be listed in both of the respective bins
+The order of the type's in each bin should be the same as the order of their respective objects was in the input list
+Example
+input = [
+  {"type": "rotten apples", "material": "organic"},
+  {"type": "out of date yogurt", "material": "organic", "secondMaterial": "plastic"},
+  {"type": "wine bottle", "material": "glass", "secondMaterial": "paper"},
+  {"type": "amazon box", "material": "paper"},
+  {"type": "beer bottle", "material": "glass", "secondMaterial": "paper"}
+]
+
+output = [
+  ["wine bottle", "amazon box", "beer bottle"],
+  ["wine bottle", "beer bottle"],
+  ["rotten apples", "out of date yogurt"],
+  ["out of date yogurt"]
+]
+*/
+
+const input = [
+	{ type: 'rotten apples', material: 'organic' },
+	{ type: 'out of date yogurt', material: 'organic', secondMaterial: 'plastic' },
+	{ type: 'wine bottle', material: 'glass', secondMaterial: 'paper' },
+	{ type: 'amazon box', material: 'paper' },
+	{ type: 'beer bottle', material: 'glass', secondMaterial: 'paper' }
+];
+const output = [
+	['wine bottle', 'amazon box', 'beer bottle'],
+	['wine bottle', 'beer bottle'],
+	['rotten apples', 'out of date yogurt'],
+	['out of date yogurt']
+];
+
+const recycle = array => {
+	const paperTrashBin = [];
+	const glassTrashBin = [];
+	const organicTrashBin = [];
+	const plasticTrashBin = [];
+	for (const obj of array) {
+		obj.material === 'paper' || obj.secondMaterial === 'paper' ? paperTrashBin.push(obj.type) : null;
+		obj.material === 'glass' || obj.secondMaterial === 'glass' ? glassTrashBin.push(obj.type) : null;
+		obj.material === 'organic' || obj.secondMaterial === 'organic' ? organicTrashBin.push(obj.type) : null;
+		obj.material === 'plastic' || obj.secondMaterial === 'plastic' ? plasticTrashBin.push(obj.type) : null;
+	}
+
+	return [paperTrashBin, glassTrashBin, organicTrashBin, plasticTrashBin];
+}
+
+console.log(recycle(input), output);
