43debf617c19e3b7c73ff145072ac08c68d609db
pavelzhelezniak <zhele3o1992@gmail.com>
2023-03-19 21:52:26 +0300

    Walk the Object Path (6 kyu)

--- js/script.js
+++ js/script.js
@@ -13248,3 +13248,48 @@ console.log(
 	runLengthEncoding("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"),
 	[[12, 'W'], [1, 'B'], [12, 'W'], [3, 'B'], [24, 'W'], [1, 'B'], [14, 'W']]
 );
+
+// Walk the Object Path (6 kyu)
+
+/* 
+Implement the method find which takes in an two parameters object and path. 
+The path will be a period-delimited string of properties that we will use to traverse 
+through our object to find our target value.
+
+Check out this Codecast to get started! https://codecast.qualified.io/interaction/594197b92f524d001c1ab790
+
+Edge Cases And Further Consideration
+Be sure to handle passing array indices. For example, if we have an object: { people: ['John', 'Dave', 'Lisa'] } 
+and the path is 'people.1', the target value is 'Dave' which is the string at position 1 inside of the people array.
+
+Also this method should handle invalid paths. If we have an object { user: { name: 'Dan' } } 
+and the path is 'user.wallet.money', we should return undefined. 
+Valid paths are exclusive to properties on the object which are not inherited, 
+in other words it is specific to this object and does not need to look up the prototype chain.
+*/
+
+const object = {
+	user: {
+		name: {
+			first: 'John',
+			last: 'Snow'
+		}
+	}
+};
+const path = 'user.name.first';
+
+const find = (object, path) => {
+	path = path.split(/[.]/);
+	for (const j in newPath) {
+		if (object.hasOwnProperty(newPath[j]) === true) {
+			object = object[newPath[j]];
+		} else {
+			return undefined;
+		}
+	}
+
+	return object;
+};
+const actual = find(object, path);
+
+console.log(actual, expected);
