7ab61e0b627d43ee98f45c3ae01efee9e0c86f7c
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-03 21:35:59 +0300

    Coding Meetup #2 - Higher-Order Functions Series - Greet developers (7 kyu)

--- js/script.js
+++ js/script.js
@@ -13720,3 +13720,84 @@ console.log(overTheRoad(3, 3), 4);
 console.log(overTheRoad(2, 3), 5);
 console.log(overTheRoad(3, 5), 8);
 console.log(overTheRoad(7, 11), 16);
+
+// Coding Meetup #2 - Higher-Order Functions Series - Greet developers (7 kyu)
+
+/* 
+You will be given an array of objects (associative arrays in PHP, 
+	tables in COBOL) representing data about developers who have signed up to attend the next 
+	coding meetup that you are organising.
+
+Your task is to return an array where each object will have a new property 
+'greeting' with the following string value:
+
+Hi < firstName here >, what do you like the most about < language here >?
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+your function should return the following array:
+
+[
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+	 greeting: 'Hi Sofia, what do you like the most about Java?'
+  },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+	 greeting: 'Hi Lukas, what do you like the most about Python?'
+  },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+	 greeting: 'Hi Madison, what do you like the most about Ruby?'
+  } 
+];
+Notes:
+
+The order of the properties in the objects does not matter (except in COBOL).
+The input array will always be valid and formatted as in the example above.
+*/
+
+const greetDevelopers = list => list
+	.map(dev => ({ ...dev, greeting: `Hi ${dev.firstName}, what do you like the most about ${dev.language}?` }));
+
+console.log(greetDevelopers([
+	{
+		firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+	},
+	{
+		firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+	},
+	{
+		firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+	},
+]), [
+	{
+		firstName: 'Sofia',
+		lastName: 'I.',
+		country: 'Argentina',
+		continent: 'Americas',
+		age: 35,
+		language: 'Java',
+		greeting: 'Hi Sofia, what do you like the most about Java?',
+	},
+	{
+		firstName: 'Lukas',
+		lastName: 'X.',
+		country: 'Croatia',
+		continent: 'Europe',
+		age: 35,
+		language: 'Python',
+		greeting: 'Hi Lukas, what do you like the most about Python?',
+	},
+	{
+		firstName: 'Madison',
+		lastName: 'U.',
+		country: 'United States',
+		continent: 'Americas',
+		age: 32,
+		language: 'Ruby',
+		greeting: 'Hi Madison, what do you like the most about Ruby?',
+	},
+]);
