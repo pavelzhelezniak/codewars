bce104ae56709fbfbafb93530ef19692bf60f5c1
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-05 18:37:07 +0300

    Simple beads count (7 kyu)

--- js/script.js
+++ js/script.js
@@ -13801,3 +13801,23 @@ console.log(greetDevelopers([
 		greeting: 'Hi Madison, what do you like the most about Ruby?',
 	},
 ]);
+
+// Simple beads count (7 kyu)
+
+/* 
+Two red beads are placed between every two blue beads. 
+There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) 
+so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.
+*/
+
+const countRedBeads = n => n < 2 ? 0 : (n - 1) * 2;
+
+console.log(countRedBeads(0), 0);
+console.log(countRedBeads(1), 0);
+console.log(countRedBeads(3), 4);
+console.log(countRedBeads(5), 8);
