873f639ddc3338241874d2118de21717c8449b16
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-10 16:24:35 +0300

    Alphabet war (7 kyu)

--- js/script.js
+++ js/script.js
@@ -13861,3 +13861,50 @@ const addLetters = (...letters) => {
 console.log(addLetters(['a', 'b', 'c']), 'f');
 console.log(addLetters(['z']), 'z');
 console.log(addLetters(['a', 'b']), 'c');
+
+// Alphabet war (7 kyu)
+
+/* 
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. 
+The tension between left side letters and right side letters was too high and the war began.
+
+Task
+Write a function that accepts fight string consists of only small letters and return who wins the fight. 
+When the left side wins return Left side wins!, when the right side wins return Right side wins!, 
+in other case return Let's fight again!.
+
+The left side letters and their power:
+
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+The right side letters and their power:
+
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+The other letters don't have power and are only victims.
+
+Example
+alphabetWar("z");        //=> Right side wins!
+alphabetWar("zdqmwpbs"); //=> Let's fight again!
+alphabetWar("zzzzs");    //=> Right side wins!
+alphabetWar("wwwwwwz");  //=> Left side wins!
+*/
+
+const alphabetWar = fight => {
+	const dictL = { w: 4, p: 3, b: 2, s: 1 }, dictR = { m: 4, q: 3, d: 2, z: 1 };
+	const arr = fight.split('');
+
+	const left = arr.filter(v => v.match(/[wpbs]/g)).map(v => dictL[v]).reduce((a, b) => a + b, 0);
+	const right = arr.filter(v => v.match(/[mqdz]/g)).map(v => dictR[v]).reduce((a, b) => a + b, 0);
+	return left > right ? "Left side wins!" : left < right ? "Right side wins!" : "Let's fight again!";
+};
+
+console.log(alphabetWar("z"), "Right side wins!");
+console.log(alphabetWar("zdqmwpbs"), "Let's fight again!");
+console.log(alphabetWar("zzzzs"), "Right side wins!");
+console.log(alphabetWar("wwwwww"), "Left side wins!");
