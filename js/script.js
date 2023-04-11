57118e9c035c3f9f784b41792c307975d188a624
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-11 13:32:22 +0300

    Deodorant Evaporator (7  kyu)

--- js/script.js
+++ js/script.js
@@ -13908,3 +13908,32 @@ console.log(alphabetWar("z"), "Right side wins!");
 console.log(alphabetWar("zdqmwpbs"), "Let's fight again!");
 console.log(alphabetWar("zzzzs"), "Right side wins!");
 console.log(alphabetWar("wwwwww"), "Left side wins!");
+
+// Deodorant Evaporator (7  kyu)
+
+/* 
+This program tests the life of an evaporator containing a gas.
+
+We know the content of the evaporator (content in ml), 
+the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) 
+in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+
+The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+Example:
+evaporator(10, 10, 5) -> 29
+Note:
+Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, 
+as you wish. Some people might prefer to reason with content, some other with percentages only. 
+It's up to you but you must keep it as a parameter because the tests have it as an argument.
+*/
+
+const evaporator = (content, evap_per_day, threshold) => {
+	let result = 0;
+	let percentage = 100;
+	while (percentage > threshold) {
+		percentage = percentage - percentage * (evap_per_day / 100);
+		result += 1;
+	}
+	return result;
+}
