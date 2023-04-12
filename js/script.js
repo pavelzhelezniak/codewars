9d1df487f33130102e0b46cc626bb8e230f149e9
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-12 13:31:54 +0300

    Fizz Buzz (7 kyu)

--- js/script.js
+++ js/script.js
@@ -13937,3 +13937,34 @@ const evaporator = (content, evap_per_day, threshold) => {
 	}
 	return result;
 }
+
+// Fizz Buzz (7 kyu)
+
+/* 
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+*/
+
+const fizzbuzz = n => {
+	const res = [];
+	for (let i = 1; i < n + 1; i++) {
+		i % 3 === 0 && i % 5 === 0 ? res.push("FizzBuzz") :
+			i % 3 === 0 ? res.push("Fizz") :
+				i % 5 === 0 ? res.push("Buzz") :
+					res.push(i);
+	}
+
+	return res;
+};
+
+console.log(fizzbuzz(10), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']);
