de24feff1738c4e9ff329fa9361b2a0384090673
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-14 19:25:04 +0300

    Triangular Treasure (7 kyu)

--- js/script.js
+++ js/script.js
@@ -13968,3 +13968,28 @@ const fizzbuzz = n => {
 };
 
 console.log(fizzbuzz(10), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']);
+
+// Triangular Treasure (7 kyu)
+
+/* 
+Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+1st (1)   2nd (3)    3rd (6)
+*          **        ***
+			  *         **
+							*
+You need to return the nth triangular number. You should return 0 for out of range values:
+
+For example: (Input --> Output)
+
+0 --> 0
+2 --> 3
+3 --> 6
+-10 --> 0
+*/
+
+const triangular = n => n > 0 ? n * (n + 1) / 2 : 0;
+
+console.log(triangular(2), 3);
+console.log(triangular(4), 10);
+console.log(triangular(-454), 0);
