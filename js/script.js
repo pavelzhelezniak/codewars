871b2c2c193add396befab9c5b6a6a8b18bb63e2
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-17 20:15:09 +0300

    Gauß needs help! (Sums of a lot of numbers). (7 kyu)

--- js/script.js
+++ js/script.js
@@ -13993,3 +13993,40 @@ const triangular = n => n > 0 ? n * (n + 1) / 2 : 0;
 console.log(triangular(2), 3);
 console.log(triangular(4), 10);
 console.log(triangular(-454), 0);
+
+// Gauß needs help! (Sums of a lot of numbers). (7 kyu)
+
+/* 
+Due to another of his misbehaved, the primary school's teacher of the young Gauß, 
+Herr J.G. Büttner, to keep the bored and unruly young schoolboy Karl Friedrich Gauss busy for a good long time, 
+while he teaching arithmetic to his mates, assigned him the problem of adding up all the whole numbers 
+from 1 through a given number n.
+
+Your task is to help the young Carl Friedrich to solve this problem as quickly as you can; 
+so, he can astonish his teacher and rescue his recreation interval.
+
+Here's, an example:
+
+f(n=100) // returns 5050 
+It's your duty to verify that n is a valid positive integer number. 
+If not, please, return false (None for Python, null for C#, 0 for COBOL).
+
+Note: the goal of this kata is to invite you to think about some 'basic' mathematic 
+formula and how you can do performance optimization on your code. 
+
+Advanced - experienced users should try to solve it in one line, without loops, or optimizing the code as much as they can.
+
+Credits: this kata was inspired by the farzher's kata 'Sum of large ints' . 
+In fact, it can be seen as a sort of prep kata for that one.
+*/
+
+const f = n => n > 0 && parseInt(n, 10) === n ? n * (n + 1) / 2 : false;
+
+console.log(f(100), 5050);
+console.log(f(300), 45150);
+console.log(f(50000), 1250025000);
+console.log(f('n'), false, "n must be a number");
+console.log(f(), false, "n must be a value");
+console.log(f(3.14), false, "n must be an integer");
+console.log(f(0), false, "a value of n greater than 0 is required");
+console.log(f(-10), false, "a positive value of n is required");
