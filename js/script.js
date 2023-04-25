1a6afa1833d3049fa9a443c3a5f6180bd39c6d27
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-25 20:06:23 +0300

    Simple consecutive pairs (7 kyu)

--- js/script.js
+++ js/script.js
@@ -14352,5 +14352,37 @@ const growingPlant = (upSpeed, downSpeed, desiredHeight) => {
 
 };
 
-console.log(growingPlant(100, 10, 910), 10)
-console.log(growingPlant(10, 9, 4), 1)
+console.log(growingPlant(100, 10, 910), 10);
+console.log(growingPlant(10, 9, 4), 1);
+
+// Simple consecutive pairs (7 kyu)
+
+/* 
+In this Kata your task will be to return the count of pairs that have consecutive numbers as follows:
+
+pairs([1,2,5,8,-4,-3,7,6,5]) = 3
+The pairs are selected as follows [(1,2),(5,8),(-4,-3),(7,6),5]
+--the first pair is (1,2) and the numbers in the pair are consecutive; Count = 1
+--the second pair is (5,8) and are not consecutive
+--the third pair is (-4,-3), consecutive. Count = 2
+--the fourth pair is (7,6), also consecutive. Count = 3. 
+--the last digit has no pair, so we ignore.
+*/
+
+const pairs = arr => {
+	let count = 0;
+
+	for (let i = 0; i < arr.length - 1; i += 2) {
+		if (Math.abs(arr[i] - arr[i + 1]) === 1) {
+			count++;
+		}
+	}
+
+	return count;
+};
+
+console.log(pairs([1, 2, 5, 8, -4, -3, 7, 6, 5]), 3);
+console.log(pairs([21, 20, 22, 40, 39, -56, 30, -55, 95, 94]), 2);
+console.log(pairs([81, 44, 80, 26, 12, 27, -34, 37, -35]), 0);
+console.log(pairs([-55, -56, -7, -6, 56, 55, 63, 62]), 4);
+console.log(pairs([73, 72, 8, 9, 73, 72]), 3);
