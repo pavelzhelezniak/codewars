cec31a90f3359fc6bd2c515c1a5b02c035c1f04c
pavelzhelezniak <zhele3o1992@gmail.com>
2023-04-28 20:12:50 +0300

    Greatest common divisor (7 kyu)

--- js/script.js
+++ js/script.js
@@ -14427,3 +14427,18 @@ console.log(maxRot(38458215), 85821534);
 console.log(maxRot(195881031), 988103115);
 console.log(maxRot(896219342), 962193428);
 console.log(maxRot(69418307), 94183076);
+
+// Greatest common divisor (7 kyu)
+
+/* 
+Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+The inputs x and y are always greater or equal to 1, 
+so the greatest common divisor will always be an integer that is also greater or equal to 1.
+*/
+
+const mygcd = (x, y) => y === 0 ? x : mygcd(y, x % y);
+
+console.log(mygcd(30, 12), 6);
+console.log(mygcd(36, 12), 12);
+console.log(mygcd(8, 9), 1);
+console.log(mygcd(1, 1), 1);
