4a03859ca55dbc25d66b238090760f4adc48d6f4
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-02 21:05:23 +0300

    Sum of Triangular Numbers (7 kyu)

--- js/script.js
+++ js/script.js
@@ -14442,3 +14442,38 @@ console.log(mygcd(30, 12), 6);
 console.log(mygcd(36, 12), 12);
 console.log(mygcd(8, 9), 1);
 console.log(mygcd(1, 1), 1);
+
+// Sum of Triangular Numbers (7 kyu)
+
+/* 
+Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+
+Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) 
+obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
+
+[01]
+02 [03]
+04 05 [06]
+07 08 09 [10]
+11 12 13 14 [15]
+16 17 18 19 20 [21]
+e.g. If 4 is given: 1 + 3 + 6 + 10 = 20.
+
+Triangular Numbers cannot be negative so return 0 if a negative number is given.
+*/
+
+const sumTriangularNumbers = n => {
+	let sum = 0;
+
+	for (let i = 1; i <= n; i++) {
+		sum += (i * (i + 1)) / 2;
+	}
+
+	return sum;
+};
+
+console.log(sumTriangularNumbers(6), 56);
+console.log(sumTriangularNumbers(34), 7140);
+console.log(sumTriangularNumbers(-291), 0);
+console.log(sumTriangularNumbers(943), 140205240);
+console.log(sumTriangularNumbers(-971), 0);
