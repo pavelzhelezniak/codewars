5e977982c0de032dd6a2737b2b89f70d3c9405d4
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-07 22:32:32 +0300

    All Inclusive? (7 kyu)

--- js/script.js
+++ js/script.js
@@ -14632,3 +14632,41 @@ console.log(countLanguages([
 	{ firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
 	{ firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
 ]), { C: 2, JavaScript: 1, Ruby: 1 });
+
+// All Inclusive? (7 kyu)
+
+/* 
+Input:
+
+a string strng
+an array of strings arr
+Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+a boolean true if all rotations of strng are included in arr
+false otherwise
+Examples:
+contain_all_rots(
+  "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+contain_all_rots(
+  "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+Note:
+Though not correct in a mathematical sense
+
+we will consider that there are no rotations of strng == ""
+and for any array arr: contain_all_rots("", arr) --> true
+*/
+
+const containAllRots = (str, arr) => {
+	for (let i = 0; i < str.length; i++) {
+		if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(containAllRots("", []), true)
+console.log(containAllRots("", ["bsjq", "qbsj"]), true)
+console.log(containAllRots("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]), true)
+console.log(containAllRots("XjYABhR", ["TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR", "ABhRXjY"]), false)
