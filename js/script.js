7e46b223fd64509195d5411a66f7360db33728d1
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-08 21:34:12 +0300

    The Office II - Boredom Score (7 kyu)

--- js/script.js
+++ js/script.js
@@ -14670,3 +14670,74 @@ console.log(containAllRots("", []), true)
 console.log(containAllRots("", ["bsjq", "qbsj"]), true)
 console.log(containAllRots("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]), true)
 console.log(containAllRots("XjYABhR", ["TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR", "ABhRXjY"]), false)
+
+// The Office II - Boredom Score (7 kyu)
+
+/* 
+Every now and then people in the office moves teams or departments. 
+Depending what people are doing with their time they can become more or less boring. 
+Time to assess the current team.
+
+You will be provided with an object(staff) containing the staff names as keys, 
+and the department they work in as values.
+
+Each department has a different boredom assessment score, as follows:
+
+accounts = 1
+finance = 2
+canteen = 10
+regulation = 3
+trading = 6
+change = 6
+IS = 8
+retail = 5
+cleaning = 4
+pissing about = 25
+
+Depending on the cumulative score of the team, return the appropriate sentiment:
+
+<=80: 'kill me now'
+< 100 & > 80: 'i can handle this'
+100 or over: 'party time!!'
+*/
+
+const boredom = staff => {
+	const departments = {
+		'accounts': 1,
+		'finance': 2,
+		'canteen': 10,
+		'regulation': 3,
+		'trading': 6,
+		'change': 6,
+		'IS': 8,
+		'retail': 5,
+		'cleaning': 4,
+		'pissing about': 25
+	};
+	let total = 0
+	for (const key in staff) {
+		total += departments[staff[key]];
+	}
+	return total <= 80 ? 'kill me now' :
+		total < 100 ? 'i can handle this' :
+			'party time!!';
+};
+
+console.log(boredom({
+	tim: 'change', jim: 'accounts',
+	randy: 'canteen', sandy: 'change', andy: 'change', katie: 'IS',
+	laura: 'change', saajid: 'IS', alex: 'trading', john: 'accounts',
+	mr: 'finance'
+}), 'kill me now');
+console.log(boredom({
+	tim: 'IS', jim: 'finance',
+	randy: 'pissing about', sandy: 'cleaning', andy: 'cleaning',
+	katie: 'cleaning', laura: 'pissing about', saajid: 'regulation',
+	alex: 'regulation', john: 'accounts', mr: 'canteen'
+}), 'i can handle this');
+console.log(boredom({
+	tim: 'accounts', jim: 'accounts',
+	randy: 'pissing about', sandy: 'finance', andy: 'change',
+	katie: 'IS', laura: 'IS', saajid: 'canteen', alex: 'pissing about',
+	john: 'retail', mr: 'pissing about'
+}), 'party time!!');
