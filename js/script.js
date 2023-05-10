d2792ecf1415305f6dc248b54ff4923ff032b1d3
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-10 22:38:06 +0300

    Recursion #1 - Factorial (7 kyu)

--- js/script.js
+++ js/script.js
@@ -14765,3 +14765,28 @@ console.log(add(1222, 30277), 31499);
 console.log(add(1236, 30977), 31111013);
 console.log(add(38810, 1383), 391193);
 console.log(add(49999, 49999), 818181818);
+
+// Recursion #1 - Factorial (7 kyu)
+
+/* 
+This is a kata series that you can only solve using recursion.
+##1 - Factorial
+
+In mathematics, the factorial of a non-negative integer n, denoted by n!, 
+is the product of all positive integers less than or equal to n. For example,
+
+5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+The value of 0! is 1.
+
+#Your task
+
+You have to create the function factorial that receives n and returns n!. You have to use recursion.
+*/
+
+const factorial = n => n > 1 ? n * factorial(n - 1) : 1;
+
+console.log(factorial(0), 1);
+console.log(factorial(1), 1);
+console.log(factorial(2), 2);
+console.log(factorial(3), 6);
