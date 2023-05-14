cbca1ce32ac545def2fc8a0f8123cc25e7211635
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-14 21:40:41 +0300

    Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language? (7 kyu)

--- js/script.js
+++ js/script.js
@@ -14964,3 +14964,50 @@ const multiples12 = (m, n) => {
 };
 
 console.log(multiples12(3, 5), [5, 10, 15]);
+
+// Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language? (7 kyu)
+
+/* 
+You will be given an array of objects (associative arrays in PHP, 
+	tables in COBOL) representing data about developers who have signed up to attend 
+	the next coding meetup that you are organising.
+
+Your task is to return either:
+
+true if all developers in the list code in the same language; or
+false otherwise.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+your function should return true.
+
+Notes:
+
+The strings representing a given language will always be formatted in the same way 
+(e.g. 'JavaScript' will always be formatted will upper-case 'J' and 'S'
+The input array will always be valid and formatted as in the example above.
+*/
+
+const isSameLanguage = list => {
+	for (let i = 1; i < list.length; i++) {
+		if (list[i].language !== list[0].language) {
+			return false;
+		}
+	}
+	return true;
+};
+
+console.log(isSameLanguage([
+	{ firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+	{ firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+	{ firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+]), true);
+console.log(isSameLanguage([
+	{ firstName: 'Mariami', lastName: 'G.', country: 'Georgia', continent: 'Europe', age: 29, language: 'Python' },
+	{ firstName: 'Mia', lastName: 'H.', country: 'Germany', continent: 'Europe', age: 39, language: 'Ruby' },
+	{ firstName: 'Maria', lastName: 'I.', country: 'Greece', continent: 'Europe', age: 32, language: 'C' },
+]), false);
