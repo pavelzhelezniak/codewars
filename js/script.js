30d737ca0456cb306570b04faf91d22d867bc935
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-16 22:44:50 +0300

    Holiday III - Fire on the boat (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15032,3 +15032,22 @@ const meeting1 = x => x.indexOf('O') > -1 ? x.indexOf('O') : 'None available!';
 console.log(meeting1(['X', 'O', 'X']), 1);
 console.log(meeting1(['O', 'X', 'X', 'X', 'X']), 0);
 console.log(meeting1(['X', 'X', 'X', 'X', 'X']), 'None available!');
+
+// Holiday III - Fire on the boat (7 kyu)
+
+/* 
+Enjoying your holiday, you head out on a scuba diving trip!
+
+Disaster!! The boat has caught fire!!
+
+You will be provided a string that lists many boat related items. 
+If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+Go to work!
+*/
+
+const fireFight = s => s.replace(/Fire/g, '~~');
+
+console.log(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"), "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast");
+console.log(fireFight("Mast Deck Engine Water Fire"), "Mast Deck Engine Water ~~");
+console.log(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"), "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain");
