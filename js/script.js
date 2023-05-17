a2189014aeebcd183362259e29a3d2edfc5ff327
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-17 20:19:50 +0300

    Between Extremes (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15051,3 +15051,18 @@ const fireFight = s => s.replace(/Fire/g, '~~');
 console.log(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"), "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast");
 console.log(fireFight("Mast Deck Engine Water Fire"), "Mast Deck Engine Water ~~");
 console.log(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"), "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain");
+
+// Between Extremes (7 kyu)
+
+/* 
+Given an array of numbers, return the difference between the largest and smallest values.
+For example:
+[23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+[1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+*/
+
+const betweenExtremes = numbers => Math.max(...numbers) - Math.min(...numbers);
+
+console.log(betweenExtremes([21, 34, 54, 43, 26, 12]), 42);
+console.log(betweenExtremes([-1, -41, -77, -100]), 99);
