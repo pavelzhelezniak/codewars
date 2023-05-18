fdf5a2fd17f3f6b400c17b2561a93cd29c8701db
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-18 21:13:29 +0300

    shorter concat [reverse longer] (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15066,3 +15066,25 @@ const betweenExtremes = numbers => Math.max(...numbers) - Math.min(...numbers);
 
 console.log(betweenExtremes([21, 34, 54, 43, 26, 12]), 42);
 console.log(betweenExtremes([-1, -41, -77, -100]), 99);
+
+// shorter concat [reverse longer] (7 kyu)
+
+/* 
+Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+If a and b have the same length treat a as the longer producing b+reverse(a)+b
+*/
+
+const shorter_reverse_longer = (a, b) => {
+	let max = a.length >= b.length ? a : b;
+	let min = a.length < b.length ? a : b;
+
+	return `${min}${max.split('').reverse().join('')}${min}`
+};
+
+console.log(shorter_reverse_longer("first", "abcde"), "abcdetsrifabcde");
+console.log(shorter_reverse_longer("hello", "bau"), "bauollehbau");
+console.log(shorter_reverse_longer("fghi", "abcde"), "fghiedcbafghi");
