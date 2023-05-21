9bf754034268346fdbe27d1c9957f5cce37b1fd2
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-21 21:34:42 +0300

    Bingo ( Or Not ) (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15105,3 +15105,23 @@ const modifiedSum = (a, n) => a.reduce((acc, item) => acc + item ** n, 0) - a.re
 
 console.log(modifiedSum([1, 2, 3], 3), 30);
 console.log(modifiedSum([1, 2], 5), 30);
+
+// Bingo ( Or Not ) (7 kyu)
+
+/* 
+For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. 
+Duplicate numbers within the array are possible.
+
+Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). 
+Write a function where you will win the game if your numbers can spell "BINGO". 
+They do not need to be in the right order in the input array. 
+Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+*/
+
+const bingo12 = a => {
+	const winSequence = [2, 9, 14, 7, 15];
+	return [...new Set(a)].filter(value => winSequence.includes(value)).length === winSequence.length ? "WIN" : "LOSE";
+};
+
+console.log(bingo12([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), "LOSE");
+console.log(bingo12([21, 13, 2, 7, 5, 14, 7, 15, 9, 10]), "WIN");
