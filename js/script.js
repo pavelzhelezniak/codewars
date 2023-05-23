696307a178a627b97e1004632991009a99ff9521
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-23 19:38:21 +0300

    Dictionary from two lists (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15139,3 +15139,38 @@ const average = scores => Math.round(scores.reduce((acc, item) => (acc + item), 
 
 console.log(average([49, 3, 5, 300, 7]), 73);
 console.log(average([90, 98, 89, 100, 100, 86, 94]), 94);
+
+// Dictionary from two lists (7 kyu)
+
+/* 
+There are two lists, possibly of different lengths. 
+The first one consists of keys, the second one consists of values. 
+Write a function createDict(keys, values) that returns a dictionary created from keys and values. 
+If there are not enough values, the rest of keys should have a None (JS null)value. 
+If there not enough keys, just ignore the rest of values.
+
+Example 1:
+
+keys = ['a', 'b', 'c', 'd']
+values = [1, 2, 3]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3, 'd': null}
+Example 2:
+
+keys = ['a', 'b', 'c']
+values = [1, 2, 3, 4]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}
+*/
+
+const createDict = (keys, values) => {
+	const res = {};
+
+	for (let i = 0; i < keys.length; i++) {
+		values[i] !== undefined ? res[keys[i]] = values[i] : res[keys[i]] = null;
+	}
+
+	return res;
+};
+
+console.log(createDict(['a', 'b', 'c'], [1, 2, 3]), { 'a': 1, 'b': 2, 'c': 3 })
+console.log(createDict(['a', 'b', 'c'], [1, 2, 3, 4]), { 'a': 1, 'b': 2, 'c': 3 })
+console.log(createDict(['a', 'b', 'c', 'd'], [1, 2, 3]), { 'a': 1, 'b': 2, 'c': 3, 'd': null })
