09481c0281fa2de5772bd73479873063a8da9500
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-26 18:10:22 +0300

    makeBackronym (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15230,3 +15230,28 @@ console.log(numberJoy(18), false, 'Harshad but digit sum=9, and 9x9 does not equ
 console.log(numberJoy(1), true, "It was correct")
 console.log(numberJoy(81), true, "It was correct")
 console.log(numberJoy(1458), true, "It was correct")
+
+// makeBackronym (7 kyu)
+
+/* 
+back·ro·nym
+An acronym deliberately formed from a phrase whose initial letters spell out a particular word or words, 
+either to create a memorable name or as a fanciful explanation of a word's origin.
+
+"Biodiversity Serving Our Nation", or BISON
+
+(from https://en.oxforddictionaries.com/definition/backronym)
+
+Complete the function to create backronyms. Transform the given string (without spaces) to a backronym, 
+using the preloaded dictionary and return a string of words, separated with a single space (but no trailing spaces).
+
+The keys of the preloaded dictionary are uppercase letters A-Z and the values are predetermined words, for example:
+
+dict["P"] == "perfect"
+Examples
+"dgm" ==> "disturbing gregarious mustache"
+
+"lkj" ==> "literal klingon joke"
+*/
+
+const makeBackronym = string => string.toUpperCase().split('').map(elem => dict[elem]).join(' ');
