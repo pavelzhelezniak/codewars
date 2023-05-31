6cce4f98756f1e56bd6bf8570e314a59f7a9ecab
pavelzhelezniak <zhele3o1992@gmail.com>
2023-05-31 20:25:58 +0300

    Character Counter (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15320,3 +15320,39 @@ const alternate = (n, firstValue, secondValue) => Array.from({ length: n }, (_, 
 console.log(alternate(5, true, false), [true, false, true, false, true]);
 console.log(alternate(20, 'blue', 'red'), ['blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red']);
 console.log(alternate(0, "lemons", "apples"), []);
+
+// Character Counter (7 kyu)
+
+/* 
+You are going to be given a word. Your job will be to make sure that each character 
+in that word has the exact same number of occurrences. You will return true if it is valid, or false if it is not.
+
+For this kata, capitals are considered the same as lowercase letters. Therefore: "A" == "a"
+
+The input is a string (with no spaces) containing [a-z],[A-Z],[0-9] and common symbols. The length will be 0 < length < 100.
+
+Examples
+"abcabc" is a valid word because "a" appears twice, "b" appears twice, and"c" appears twice.
+"abcabcd" is NOT a valid word because "a" appears twice, "b" appears twice, "c" appears twice, but "d" only appears once!
+"123abc!" is a valid word because all of the characters only appear once in the word.
+*/
+
+const validateWord = s => {
+	s = s.toLowerCase();
+	const counts = {};
+
+	for (let char of s) {
+		if (counts[char] === undefined) {
+			counts[char] = 0;
+		}
+		counts[char]++;
+	}
+	return Math.max(...Object.values(counts)) === Math.min(...Object.values(counts));
+};
+
+console.log(validateWord("abcabc"), true, "The word was: \"abcabc\" \n");
+console.log(validateWord("Abcabc"), true, "The word was: \"Abcabc\" \n");
+console.log(validateWord("abc123"), true, "The word was: \"abc123\" \n");
+console.log(validateWord("abcabcd"), false, "The word was: \"abcabcd\" \n");
+console.log(validateWord("abc!abc!"), true, "The word was: \"abc!abc!\" \n");
+console.log(validateWord("abc:abc"), false, "The word was: \"abc:abc\" \n");
