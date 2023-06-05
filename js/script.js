3f1077d761d4d5411767b1598a966f07aa2109a0
pavelzhelezniak <zhele3o1992@gmail.com>
2023-06-05 18:07:59 +0300

    See You Next Happy Year (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15390,3 +15390,43 @@ const killer = (suspectInfo, dead) => {
 
 console.log(killer({ 'James': ['Jacob', 'Bill', 'Lucas'], 'Johnny': ['David', 'Kyle', 'Lucas'], 'Peter': ['Lucy', 'Kyle'] }, ['Lucas', 'Bill']), 'James');
 console.log(killer({ 'Brad': [], 'Megan': ['Ben', 'Kevin'], 'Finn': [] }, ['Ben']), 'Megan');
+
+// See You Next Happy Year (7 kyu)
+
+/* 
+Scenario
+You're saying good-bye your best friend , See you next happy year .
+
+Happy Year is the year with only distinct digits , (e.g) 2018
+
+Task
+Given a year, Find The next happy year or The closest year You'll see your best friend!alt!alt
+
+Notes
+Year Of Course always Positive .
+Have no fear , It is guaranteed that the answer exists .
+It's not necessary that the year passed to the function is Happy one .
+Input Year with in range (1000  ≤  y  ≤  9000)
+Input >> Output Examples:
+nextHappyYear (7712) ==> return (7801)
+Explanation:
+As the Next closest year with only distinct digits is 7801 .
+
+nextHappyYear (8989) ==> return (9012)
+Explanation:
+As the Next closest year with only distinct digits is 9012 .
+
+nextHappyYear (1001) ==> return (1023)
+Explanation:
+As the Next closest year with only distinct digits is 1023 .
+*/
+
+const nextHappyYear = year => {
+	const newYear = year + 1;
+	const arr = newYear.toString().split('');
+	const uniq = arr.filter((item, pos) => arr.indexOf(item) == pos);
+	if (uniq.length !== arr.length) {
+		return nextHappyYear(newYear);
+	}
+	return newYear;
+};
