46fb2fafd4f48a2df1bcd827e6fc9651d6a1dd08
pavelzhelezniak <zhele3o1992@gmail.com>
2023-06-08 17:56:41 +0300

    Complete The Pattern #2 (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15497,3 +15497,61 @@ console.log(countLettersAndDigits("?"), 0);
 console.log(countLettersAndDigits("12345f%%%t5t&/6"), 10);
 console.log(countLettersAndDigits("aBcDeFg090"), 10);
 console.log(countLettersAndDigits("u_n_d_e_r__S_C_O_R_E"), 10);
+
+// Complete The Pattern #2 (7 kyu)
+
+/* 
+You have to write a function pattern which returns the following Pattern (See Pattern & Examples) upto n number of rows.
+
+Note: Returning the pattern is not the same as Printing the pattern.
+Rules/Note:
+If n < 1 then it should return "" i.e. empty string.
+There are no whitespaces in the pattern.
+Pattern:
+(n)(n-1)(n-2)...4321
+(n)(n-1)(n-2)...432
+(n)(n-1)(n-2)...43
+(n)(n-1)(n-2)...4
+...............
+..............
+(n)(n-1)(n-2)
+(n)(n-1)
+(n)
+Examples:
+pattern(4):
+
+4321
+432
+43
+4
+pattern(11):
+
+1110987654321
+111098765432
+11109876543
+1110987654
+111098765
+11109876
+1110987
+111098
+11109
+1110
+11
+Hint: Use \n in string to jump to next line
+*/
+
+const pattern12 = n => {
+	let number = "";
+	let output = "";
+
+	while (n > 0) {
+		number = number + n;
+		output = number + (output ? "\n" : "") + output;
+		n--;
+	}
+	return output;
+};
+
+console.log(pattern12(1), "1");
+console.log(pattern12(2), "21\n2");
+console.log(pattern12(5), "54321\n5432\n543\n54\n5");
