8a0c1cc1e731e6d4e76a991c628243d9b62e14e5
pavelzhelezniak <zhele3o1992@gmail.com>
2023-06-10 17:15:08 +0300

    Triple trouble (6 kyu)

--- js/script.js
+++ js/script.js
@@ -15593,3 +15593,41 @@ console.log(findMissing([1, 3, 4]), 2);
 console.log(findMissing([1, 4, 7, 10, 16]), 13);
 console.log(findMissing([1, 3, 5, 9, 11]), 7);
 console.log(findMissing([1, 5, 13, 17, 21, 25, 29]), 9);
+
+// Triple trouble (6 kyu)
+
+/* 
+Write a function
+
+tripledouble(num1,num2)
+which takes numbers num1 and num2 and returns 1 if there is a straight triple of a number 
+at any place in num1 and also a straight double of the same number in num2.
+
+If this isn't the case, return 0
+
+Examples
+tripledouble(451999277, 41177722899) == 1 // num1 has straight triple 999s and 
+														// num2 has straight double 99s
+
+tripledouble(1222345, 12345) == 0 // num1 has straight triple 2s but num2 has only a single 2
+
+tripledouble(12345, 12345) == 0
+
+tripledouble(666789, 12345667) == 1
+*/
+
+const tripledouble = (num1, num2) => {
+	for (let i = 0; i < 10; i++) {
+		if (new RegExp(`${i}{3}`).test(num1) && new RegExp(`${i}{2}`).test(num2)) {
+			return 1;
+		}
+	}
+	return 0;
+};
+
+console.log(tripledouble(451999277, 41177722899), 1);
+console.log(tripledouble(1222345, 12345), 0);
+console.log(tripledouble(12345, 12345), 0);
+console.log(tripledouble(666789, 12345667), 1);
+console.log(tripledouble(10560002, 100), 1);
+console.log(tripledouble(1112, 122), 0);
