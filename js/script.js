fc0bff375c275e318c7c3fb9cd9d438f402ac75e
pavelzhelezniak <zhele3o1992@gmail.com>
2023-06-11 21:33:03 +0300

    What century is it? (6 kyu)

--- js/script.js
+++ js/script.js
@@ -15631,3 +15631,38 @@ console.log(tripledouble(12345, 12345), 0);
 console.log(tripledouble(666789, 12345667), 1);
 console.log(tripledouble(10560002, 100), 1);
 console.log(tripledouble(1112, 122), 0);
+
+// What century is it? (6 kyu)
+
+/* 
+Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation.
+
+Examples
+"1999" --> "20th"
+"2011" --> "21st"
+"2154" --> "22nd"
+"2259" --> "23rd"
+"1124" --> "12th"
+"2000" --> "20th"
+*/
+
+const addBC = year => year < 0 ? " BC" : "";
+
+const centurify = year => {
+	const num = Math.ceil(Math.abs(year) / 100).toString()
+	const suffix = num.match(/(11|12|13)$/)
+		? "th" : num.endsWith("1")
+			? "st" : num.endsWith("2")
+				? "nd" : num.endsWith("3")
+					? "rd" : "th";
+
+	return num + suffix + addBC(year)
+};
+
+console.log(centurify("1999"), "20th", "With input '1999' solution produced wrong output");
+console.log(centurify("2011"), "21st", "With input '2011' solution produced wrong output");
+console.log(centurify("2154"), "22nd", "With input '2154' solution produced wrong output");
+console.log(centurify("2259"), "23rd", "With input '2259' solution produced wrong output");
+console.log(centurify("1234"), "13th", "With input '1234' solution produced wrong output");
+console.log(centurify("1023"), "11th", "With input '1023' solution produced wrong output");
+console.log(centurify("2000"), "20th", "With input '2000' solution produced wrong output");
