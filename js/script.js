8e950307daf22be04445403a2c29fa03f7a93928
pavelzhelezniak <zhele3o1992@gmail.com>
2023-06-13 09:53:09 +0300

    Find the next perfect square! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15666,3 +15666,32 @@ console.log(centurify("2259"), "23rd", "With input '2259' solution produced wron
 console.log(centurify("1234"), "13th", "With input '1234' solution produced wrong output");
 console.log(centurify("1023"), "11th", "With input '1023' solution produced wrong output");
 console.log(centurify("2000"), "20th", "With input '2000' solution produced wrong output");
+
+//  Find the next perfect square! (7 kyu)
+
+/* 
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. 
+Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+Examples:(Input --> Output)
+
+121 --> 144
+625 --> 676
+114 --> -1 since 114 is not a perfect square
+*/
+
+const findNextSquare = sq => {
+	const currSq = Math.sqrt(sq);
+	return Number.isInteger(currSq) ? (currSq + 1) ** 2 : -1;
+};
+
+console.log(findNextSquare(121), 144, "Wrong output for 121");
+console.log(findNextSquare(625), 676, "Wrong output for 625");
+console.log(findNextSquare(319225), 320356, "Wrong output for 319225");
+console.log(findNextSquare(15241383936), 15241630849, "Wrong output for 15241383936");
+console.log(findNextSquare(155), -1, "Wrong output for 155");
+console.log(findNextSquare(342786627), -1, "Wrong output for 342786627");
