25d2d94043c5588c42ab5b8b28ff017e48748d90
pavelzhelezniak <zhele3o1992@gmail.com>
2023-06-14 16:22:07 +0300

    Sum of the first nth term of Series (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15695,3 +15695,41 @@ console.log(findNextSquare(319225), 320356, "Wrong output for 319225");
 console.log(findNextSquare(15241383936), 15241630849, "Wrong output for 15241383936");
 console.log(findNextSquare(155), -1, "Wrong output for 155");
 console.log(findNextSquare(342786627), -1, "Wrong output for 342786627");
+
+// Sum of the first nth term of Series (7 kyu)
+
+/* 
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+*/
+
+const SeriesSum = n => {
+	let result = 0;
+	let reverage = 1;
+	for (let i = 0; i < n; i += 1) {
+		if (i === 0) {
+			result = 1;
+		} else {
+			reverage += 3;
+			result = result + (1 / reverage);
+		}
+	}
+	return result.toFixed(2);
+};
+
+console.log(SeriesSum(1), "1.00");
+console.log(SeriesSum(2), "1.25");
+console.log(SeriesSum(3), "1.39");
+console.log(SeriesSum(4), "1.49");
