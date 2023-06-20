966a1968bcf989e648e2ef5d8735cb8e03b2f931
pavelzhelezniak <zhele3o1992@gmail.com>
2023-06-20 13:43:43 +0300

    Speed Control (7 kyu)

--- js/script.js
+++ js/script.js
@@ -15784,3 +15784,40 @@ const calculateYears = (principal, interest, tax, desired) => {
 console.log(calculateYears(1000, 0.05, 0.18, 1100), 3);
 console.log(calculateYears(1000, 0.01625, 0.18, 1200), 14);
 console.log(calculateYears(1000, 0.05, 0.18, 1000), 0);
+
+// Speed Control (7 kyu)
+
+/* 
+In John's car the GPS records every s seconds the distance travelled from an origin 
+(distances are measured in an arbitrary but consistent unit). 
+For example, below is part of a record with s = 15:
+
+x = [0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]
+The sections are:
+
+0.0-0.19, 0.19-0.5, 0.5-0.75, 0.75-1.0, 1.0-1.25, 1.25-1.50, 1.5-1.75, 1.75-2.0, 2.0-2.25
+We can calculate John's average hourly speed on every section and we get:
+
+[45.6, 74.4, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0]
+Given s and x the task is to return as an integer the *floor* of the maximum average speed per hour obtained on the sections of x. 
+If x length is less than or equal to 1 return 0 since the car didn't move.
+
+Example:
+With the above data your function gps(s, x) should return 74
+*/
+
+const gps = (s, x) => {
+	const output = [];
+	if (x.length <= 1) {
+		return 0;
+	}
+
+	for (let i = 0; i < x.length - 1; i++) {
+		output.push((x[i + 1] - x[i]) * 3600 / s);
+	}
+
+	return Math.max(...output);
+};
+
+console.log(gps(20, [0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61]), 41);
+console.log(gps(12, [0.0, 0.11, 0.22, 0.33, 0.44, 0.65, 1.08, 1.26, 1.68, 1.89, 2.1, 2.31, 2.52, 3.25]), 219);
