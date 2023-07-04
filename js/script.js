dcc5fe531a865f1453d9201f878e88377c862321
pavelzhelezniak <zhele3o1992@gmail.com>
2023-07-04 14:57:34 +0300

    Sum of the first nth term of Series (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16080,3 +16080,36 @@ What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
 const perimeterSequence = (a, n) => a * n * 4;
 
 console.log(perimeterSequence(1, 3), 12);
+
+// Sum of the first nth term of Series (7 kyu)
+
+/* 
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+*/
+
+const SeriesSum1 = n => {
+	for (var s = 0, i = 0; i < n; i++) {
+		s += 1 / (1 + i * 3)
+	}
+
+	return s.toFixed(2)
+};
+
+console.log(SeriesSum1(1), "1.00");
+console.log(SeriesSum1(2), "1.25");
+console.log(SeriesSum1(3), "1.39");
+console.log(SeriesSum1(4), "1.49");
+console.log(SeriesSum1(5), "1.57");
