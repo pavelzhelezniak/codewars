1b42375ec2a7a7afe3b1923c2ea5843523146783
pavelzhelezniak <zhele3o1992@gmail.com>
2023-07-05 12:52:31 +0300

    Basic Sequence Practice (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16113,3 +16113,32 @@ console.log(SeriesSum1(2), "1.25");
 console.log(SeriesSum1(3), "1.39");
 console.log(SeriesSum1(4), "1.49");
 console.log(SeriesSum1(5), "1.57");
+
+// Basic Sequence Practice (7 kyu)
+
+/* 
+A sequence or a series, in mathematics, is a string of objects, like numbers, that follow a particular pattern. 
+The individual elements in a sequence are called terms. A simple example is 3, 6, 9, 12, 15, 18, 21, ..., 
+where the pattern is: "add 3 to the previous term".
+
+In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28, ... 
+This sequence is generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
+
+[ 0,  1,    3,      6,   ...]
+  0  0+1  0+1+2  0+1+2+3
+Your Task
+Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of 
+the arithmetic series explained above. Whenn < 0 return the sequence with negative terms.
+
+Examples
+ 5  -->  [0,  1,  3,  6,  10,  15]
+-5  -->  [0, -1, -3, -6, -10, -15]
+ 7  -->  [0,  1,  3,  6,  10,  15,  21,  28]
+*/
+
+const sumOfN = n => Array.from(Array(Math.abs(n) + 1), (el, i) => (i + 1) * i / 2 * (i > 0 ? Math.sign(n) : 1));
+
+console.log(sumOfN(3), [0, 1, 3, 6]);
+console.log(sumOfN(-4), [0, -1, -3, -6, -10]);
+console.log(sumOfN(1), [0, 1]);
+console.log(sumOfN(-6), [0, -1, -3, -6, -10, -15, -21]);
