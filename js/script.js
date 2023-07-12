4d9085111664fb4cba6645fa31164bc95f37fb77
pavelzhelezniak <zhele3o1992@gmail.com>
2023-07-12 20:52:44 +0300

    Drying Potatoes (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16217,4 +16217,68 @@ const dog = new Animal('Max', 'dog');
 console.log(dog.toString());
 console.log(dog.type);
 console.log(dog.name);
-console.log(dog.name = 'Lassie'); 
+console.log(dog.name = 'Lassie');
+
+// Get key/value pairs as arrays (7 kyu)
+
+/* 
+Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+Example:
+
+keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). 
+For extra style, can you get your method to check for objects that extend their prototype?
+*/
+
+const keysAndValues = data => {
+	const kyes = [];
+	const values = [];
+	for (const [key, value] of Object.entries(data)) {
+		kyes.push(key);
+		values.push(value);
+	}
+
+	return [kyes, values];
+};
+
+console.log(keysAndValues({ a: 1, b: 2, c: 3 }), [['a', 'b', 'c'], [1, 2, 3]]);
+console.log(keysAndValues({}), [[], []]);
+console.log(keysAndValues({ 1: 'a', 2: 'b', 3: 'c' }), [['1', '2', '3'], ['a', 'b', 'c']]);
+
+// Drying Potatoes (7 kyu)
+
+/* 
+All we eat is water and dry matter.
+
+Let us begin with an example:
+
+John bought potatoes: their weight is 100 kilograms. 
+Potatoes contain water and dry matter. 
+The water content is 99 percent of the total weight. 
+He thinks they are too wet and puts them in an oven - at low temperature - for them to lose some water.
+
+At the output the water content is only 98%.
+
+What is the total weight in kilograms (water content plus dry matter) coming out of the oven?
+
+He finds 50 kilograms and he thinks he made a mistake: "So much weight lost for such a small change in water content!"
+
+Can you help him?
+
+Task
+Write function potatoes with
+
+int parameter p0 - initial percent of water-
+int parameter w0 - initial weight -
+int parameter p1 - final percent of water -
+potatoesshould return the final weight coming out of the oven w1 truncated as an int.
+
+Example:
+potatoes(99, 100, 98) --> 50
+*/
+
+const potatoes = (p0, w0, p1) => Math.floor(w0 * (100 - p0) / (100 - p1));
+
+console.log(potatoes(82, 127, 80), 114);
+console.log(potatoes(93, 129, 91), 100);
