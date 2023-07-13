5ce678a3cae9da63cef1dc67d140641bcee6ba20
pavelzhelezniak <zhele3o1992@gmail.com>
2023-07-13 10:57:09 +0300

    Caffeine Script (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16282,3 +16282,36 @@ const potatoes = (p0, w0, p1) => Math.floor(w0 * (100 - p0) / (100 - p1));
 
 console.log(potatoes(82, 127, 80), 114);
 console.log(potatoes(93, 129, 91), 100);
+
+// Caffeine Script (7 kyu)
+
+/* 
+Complete the function which takes a non-zero integer as its argument.
+
+If the integer is divisible by 3, return the string "Java".
+
+If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+
+If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+
+If none of the condition is true, return the string "mocha_missing!"
+
+Examples
+1   -->  "mocha_missing!"
+3   -->  "Java"
+6   -->  "JavaScript"
+12  -->  "CoffeeScript"
+*/
+
+const caffeineBuzz = n => {
+	if (n % 12 == 0) {
+		return 'CoffeeScript';
+	}
+	if (n % 6 == 0) {
+		return 'JavaScript';
+	}
+	if (n % 3 == 0) {
+		return 'Java';
+	}
+	return 'mocha_missing!';
+}
