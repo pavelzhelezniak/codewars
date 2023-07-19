34252953c2ce40e869bfdd56aaa2d17ff61ce65b
pavelzhelezniak <zhele3o1992@gmail.com>
2023-07-19 21:16:11 +0300

    Making Copies (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16443,3 +16443,30 @@ console.log(colourAssociation([["white", "goodness"], ["blue", "tranquility"]]),
 	[{ white: "goodness" }, { blue: "tranquility" }]);
 console.log(colourAssociation([["red", "energy"], ["yellow", "creativity"], ["brown", "friendly"], ["green", "growth"]]),
 	[{ red: "energy" }, { yellow: "creativity" }, { brown: "friendly" }, { green: "growth" }]);
+
+// Making Copies (7 kyu)
+
+/* 
+Write a function that takes a list (in Python) or array (in other languages) of numbers, and makes a copy of it.
+
+Note that you may have troubles if you do not return an actual copy, item by item, 
+just a pointer or an alias for an existing list or array.
+
+If not a list or array is given as a parameter in interpreted languages, the function should raise an error.
+
+Examples:
+
+t = [1, 2, 3, 4]
+tCopy = copyList(t)
+t[1] += 5
+t = [1, 7, 3, 4]
+tCopy = [1, 2, 3, 4]
+*/
+
+const copyList = l => [...l];
+const t = [1, 2, 3, 4];
+const tCopy = copyList(t);
+t[1] += 5;
+
+console.log(t);
+console.log(tCopy);
