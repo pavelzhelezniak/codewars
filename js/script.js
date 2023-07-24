a44f9a81c732d89be3fcb8fd56526dbba151ec6f
pavelzhelezniak <zhele3o1992@gmail.com>
2023-07-24 15:03:16 +0300

    Count consonants (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16470,3 +16470,23 @@ t[1] += 5;
 
 console.log(t);
 console.log(tCopy);
+
+// Count consonants (7 kyu)
+
+/* 
+Complete the function that takes a string of English-language text and returns the number of consonants in the string.
+
+Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+*/
+
+const consonantCount = str => str.split('')
+	.filter(letter => 'bcdfghjklmnpqrstvwxyz'.includes(letter.toLowerCase())).length;
+
+console.log(consonantCount(''), 0);
+console.log(consonantCount('aeiouAEIOU'), 0);
+console.log(consonantCount('bcdfghjklmnpqrstvwxyz'), 21);
+console.log(consonantCount('bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'), 42);
+console.log(consonantCount('y'), 1);
+console.log(consonantCount('01234567890_'), 0);
+console.log(consonantCount(' ^&$#'), 0);
+console.log(consonantCount('aeiou AEIOU bcdfghjklmnpqrstvwxyz BCDFGHJKLMNPQRSTVWXYZ 01234567890_ ^&$#'), 42);
