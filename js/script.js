1dc1ec87747ddb5e52cec89bf260fddb784f2998
pavelzhelezniak <zhele3o1992@gmail.com>
2023-07-31 17:48:05 +0300

    Find the missing element between two arrays (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16547,3 +16547,34 @@ const getDecimal = n => Math.abs(n % 1);
 console.log(getDecimal(10), 0);
 console.log(getDecimal(-1.2), 0.2);
 console.log(getDecimal(4.5), 0.5);
+
+// Find the missing element between two arrays (7 kyu)
+
+/* 
+Given two integer arrays where the second array is a shuffled duplicate of the first array with one element missing, 
+find the missing element.
+
+Please note, there may be duplicates in the arrays, 
+so checking if a numerical value exists in one and not the other is not a valid solution.
+
+find_missing([1, 2, 2, 3], [1, 2, 3]) => 2
+find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]) => 8
+The first array will always have at least one element.
+*/
+
+const findMissing1 = (arr1, arr2) => {
+	const arr1Sort = arr1.sort((a, b) => a - b);
+	const arr2Sort = arr2.sort((a, b) => a - b);
+
+	for (let i = 0; i < arr1Sort.length; i++) {
+		if (arr1Sort[i] !== arr2Sort[i]) {
+			return arr1Sort[i];
+		}
+	}
+};
+
+console.log(findMissing1([1, 2, 3], [1, 3]), 2);
+console.log(findMissing1([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]), 8);
+console.log(findMissing1([7], []), 7);
+console.log(findMissing1([4, 3, 3, 61, 8, 8], [8, 61, 8, 3, 4]), 3);
+console.log(findMissing1([0, 0, 0, 0, 0], [0, 0, 0, 0]), 0);
