c762fcc5911c45bd8773b034e7d0b1826c17dd1c
pavelzhelezniak <zhele3o1992@gmail.com>
2023-08-02 14:43:15 +0300

    Split The Bill (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16578,3 +16578,47 @@ console.log(findMissing1([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]), 8);
 console.log(findMissing1([7], []), 7);
 console.log(findMissing1([4, 3, 3, 61, 8, 8], [8, 61, 8, 3, 4]), 3);
 console.log(findMissing1([0, 0, 0, 0, 0], [0, 0, 0, 0]), 0);
+
+// Split The Bill (7 kyu)
+
+/* 
+You're in a restaurant with your friends and it's time to go, but there's still one big problem...the bill. 
+Who will pay what? Lucky for you, you've got your computer handy! One simple function and the bill is paid——fairly, too!
+
+The function should take one parameter: an object/dict with two or 
+more name-value pairs that represent the members of the group and the amount spent by each.
+
+Your function should return an object/dict with the same names, 
+showing how much money the members should pay or receive.
+
+Further points:
+
+The values should be positive numbers if the person should receive money from the group, 
+negative numbers if they owe money to the group.
+If value is a decimal, round to two decimal places.
+Translations and comments (and upvotes!) welcome.
+
+Example
+3 friends go out together: A spends £20, B spends £15, and C spends £10. 
+The function should return an object/dict showing that A should receive £5, B should receive £0, and C should pay £5.
+
+var group = {
+	 A: 20, 
+	 B: 15, 
+	 C: 10
+}
+
+splitTheBill(group) // returns {A: 5, B: 0, C: -5}
+Good Luck and Happy Eats!
+*/
+
+const splitTheBill = x => {
+	const average = Object.values(x).reduce((a, b) => a + b, 0) / Object.values(x).length;
+	const arrKeys = Object.keys(x);
+	const arrValues = Object.values(x).map((a, b) => (a - average));
+
+	return Object.assign(...arrKeys.map((n, i) => ({ [n]: parseFloat((arrValues[i]).toFixed(2)) })));
+};
+
+console.log(splitTheBill({ A: 20, B: 15, C: 10 }), { A: 5, B: 0, C: -5 });
+console.log(splitTheBill({ A: 40, B: 25, X: 10 }), { A: 15, B: 0, X: -15 });
