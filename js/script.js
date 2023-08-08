1e359cc90fdec406c149fa3e71fbdb13623316bd
pavelzhelezniak <zhele3o1992@gmail.com>
2023-08-08 18:31:57 +0300

    Absent vowel (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16644,3 +16644,34 @@ const solution = (fullText, searchText) => {
 console.log(solution('abcdeb', 'b'), 2);
 console.log(solution('abc', 'b'), 1);
 console.log(solution('abbc', 'bb'), 1);
+
+// Absent vowel (7 kyu)
+
+/* 
+Your job is to figure out the index of which vowel is missing from a given string:
+
+A has an index of 0,
+E has an index of 1,
+I has an index of 2,
+O has an index of 3,
+U has an index of 4.
+Notes: There is no need for string validation and every sentence given will contain all vowels but one. 
+Also, you won't need to worry about capitals.
+
+Examples
+"John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
+"Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
+*/
+
+const absentVowel = x => {
+	const vowel = 'AEIOU';
+
+	for (let i = 0; i < vowel.length; i++) {
+		if (!x.toUpperCase().includes(vowel[i])) {
+			return i;
+		}
+	}
+};
+
+console.log(absentVowel("John Doe hs seven red pples under his bsket"), 0);
+console.log(absentVowel("Bb Smith sent us six neatly arranged range bicycles"), 3);
