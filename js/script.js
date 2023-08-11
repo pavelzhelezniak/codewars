26178d5d342dd9829dc60c6c48fbfdb512bdd088
pavelzhelezniak <zhele3o1992@gmail.com>
2023-08-11 12:51:02 +0300

    Dot Calculator (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16735,3 +16735,44 @@ const isNice = arr => arr.length !== 0 ?
 
 console.log(isNice([2, 10, 9, 3]), true);
 console.log(isNice([3, 4, 5, 7]), false);
+
+// Dot Calculator (7 kyu)
+
+/* 
+You have to write a calculator that receives strings for input. The dots will represent the number in the equation. 
+There will be dots on one side, an operator, and dots again after the operator. 
+The dots and the operator will be separated by one space.
+
+Here are the following valid operators :
+
++ Addition
+- Subtraction
+* Multiplication
+// Integer Division
+Your Work (Task)
+You'll have to return a string that contains dots, as many the equation returns. 
+If the result is 0, return the empty string. 
+When it comes to subtraction, the first number will always be greater than or equal to the second number.
+*/
+
+const dotCalculator = equation => {
+	const [firstOperand, operator, secondOperand] = equation.split(' ');
+	const operations = {
+		'+': (a, b) => a + b,
+		'-': (a, b) => a - b,
+		'*': (a, b) => a * b,
+		'//': (a, b) => a / b,
+	}
+
+	return '.'.repeat(operations[operator](firstOperand.length, secondOperand.length));
+};
+
+console.log(dotCalculator("..... + ..............."), "....................");
+console.log(dotCalculator("..... - ..."), "..");
+console.log(dotCalculator("..... - ."), "....");
+console.log(dotCalculator("..... * ..."), "...............");
+console.log(dotCalculator("..... * .."), "..........");
+console.log(dotCalculator("..... // .."), "..");
+console.log(dotCalculator("..... // ."), ".....");
+console.log(dotCalculator(". // .."), "");
+console.log(dotCalculator(". - ."), "");
