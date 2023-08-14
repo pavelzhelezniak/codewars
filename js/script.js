e65b4fcb521378a57d3ee732bf5f8fa2b1c4e7b2
pavelzhelezniak <zhele3o1992@gmail.com>
2023-08-14 14:04:44 +0300

    Partial Word Searching (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16776,3 +16776,30 @@ console.log(dotCalculator("..... // .."), "..");
 console.log(dotCalculator("..... // ."), ".....");
 console.log(dotCalculator(". // .."), "");
 console.log(dotCalculator(". - ."), "");
+
+// Partial Word Searching (7 kyu)
+
+/* 
+Write a method that will search an array of strings for all strings that contain another string, ignoring capitalization. 
+Then return an array of the found strings.
+
+The method takes two parameters, the query string and the array of strings to search, and returns an array.
+
+If the string isn't contained in any of the strings in the array, 
+the method returns an array containing a single string: "Empty" (or Nothing in Haskell, or "None" in Python and C)
+
+Examples
+If the string to search for is "me", and the array to search is ["home", "milk", "Mercury", "fish"], 
+the method should return ["home", "Mercury"].
+*/
+
+const wordSearch = (query, seq) => {
+	const res = seq.filter(item => item.toLowerCase().includes(query.toLowerCase()));
+
+	return res.length ? res : ['Empty'];
+};
+
+console.log(wordSearch("ab", ["za", "ab", "abc", "zab", "zbc"]), ["ab", "abc", "zab"])
+console.log(wordSearch("aB", ["za", "ab", "abc", "zab", "zbc"]), ["ab", "abc", "zab"])
+console.log(wordSearch("ab", ["za", "aB", "Abc", "zAB", "zbc"]), ["aB", "Abc", "zAB"])
+console.log(wordSearch("abcd", ["za", "aB", "Abc", "zAB", "zbc"]), ["Empty"])
