200831b7e46eff02e16a08b2ef3c6473954b8b3e
pavelzhelezniak <zhele3o1992@gmail.com>
2023-08-15 18:18:30 +0300

    First-Class Function Factory (7 kyu)

--- js/script.js
+++ js/script.js
@@ -16803,3 +16803,31 @@ console.log(wordSearch("ab", ["za", "ab", "abc", "zab", "zbc"]), ["ab", "abc", "
 console.log(wordSearch("aB", ["za", "ab", "abc", "zab", "zbc"]), ["ab", "abc", "zab"])
 console.log(wordSearch("ab", ["za", "aB", "Abc", "zAB", "zbc"]), ["aB", "Abc", "zAB"])
 console.log(wordSearch("abcd", ["za", "aB", "Abc", "zAB", "zbc"]), ["Empty"])
+
+// First-Class Function Factory (7 kyu)
+
+/*
+Description:
+Write a function, factory, that takes a number as its parameter and returns another function.
+
+The returned function should take an array of numbers as its parameter, 
+and return an array of those numbers multiplied by the number that was passed into the first function.
+
+In the example below, 5 is the number passed into the first function. 
+So it returns a function that takes an array and multiplies all elements in it by five.
+
+Translations and comments (and upvotes) welcome!
+
+Example
+var fives = factory(5);       // returns a function - fives
+var myArray = [1, 2, 3];
+fives(myArray);               //returns [5, 10, 15];
+*/
+
+const factory = x => array => array.map(a => a * x);
+
+const myArray = [1, 2, 3];
+const threes = factory(3);
+console.log(threes(myArray), [3, 6, 9]);
+const fives = factory(5);
+console.log(fives(myArray), [5, 10, 15]);
