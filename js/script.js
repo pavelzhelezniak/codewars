cb2b86373c2243dc6e78fce1a536d3afffeb2eb4
pavelzhelezniak <zhele3o1992@gmail.com>
2023-08-28 14:33:35 +0300

    Combine objects (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17099,3 +17099,47 @@ const outed = (meet, boss) => {
 console.log(outed({ 'tim': 0, 'jim': 2, 'randy': 0, 'sandy': 7, 'andy': 0, 'katie': 5, 'laura': 1, 'saajid': 2, 'alex': 3, 'john': 2, 'mr': 0 }, 'laura'), 'Get Out Now!');
 console.log(outed({ 'tim': 1, 'jim': 3, 'randy': 9, 'sandy': 6, 'andy': 7, 'katie': 6, 'laura': 9, 'saajid': 9, 'alex': 9, 'john': 9, 'mr': 8 }, 'katie'), 'Nice Work Champ!');
 console.log(outed({ 'tim': 2, 'jim': 4, 'randy': 0, 'sandy': 5, 'andy': 8, 'katie': 6, 'laura': 2, 'saajid': 2, 'alex': 3, 'john': 2, 'mr': 8 }, 'john'), 'Get Out Now!');
+
+// Combine objects (7 kyu)
+
+/* 
+Your task is to write a function that takes two or more objects and returns 
+a new object which combines all the input objects.
+
+All input object properties will have only numeric values. 
+Objects are combined together so that the values of matching keys are added together.
+
+An example:
+
+const objA = { a: 10, b: 20, c: 30 }
+const objB = { a: 3, c: 6, d: 3 }
+combine(objA, objB) // Returns { a: 13, b: 20, c: 36, d: 3 }
+The combine function should be a good citizen, so should not mutate the input objects.
+*/
+
+function combine() {
+	const argsArray = [...arguments];
+	const comboObj = {};
+	for (let i = 0; i < arguments.length; i++) {
+		for (let key in arguments[i]) {
+			if (!comboObj[key]) {
+				comboObj[key] = arguments[i][key];
+			}
+			else {
+				comboObj[key] += arguments[i][key];
+			}
+		}
+	}
+	return comboObj;
+};
+
+const objA = { a: 10, b: 20, c: 30 }
+const objB = { a: 3, c: 6, d: 3 }
+const objC = { a: 5, d: 11, e: 8 }
+const objD = { c: 3 }
+
+
+console.log(combine(objA, objB), { a: 13, b: 20, c: 36, d: 3 });
+console.log(combine(objA, objC), { a: 15, b: 20, c: 30, d: 11, e: 8 });
+console.log(combine(objA, objB, objC), { a: 18, b: 20, c: 36, d: 14, e: 8 });
+console.log(combine(objA, objC, objD), { a: 15, b: 20, c: 33, d: 11, e: 8 });
