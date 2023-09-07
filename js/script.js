5666a528ec56b4601dff60525a1db67c49212689
pavelzhelezniak <zhele3o1992@gmail.com>
2023-09-07 21:29:31 +0300

    Two numbers are positive (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17309,3 +17309,29 @@ console.log(validParentheses("())(()"), false);
 console.log(validParentheses(")()"), false);
 console.log(validParentheses(")"), false);
 console.log(validParentheses(""), true);
+
+// Two numbers are positive (7 kyu)
+
+/* 
+Your job is to write a function, which takes three integers a, b, 
+and c as arguments, and returns True if exactly two of of the three integers are 
+positive numbers (greater than zero), and False - otherwise.
+
+Examples:
+twoArePositive(2, 4, -3) == true
+twoArePositive(-4, 6, 8) == true
+twoArePositive(4, -6, 9) == true
+twoArePositive(-4, 6, 0) == false
+twoArePositive(4, 6, 10) == false
+twoArePositive(-14, -3, -4) == false
+*/
+
+const twoArePositive = (a, b, c) => [a, b, c].filter(item => item > 0).length === 2;
+
+console.log(twoArePositive(2, 4, -3), true, '(2, 4, -3)');
+console.log(twoArePositive(-4, 6, 8), true, '(-4, 6, 8)');
+console.log(twoArePositive(4, -6, 9), true, '(4, -6, 9)');
+console.log(twoArePositive(4, 6, 0), true, '(4, 6, 0)');
+console.log(twoArePositive(-4, 6, 0), false, '(-4, 6, 0)');
+console.log(twoArePositive(4, 6, 10), false, '(4, 6, 10)');
+console.log(twoArePositive(-14, -3, -4), false, '(-14, -3, -4)');
