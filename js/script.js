69886beec786f630e734d6b82e7e2c2f7dbd6cd4
pavelzhelezniak <zhele3o1992@gmail.com>
2023-09-11 20:30:33 +0300

    Return a sorted list of objects (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17422,3 +17422,42 @@ numbers(1, 3, NaN); // true
 */
 
 const numbers12 = (...args) => args.every(arg => typeof arg === 'number');
+
+// Return a sorted list of objects (7 kyu)
+
+/* 
+You'll be passed an array of objects (list) - you must sort them in descending order 
+based on the value of the specified property (sortBy).
+
+Example
+When sorted by "a", this:
+
+[
+  {"a": 1, "b": 3},
+  {"a": 3, "b": 2},
+  {"a": 2, "b": 40},
+  {"a": 4, "b": 12}
+]
+should return:
+
+[
+  {"a": 4, "b": 12},
+  {"a": 3, "b": 2},
+  {"a": 2, "b": 40},
+  {"a": 1, "b": 3}
+]
+*/
+
+const sortList = (sortBy, list) => list.sort((a, b) => b[sortBy] - a[sortBy]);
+
+console.log(sortList('a', [
+	{ "a": 1, "b": 3 },
+	{ "a": 3, "b": 2 },
+	{ "a": 2, "b": 40 },
+	{ "a": 4, "b": 12 }
+]), [
+	{ "a": 4, "b": 12 },
+	{ "a": 3, "b": 2 },
+	{ "a": 2, "b": 40 },
+	{ "a": 1, "b": 3 }
+]);
