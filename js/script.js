7b2700acecaf2f4b39d8939117f6ab0c8711b972
pavelzhelezniak <zhele3o1992@gmail.com>
2023-09-12 19:10:26 +0300

    Find Your Villain Name (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17461,3 +17461,58 @@ console.log(sortList('a', [
 	{ "a": 2, "b": 40 },
 	{ "a": 1, "b": 3 }
 ]);
+
+// Find Your Villain Name (7 kyu)
+
+/* 
+Create a function that returns a villain name based on the user's birthday. 
+The birthday will be passed to the function as a valid Date object, so for simplicity, 
+there's no need to worry about converting strings to dates.
+
+The first name will come from the month, and the last name will come from the last digit of the date:
+
+Month -> first name
+
+January -> "The Evil"
+February -> "The Vile"
+March -> "The Cruel"
+April -> "The Trashy"
+May -> "The Despicable"
+June -> "The Embarrassing"
+July -> "The Disreputable"
+August -> "The Atrocious"
+September -> "The Twirling"
+October -> "The Orange"
+November -> "The Terrifying"
+December -> "The Awkward"
+Last digit of date -> last name
+
+0 -> "Mustache"
+1 -> "Pickle"
+2 -> "Hood Ornament"
+3 -> "Raisin"
+4 -> "Recycling Bin"
+5 -> "Potato"
+6 -> "Tomato"
+7 -> "House Cat"
+8 -> "Teaspoon"
+9 -> "Laundry Basket"
+The returned value should be a string in the form of "First Name Last Name".
+
+For example, a birthday of November 18 would return "The Terrifying Teaspoon"
+*/
+
+const getVillainName = birthday => {
+	const firstNames = ["The Evil", "The Vile", "The Cruel", "The Trashy", "The Despicable", "The Embarrassing", "The Disreputable", "The Atrocious", "The Twirling", "The Orange", "The Terrifying", "The Awkward"];
+	const lastNames = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"];
+	return `${firstNames[birthday.getMonth()]} ${lastNames[birthday.getDate() % 10]}`
+};
+
+//may 3
+console.log(getVillainName(new Date("May 3")), "The Despicable Raisin", "Expected result: The Despicable Raisin; Your result: " + getVillainName(new Date("May 13")));
+
+//apr 1
+console.log(getVillainName(new Date("April 21")), "The Trashy Pickle", "Expected result: The Trashy Pickle; Your result: " + getVillainName(new Date("April 21")));
+
+//dec 7
+console.log(getVillainName(new Date("December 17")), "The Awkward House Cat", "Expected result: The Awkward House Cat; Your result: " + getVillainName(new Date("December 17")));
