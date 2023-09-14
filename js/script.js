daf75dd08216317b04e0b52988d3f76a9ae37b46
pavelzhelezniak <zhele3o1992@gmail.com>
2023-09-14 15:25:59 +0300

    Find the index of the second occurrence of a letter in a string (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17516,3 +17516,33 @@ console.log(getVillainName(new Date("April 21")), "The Trashy Pickle", "Expected
 
 //dec 7
 console.log(getVillainName(new Date("December 17")), "The Awkward House Cat", "Expected result: The Awkward House Cat; Your result: " + getVillainName(new Date("December 17")));
+
+// Find the index of the second occurrence of a letter in a string (7 kyu)
+
+/* 
+In this kata, you need to write a function that takes a string and a letter as input and then returns 
+the index of the second occurrence of that letter in the string. 
+If there is no such letter in the string, then the function should return -1. 
+If the letter occurs only once in the string, then -1 should also be returned.
+
+Examples:
+
+secondSymbol('Hello world!!!','l')  --> 3
+secondSymbol('Hello world!!!', 'A') --> -1
+Good luck ;) And don't forget to rate this Kata if you liked it.
+*/
+
+const secondSymbol = (str, symbol) => {
+	let i = str.indexOf(symbol);
+	if (i >= 0) {
+		i = str.indexOf(symbol, i + 1);
+	}
+
+	return i;
+};
+
+console.log(secondSymbol('Hello world!!!', 'l'), 3, 'Find the index of the second symbol "l" in the string');
+console.log(secondSymbol('Hello world!!!', 'o'), 7, 'Find the index of the second symbol "o" in the string');
+console.log(secondSymbol('Hello world!!!', 'A'), -1, 'The symbol "A" is not in the string');
+console.log(secondSymbol('', 'q'), -1, 'The symbol "q" is not in the string');
+console.log(secondSymbol('Hello', '!'), -1, 'The symbol "!" is not in the string');
