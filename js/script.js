4f8f2c702963d20ce24d4070fbf41f6cf9937f0f
pavelzhelezniak <zhele3o1992@gmail.com>
2023-09-20 14:42:42 +0300

    LinkedList -> Array (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17642,3 +17642,39 @@ const fixedCases = [
 for (let [input, expected] of fixedCases) {
 	console.log(sumNoDuplicates(input), expected);
 }
+
+// LinkedList -> Array (7 kyu)
+
+/* 
+Linked lists are data structures composed of nested or chained objects, 
+each containing a single value and a reference to the next object.
+
+Here's an example of a list:
+
+{value: 1, next: {value: 2, next: {value: 3, next: null}}}
+Write a function listToArray (or list_to_array in Python) that converts a list to an array, like this:
+
+[1, 2, 3]
+Assume all inputs are valid lists with at least one value. For the purpose of simplicity, 
+all values will be either numbers, strings, or Booleans.
+*/
+
+const listToArray = list => {
+	const result = [];
+
+	for (const key in list) {
+		if (typeof list[key] === 'object') {
+			result.push(listToArray(list[key]))
+		} else {
+			result.push(list[key])
+		}
+	}
+
+	return result.flat();
+};
+
+const list123456 = { value: 1, next: { value: 2, next: { value: 3, next: null } } };
+const list2345 = { value: "foo", next: { value: "bar", next: null } };
+
+console.log(listToArray(list123456), [1, 2, 3]);
+console.log(listToArray(list2345), ["foo", "bar"]);
