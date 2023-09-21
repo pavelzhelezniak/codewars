b9c950c7ddf080794a02b6b4d08ca7e91f10c855
pavelzhelezniak <zhele3o1992@gmail.com>
2023-09-21 19:13:17 +0300

    Sort Santa's Reindeer (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17678,3 +17678,48 @@ const list2345 = { value: "foo", next: { value: "bar", next: null } };
 
 console.log(listToArray(list123456), [1, 2, 3]);
 console.log(listToArray(list2345), ["foo", "bar"]);
+
+// Sort Santa's Reindeer (7 kyu)
+
+/* 
+Happy Holidays fellow Code Warriors!
+Now, Dasher! Now, Dancer! Now, Prancer, and Vixen! On, Comet! On, Cupid! On, 
+Donder and Blitzen! That's the order Santa wanted his reindeer...right? 
+What do you mean he wants them in order by their last names!? Looks like we need your help Code Warrior!
+
+Sort Santa's Reindeer
+Write a function that accepts a sequence of Reindeer names, 
+and returns a sequence with the Reindeer names sorted by their last names.
+
+Notes:
+It's guaranteed that each string is composed of two words
+In case of two identical last names, keep the original order
+Examples
+For this input:
+
+[
+  "Dasher Tonoyan", 
+  "Dancer Moore", 
+  "Prancer Chua", 
+  "Vixen Hall", 
+  "Comet Karavani",        
+  "Cupid Foroutan", 
+  "Donder Jonker", 
+  "Blitzen Claus"
+]
+*/
+
+const sortReindeer = reindeerNames =>
+	reindeerNames.sort((a, b) =>
+		a.split(' ')[1].localeCompare(b.split(' ')[1]));
+
+
+const input12349 = [
+	"Dasher Tonoyan", "Dancer Moore", "Prancer Chua", "Vixen Hall",
+	"Comet Karavani", "Cupid Foroutan", "Donder Jonker", "Blitzen Claus"
+];
+const output129 = [
+	"Prancer Chua", "Blitzen Claus", "Cupid Foroutan", "Vixen Hall",
+	"Donder Jonker", "Comet Karavani", "Dancer Moore", "Dasher Tonoyan"
+];
+console.log(sortReindeer(input12349), output129);
