e129a8e40947994fff48dda6c141c8bb93296962
pavelzhelezniak <zhele3o1992@gmail.com>
2023-09-24 20:16:55 +0300

    Ninja vs Samurai: Strike (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17723,3 +17723,31 @@ const output129 = [
 	"Donder Jonker", "Comet Karavani", "Dancer Moore", "Dasher Tonoyan"
 ];
 console.log(sortReindeer(input12349), output129);
+
+// Ninja vs Samurai: Strike (7 kyu)
+
+/* 
+Something is wrong with our Warrior class. The strike method does not work correctly. 
+The following shows an example of this code being used:
+
+var ninja = new Warrior('Ninja');
+var samurai = new Warrior('Samurai');
+
+samurai.strike(ninja, 3);
+// ninja.health should == 70
+Can you figure out what is wrong?
+*/
+
+const Warrior = function (name) {
+	this.name = name;
+	this.health = 100;
+}
+
+Warrior.prototype.strike = function (enemy, swings) {
+	return enemy.health = Math.max(0, enemy.health - (swings * 10));
+}
+
+const ninja = new Warrior('Ninja');
+const samurai = new Warrior('Samurai');
+
+console.log(samurai.strike(ninja, 3));
