754dfb16a5ac0e11654889b9feb6a9c0e52a9180
pavelzhelezniak <zhele3o1992@gmail.com>
2023-10-03 15:54:58 +0300

    Arithmetic List! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17892,3 +17892,33 @@ const list191 = [
 const answer = { vegetarian: 2, standard: 1, vegan: 1 };
 
 console.log(orderFood(list191), answer);
+
+// Arithmetic List! (7 kyu)
+
+/* 
+In this kata, you will write an arithmetic list which is basically a list that contains consecutive terms in the sequence.
+You will be given three parameters :
+
+first the first term in the sequence
+c the constant that you are going to ADD ( since it is an arithmetic sequence...)
+l the number of terms that should be returned
+Useful link: Sequence
+
+Be sure to check out my Arithmetic sequence Kata first ;)
+Don't forget about the indexing pitfall ;)
+*/
+
+const seqlist = (first, c, l) => {
+	const res = [];
+	let count = 0;
+
+	while (count < l) {
+		res.push(first);
+		first += c;
+		count++;
+	}
+
+	return res;
+};
+
+console.log(seqlist(0, 1, 20), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
