bc54a93013b1e8d44f56060ac9ffe8a6dc937f8c
pavelzhelezniak <zhele3o1992@gmail.com>
2023-10-04 15:23:57 +0300

    Arithmetic List! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17922,3 +17922,22 @@ const seqlist = (first, c, l) => {
 };
 
 console.log(seqlist(0, 1, 20), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+
+// Use map() to double the values in an array (7 kyu)
+
+/* 
+Make the double() function return a new array with each value twice as large as the corresponding value in the passed in array. 
+Your solution must use the map() function of the built-in javascript Array object. 
+If you're not familiar with map(), reading over the documentation may help.
+
+For example:
+
+var someNumbers = [1,2,10,57];
+
+double(someNumbers); // should return [2,4,20,114]
+*/
+
+const double = array => array.map(item => item * 2);
+
+console.log(double([1, 2, 3, 4, 5]), [2, 4, 6, 8, 10]);
+console.log(double([71, -548, 12.3, 31415]), [142, -1096, 24.6, 62830]);
