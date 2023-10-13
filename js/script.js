9182101260385b9d11f7601e166ff3283fda52d9
pavelzhelezniak <zhele3o1992@gmail.com>
2023-10-13 19:30:42 +0300

    Ghostbusters (whitespace removal) (7 kyu)

--- js/script.js
+++ js/script.js
@@ -17980,3 +17980,34 @@ console.log(sumOfABeach("weoqipurpoqwuirpousandiupqwoieurioweuwateruierqpoiweurp
 console.log(sumOfABeach("sAnDsandwaTerwatErfishFishsunsunsandwater"), 10);
 console.log(sumOfABeach("joifjepiojfoiejfoajoijawoeifjowejfjoiwaefjiaowefjaofjwoj fawojef "), 0);
 console.log(sumOfABeach("jwefjwjfsandsandwaterwaterfishfishsunsunsandwateriojwhefa;jawof;jawio;f"), 10);
+
+// Ghostbusters (whitespace removal) (7 kyu)
+
+/* 
+Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+Example:
+
+ghostBusters("Sky scra per");
+Should return:
+
+"Skyscraper"
+If the building contains no ghosts, return the string:
+
+"You just wanted my autograph didn't you?"
+*/
+
+const ghostBusters = building => {
+	if (building.includes(' ')) {
+		return building.split(' ').join('');
+	}
+	return 'You just wanted my autograph didn\'t you?'
+}
+
+console.log(ghostBusters("Factor y") === "Factory", 'Nope, there may still be a ghost in the building. Try again.');
+console.log(ghostBusters("O  f fi ce") === "Office", 'Nope, there may still be a ghost in the building. Try again.');
+console.log(ghostBusters("BusStation") === "You just wanted my autograph didn't you?", 'Nope, as there were no ghosts in the BusStation you need to return a witty retort.');
