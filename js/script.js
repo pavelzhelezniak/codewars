f1a4428d19a2f965a1d2a115ceb457af815b041a
pavelzhelezniak <zhele3o1992@gmail.com>
2023-10-15 08:59:43 +0300

    Nth Root of a Number (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18033,3 +18033,24 @@ console.log(isNegativeZero(-4), false);
 console.log(isNegativeZero(-3), false);
 console.log(isNegativeZero(-2), false);
 console.log(isNegativeZero(-1), false);
+
+// Nth Root of a Number (7 kyu)
+
+/* 
+Given two numbers x and n, calculate the (positive) nth root of x; this means that being r = result, r^n = x
+
+Examples
+x = 4     n = 2  -->  2    # the square root of 4 is 2     2^2 = 4
+x = 8     n = 3  -->  2    # the cube root of 8 is 2       2^3 = 8
+x = 256   n = 4  -->  4    # the 4th root of 256 is 4      4^4 = 256
+x = 9     n = 2  -->  3    # the square root of 9 is 3     3^2 = 9
+x = 6.25  n = 2  -->  2.5  #                             2.5^2 = 6.25
+Notes:
+4 <= x < 10 ^ 20
+4 <= n <= 50
+*/
+
+const root=(x, n)=> Math.pow(x, 1 / n);
+
+console.log(root(8, 3), 2, "Wrong value");
+console.log(root(6.25, 2), 2.5, "Wrong value");
