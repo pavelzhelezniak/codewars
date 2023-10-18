ef5c63419bec2748ff3d30935f4b78cc0e48a852
pavelzhelezniak <zhele3o1992@gmail.com>
2023-10-18 13:08:52 +0300

    Determine if the poker hand is flush (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18098,4 +18098,30 @@ console.log(alphabetic("codewars"), false);
 console.log(alphabetic("door"), true);
 console.log(alphabetic("cell"), true);
 console.log(alphabetic("z"), true);
-console.log(alphabetic(""), true);    
+console.log(alphabetic(""), true);
+
+// Determine if the poker hand is flush (7 kyu)
+
+/* 
+Determine if the poker hand is flush, meaning if the five cards are of the same suit.
+
+Your function will be passed a list/array of 5 strings, each representing a poker card in the format "5H" (5 of hearts), 
+meaning the value of the card followed by the initial of its suit (Hearts, Spades, Diamonds or Clubs). No jokers included.
+
+Your function should return true if the hand is a flush, false otherwise.
+
+The possible card values are 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+
+Examples
+["AS", "3S", "9S", "KS", "4S"]  ==> true
+
+["AD", "4S", "7H", "KS", "10S"] ==> false
+*/
+
+const isFlush = cards => cards.filter((item, _i, arr) => item[item.length - 1] === arr[0][arr[0].length - 1]).length === cards.length;
+
+console.log(isFlush(["AS", "3S", "9S", "KS", "4S"]), true);
+console.log(isFlush(["AD", "4S", "7H", "KC", "5S"]), false);
+console.log(isFlush(["AD", "4S", "10H", "KC", "5S"]), false);
+console.log(isFlush(["QD", "4D", "10D", "KD", "5D"]), true);
+console.log(isFlush(["10D", "4D", "QD", "KD", "5D"]), true);
