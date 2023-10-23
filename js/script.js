dd587dac0f32de21542c84516f098b7e60543f31
pavelzhelezniak <zhele3o1992@gmail.com>
2023-10-23 13:05:10 +0300

    Every archer has its arrows (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18125,3 +18125,28 @@ console.log(isFlush(["AD", "4S", "7H", "KC", "5S"]), false);
 console.log(isFlush(["AD", "4S", "10H", "KC", "5S"]), false);
 console.log(isFlush(["QD", "4D", "10D", "KD", "5D"]), true);
 console.log(isFlush(["10D", "4D", "QD", "KD", "5D"]), true);
+
+// Every archer has its arrows (7 kyu)
+
+/* 
+#It's show time! Archers have gathered from all around the world to participate in the Arrow Function Faire. 
+But the faire will only start if there are archers signed and if they all have enough arrows in their quivers - 
+at least 5 is the requirement! Are all the archers ready?
+
+#Reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+#Argument archers is an array of integers, in which each element corresponds to the number of arrows each archer has.
+
+#Return Your function must return true if the requirements are met or false otherwise.
+
+#Examples archersReady([1, 2, 3, 4, 5, 6, 7, 8]) returns false because there are archers with not enough arrows.
+
+archersReady([5, 6, 7, 8]) returns true.
+*/
+
+const archersReady = archers => archers.length === 0 ? false : archers.every(item => item >= 5);
+
+console.log(archersReady([]), false, "Should handle no archers");
+console.log(archersReady([1, 2, 3, 4]), false, "Should handle unprepared archers");
+console.log(archersReady([5, 6, 7, 8]), true, "Should handle prepared archers");
+console.log(archersReady([1, 2, 3, 4, 5, 6, 7, 8]), false, "Should handle mixed archers");
