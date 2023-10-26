df788932f608a82293e667ca41a8a03e8b6a3bd2
pavelzhelezniak <zhele3o1992@gmail.com>
2023-10-26 15:25:58 +0300

    Largest Square Inside A Circle (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18162,3 +18162,15 @@ const squareSum = numbers => numbers.reduce((acc, item) => acc + item ** 2, 0);
 console.log(squareSum([1, 2]), 5);
 console.log(squareSum([0, 3, 4, 5]), 50);
 console.log(squareSum([]), 0);
+
+// Largest Square Inside A Circle (7 kyu)
+
+/* 
+Determine the area of the largest square that can fit inside a circle with radius r.
+*/
+
+const areaLargestSquare = r => 2 * (r ** 2);
+
+console.log(areaLargestSquare(5), 50);
+console.log(areaLargestSquare(7), 98);
+console.log(areaLargestSquare(15), 450);
