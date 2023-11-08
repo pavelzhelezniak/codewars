8c315005bed612f38aab2c20fca275872c9a7f70
pavelzhelezniak <zhele3o1992@gmail.com>
2023-11-08 11:43:38 +0300

    Thinkful - String Drills: Quotable (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18344,3 +18344,27 @@ console.log(reverseBits(0), 0);
 console.log(reverseBits(2017), 1087);
 console.log(reverseBits(1023), 1023);
 console.log(reverseBits(1024), 1);
+
+// Thinkful - String Drills: Quotable (7 kyu)
+
+/* 
+This function should take two string parameters: a person's name (name) and a quote of theirs (quote), 
+and return a string attributing the quote to the person in the following format:
+
+'[name] said: "[quote]"'
+For example, if name is 'Grae' and 'quote' is 'Practice makes perfect' then your function should return the string
+
+'Grae said: "Practice makes perfect"'
+Unfortunately, something is wrong with the instructions in the function body. 
+Your job is to fix it so the function returns correctly formatted quotes.
+
+Click the "Train" button to get started, and be careful with your quotation marks.
+*/
+
+const quotable = (name, quote) => `${name} said: "${quote}"`;
+
+console.log(quotable('Grae', 'Practice makes perfect'), 'Grae said: "Practice makes perfect"')
+console.log(quotable('Dan', 'Get back to work, Grae'), 'Dan said: "Get back to work, Grae"')
+console.log(quotable('Alex', 'Ruby is great fun'), 'Alex said: "Ruby is great fun"')
+console.log(quotable('Bethany', 'Yes, way more fun than R'), 'Bethany said: "Yes, way more fun than R"')
+console.log(quotable('Darrell', 'What the heck is this thing?'), 'Darrell said: "What the heck is this thing?"')
