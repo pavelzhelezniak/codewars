92323dde3b18b599a263bb9ffbe6c7700841ab05
pavelzhelezniak <zhele3o1992@gmail.com>
2023-11-19 18:18:52 +0300

    Multiples! (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18585,3 +18585,25 @@ console.log(modifyMultiply("Creativity is the process of having original ideas t
 console.log(modifyMultiply("Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence", 1, 1), "means");
 console.log(modifyMultiply("Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.", 6, 8), "ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance");
 console.log(modifyMultiply("Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.", 2, 5), "around-around-around-around-around");
+
+// Multiples! (7 kyu)
+
+/* 
+Make a program that takes a value (x) and returns "Bang" if the number is divisible by 3, 
+"Boom" if it is divisible by 5, "BangBoom" if it divisible by 3 and 5, and "Miss" if it isn't divisible by any of them. 
+Note: Your program should only return one value
+
+Ex: Input: 105 --> Output: "BangBoom" Ex: Input: 9 --> Output: "Bang" Ex:Input: 25 --> Output: "Boom"
+*/
+
+const multiple = x => x % 3 === 0 && x % 5 === 0 ? "BangBoom" :
+	x % 3 === 0 ? "Bang" :
+		x % 5 === 0 ? "Boom" :
+			"Miss";
+
+console.log(multiple(30), "BangBoom")
+console.log(multiple(3), "Bang")
+console.log(multiple(98), "Miss")
+console.log(multiple(65), "Boom")
+console.log(multiple(23), "Miss")
+console.log(multiple(15), "BangBoom")
