8a0aa3da7bc6703e08900b65bc2f395f2449779b
pavelzhelezniak <zhele3o1992@gmail.com>
2023-11-21 20:59:24 +0300

    Re-open class (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18607,3 +18607,28 @@ console.log(multiple(98), "Miss")
 console.log(multiple(65), "Boom")
 console.log(multiple(23), "Miss")
 console.log(multiple(15), "BangBoom")
+
+// Re-open class (7 kyu)
+
+/* 
+Ruby and Javascript let you re-open classes so you can add new functionalities to existing classes and objects.
+
+To demonstrate this, you'll have to add a new method (called my_new_method in Ruby or myNewMethod in JS) 
+into the String class that simply calls the upcase method (toUpperCase() in Javascript), so that:
+
+Ruby
+
+"abc".my_new_method
+JS
+
+"abc".myNewMethod();
+returns
+
+"ABC"
+*/
+
+String.prototype.myNewMethod = function () {
+	return this.toUpperCase();
+};
+
+console.log("abc".myNewMethod(), "ABC", '"abc".myNewMethod() should return "ABC"');
