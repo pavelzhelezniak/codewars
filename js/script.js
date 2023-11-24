111ce91ea90e3608fe4bbed19694b75e307bd527
pavelzhelezniak <zhele3o1992@gmail.com>
2023-11-24 16:24:32 +0300

    Filter Coffee (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18684,3 +18684,24 @@ const tailSwap = arr => {
 
 console.log(tailSwap(['abc:123', 'cde:456']), ['abc:456', 'cde:123']);
 console.log(tailSwap(['a:12345', '777:xyz']), ['a:xyz', '777:12345']);
+
+// Filter Coffee (7 kyu)
+
+/* 
+You love coffee and want to know what beans you can afford to buy it.
+
+The first argument to your search function will be a number which represents your budget.
+
+The second argument will be an array of coffee bean prices.
+
+Your 'search' function should return the stores that sell coffee within your budget.
+
+The search function should return a string of prices for the coffees beans you can afford. 
+The prices in this string are to be sorted in ascending order.
+*/
+
+const search = (budget, prices) => prices.filter(item => item <= budget).sort((a, b) => a - b).join(',');
+
+console.log(search(3, [6, 1, 2, 9, 2]), "1,2,2")
+console.log(search(14, [7, 3, 23, 9, 14, 20, 7]), "3,7,7,9,14")
+console.log(search(0, [6, 1, 2, 9, 2]), "") 
