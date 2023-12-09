c501bc67b73592fca094607a50ea93e71a10e60c
pavelzhelezniak <zhele3o1992@gmail.com>
2023-12-09 20:28:14 +0300

    Enumerable Magic #5- True for Just One? (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18855,3 +18855,24 @@ console.log(OrderPeople([{ age: 83, name: 'joel' },
 	{ age: 83, name: "joel" },
 	{ age: 99, name: "vinny" }],
 	'The array is still empty');
+
+// Enumerable Magic #5- True for Just One? (7 kyu)
+
+/* 
+Create a function called one that accepts two params:
+
+a sequence
+a function
+and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+
+Example
+one([1, 3, 5, 6, 99, 1, 3], bigger_than_ten) -> true
+one([1, 3, 5, 6, 99, 88, 3], bigger_than_ten) -> false
+one([1, 3, 5, 6, 5, 1, 3], bigger_than_ten) -> false
+*/
+
+const one1 = (arr, fun) => arr.filter(fun).length === 1;
+
+console.log(one1([1, 2, 3, 4, 5], function (item) { return item < 2 }), true)
+console.log(one1([1, 2, 3, 4, 5], function (item) { return item % 2 }), false, "more than 1 item are odd")
+console.log(one1([1, 2, 3, 4, 5], function (item) { return item > 5 }), false, "none item is greater than 5")
