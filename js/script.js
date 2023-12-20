cb3a9ee298cfac9a4ecd23feb886cdfe9af2ff5b
pavelzhelezniak <zhele3o1992@gmail.com>
2023-12-20 17:30:46 +0300

    99 Problems, #1: last in list (7 kyu)

--- js/script.js
+++ js/script.js
@@ -18938,3 +18938,20 @@ returns "Hello World"
 const say = string1 => string2 => `${string1} ${string2}`;
 
 console.log(say('Hello')('World'), 'Hello World');
+
+// 99 Problems, #1: last in list (7 kyu)
+
+/* 
+Write a function last that accepts a list and returns the last element in the list.
+
+If the list is empty:
+
+In languages that have a built-in option or result type (like OCaml or Haskell), return an empty option
+
+In languages that do not have an empty option, just return null
+*/
+
+const last9 = xs => xs.length === 0 ? null : xs[xs.length - 1];
+
+console.log(last9([1, 2, 3]), 3, "last([1,2,3]) == 3");
+console.log(last9([]), null, "last( [] ) == null");
