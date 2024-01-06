5b099840a716e24d0383b5e2facc4fd99c538095
pavelzhelezniak <zhele3o1992@gmail.com>
2024-01-06 14:16:43 +0300

    Sort My Animals (7 kyu)

--- js/script.js
+++ js/script.js
@@ -19091,3 +19091,40 @@ console.log(dominantArrayElement([75, 47, 42, 56, 13, 55]), [75, 56, 55]);
 console.log(dominantArrayElement([67, 54, 27, 85, 66, 88, 31, 24, 49]), [88, 49]);
 console.log(dominantArrayElement([76, 17, 25, 36, 29]), [76, 36, 29]);
 console.log(dominantArrayElement([104, 18, 37, 9, 36, 47, 28]), [104, 47, 28]);
+
+// Sort My Animals (7 kyu)
+
+/* 
+Consider the following class:
+
+var Animal = { 
+    name: "Cat", 
+    numberOfLegs: 4 
+}
+Write a method that accepts a list of objects of type Animal, and returns a new list. 
+The new list should be a copy of the original list, sorted first by the animal's number of legs, 
+and then by its name.
+
+If an empty list is passed in, it should return an empty list back.
+*/
+
+const sortAnimal = animals => {
+	const sortAnimals = [...animals]
+	return animals ? sortAnimals.sort((a, b) => a.numberOfLegs - b.numberOfLegs || a.name.localeCompare(b.name)) : null;
+};
+
+console.log(sortAnimal([
+	{ name: "Cat", numberOfLegs: 4 },
+	{ name: "Snake", numberOfLegs: 0 },
+	{ name: "Dog", numberOfLegs: 4 },
+	{ name: "Pig", numberOfLegs: 4 },
+	{ name: "Human", numberOfLegs: 2 },
+	{ name: "Bird", numberOfLegs: 2 }
+]), [
+	{ name: 'Snake', numberOfLegs: 0 },
+	{ name: 'Bird', numberOfLegs: 2 },
+	{ name: 'Human', numberOfLegs: 2 },
+	{ name: 'Cat', numberOfLegs: 4 },
+	{ name: 'Dog', numberOfLegs: 4 },
+	{ name: 'Pig', numberOfLegs: 4 }
+]);
