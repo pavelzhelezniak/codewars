a2f86c0ce93159ed4cddeb18d0a21e3fd55ef04a
pavelzhelezniak <zhele3o1992@gmail.com>
2024-01-21 19:10:13 +0300

    How many pages in a book? (6 kyu)

--- js/script.js
+++ js/script.js
@@ -19403,4 +19403,42 @@ const insideOut = x => {
 
 console.log(insideOut('man i need a taxi up to ubud'), 'man i ende a atix up to budu');
 console.log(insideOut('what time are we climbing up the volcano'), 'hwta item are we milcgnib up the lovcona');
-console.log(insideOut('take me to semynak'), 'atek me to mesykan'); 
+console.log(insideOut('take me to semynak'), 'atek me to mesykan');
+
+// How many pages in a book? (6 kyu)
+
+/* 
+Every book has n pages with page numbers 1 to n. 
+The summary is made by adding up the number of digits of all page numbers.
+
+Task: Given the summary, find the number of pages n the book has.
+
+Example
+If the input is summary=25, then the output must be n=17: 
+The numbers 1 to 17 have 25 digits in total: 1234567891011121314151617.
+
+Be aware that you'll get enormous books having up to 100.000 pages.
+
+All inputs will be valid.
+*/
+
+const amountOfPages = summary => {
+	if (summary <= 9) {
+		return summary;
+	}
+	let count = 0;
+	let res = 0;
+	for (let i = 1; i < summary; i++) {
+		count += i.toString().length
+		if (count === summary) {
+			res = i;
+		}
+	}
+	return res
+};
+
+console.log(amountOfPages(5), 5)
+console.log(amountOfPages(25), 17)
+console.log(amountOfPages(1095), 401)
+console.log(amountOfPages(185), 97)
+console.log(amountOfPages(660), 256)
