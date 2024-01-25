47affc3980b2e505637f80d5ef97ccdc3fbaf85b
pavelzhelezniak <zhele3o1992@gmail.com>
2024-01-25 14:07:59 +0300

    MinMaxMin: Bounded Nums (7 kyu)

--- js/script.js
+++ js/script.js
@@ -19483,3 +19483,34 @@ Remember the function will return 0 if no arguments are passed.
 const add = (...arg) => arg.reduce((acc, item, i) => acc + item * (i + 1), 0);
 
 console.log(add(100, 200, 300), 1400);
+
+// MinMaxMin: Bounded Nums (7 kyu)
+
+/* 
+Given an unsorted array of integers, find the smallest number in the array, 
+the largest number in the array, and the smallest number between the two array bounds that is not in the array.
+
+For instance, given the array [-1, 4, 5, -23, 24], the smallest number is -23, 
+the largest number is 24, and the smallest number between the array bounds is -22. 
+You may assume the input is well-formed.
+
+You solution should return an array [smallest, minimumAbsent, largest]
+
+The smallest integer should be the integer from the array with the lowest value.
+
+The largest integer should be the integer from the array with the highest value.
+
+The minimumAbsent is the smallest number between the largest and the smallest number that is not in the array.
+*/
+
+const minMinMax = array => {
+	let minNumber = Math.min(...array);
+	while (array.includes(minNumber)) {
+		minNumber++;
+	}
+	return [Math.min(...array), minNumber, Math.max(...array)]
+};
+
+console.log(minMinMax([-1, 4, 5, -23, 24]), [-23, -22, 24], 'Test Failed');
+console.log(minMinMax([1, 3, -3, -2, 8, -1]), [-3, 0, 8], 'Test Failed');
+console.log(minMinMax([2, -4, 8, -5, 9, 7]), [-5, -3, 9], 'Test Failed');
