a6c94a29873aa0c13d745d55e9d39d1fefe27443
pavelzhelezniak <zhele3o1992@gmail.com>
2024-02-14 22:26:34 +0300

    Find within array (6 kyu)

--- js/script.js
+++ js/script.js
@@ -19910,3 +19910,44 @@ console.log(remember("pippi"), ["p", "i"]);
 console.log(remember('Pippi'), ["p", "i"]);
 console.log(remember("limbojackassin the garden"), ["a", "s", "i", " ", "e", "n"]);
 console.log(remember("11pinguin"), ["1", "i", "n"]);
+
+// Find within array (6 kyu)
+
+/* 
+We'll create a function that takes in two parameters:
+
+a sequence (length and types of items are irrelevant)
+a function (value, index) that will be called on members of the sequence and their index. 
+The function will return either true or false.
+Your function will iterate through the members of the sequence in order until the provided function returns true; 
+at which point your function will return that item's index.
+
+If the function given returns false for all members of the sequence, your function should return -1.
+
+var trueIfEven = function(value, index) { return (value % 2 === 0) };
+findInArray([1,3,5,6,7], trueIfEven) // should === 3
+*/
+
+const findInArray = (array, iterator) => {
+	for (let i = 0; i < array.length; i++) {
+		const elem = array[i];
+		if (iterator(elem, i)) {
+			return i;
+		}
+	}
+
+	return -1;
+};
+
+const trueIfEven = (v, i) => v % 2 === 0;
+const neverTrue = (v, i) => false;
+const trueIfValueEqualsIndex = (v, i) => v === i;
+const trueIfLengthEqualsIndex = (v, i) => v.length === i;
+
+console.log(findInArray([], trueIfEven), -1);
+console.log(findInArray([1, 3, 5, 6, 7], trueIfEven), 3);
+console.log(findInArray([2, 4, 6, 8], trueIfEven), 0);
+console.log(findInArray([2, 4, 6, 8], neverTrue), -1);
+console.log(findInArray([13, 5, 3, 1, 4, 5], trueIfValueEqualsIndex), 4);
+console.log(findInArray(["one", "two", "three", "four", "five", "six"], trueIfLengthEqualsIndex), 4);
+console.log(findInArray(["bc", "af", "d", "e"], trueIfLengthEqualsIndex), -1);
