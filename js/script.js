2e93fd6e7537ee981b665a3505344d9034d0c459
pavelzhelezniak <zhele3o1992@gmail.com>
2024-02-15 14:45:58 +0300

    Function iteration (6 kyu)

--- js/script.js
+++ js/script.js
@@ -19951,3 +19951,33 @@ console.log(findInArray([2, 4, 6, 8], neverTrue), -1);
 console.log(findInArray([13, 5, 3, 1, 4, 5], trueIfValueEqualsIndex), 4);
 console.log(findInArray(["one", "two", "three", "four", "five", "six"], trueIfLengthEqualsIndex), 4);
 console.log(findInArray(["bc", "af", "d", "e"], trueIfLengthEqualsIndex), -1);
+
+// Function iteration (6 kyu)
+
+/* 
+The purpose of this kata is to write a higher-order function which is capable of creating a function 
+that iterates on a specified function a given number of times. 
+This new functions takes in an argument as a seed to start the computation from.
+
+For instance, consider the function getDouble. When run twice on value 3, yields 12 as shown below.
+
+getDouble(3) => 6
+getDouble(6) => 12
+Let us name the new function createIterator and we should be able to obtain the same result using 
+createIterator as shown below:
+
+var doubleIterator = createIterator(getDouble, 2); // This means, it runs *getDouble* twice
+doubleIterator(3) => 12
+For the sake of simplicity, all function inputs to createIterator would be functions returning 
+a small number and number of iterations would always be integers.
+*/
+
+const createIterator = function (func, n) {
+	return function () {
+		let start = func(...arguments)
+		for (let i = 0; i < n - 1; i++) {
+			start = func(start)
+		}
+		return start
+	}
+};
