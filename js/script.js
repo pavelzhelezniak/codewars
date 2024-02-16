b40eba02917bb0793a5b923ba2ae0b217d930baf
pavelzhelezniak <zhele3o1992@gmail.com>
2024-02-16 14:17:32 +0300

    Custom Array Filters (6 kyu)

--- js/script.js
+++ js/script.js
@@ -19981,3 +19981,54 @@ const createIterator = function (func, n) {
 		return start
 	}
 };
+
+// Custom Array Filters (6 kyu)
+
+/* 
+Dave has a lot of data he is required to apply filters to, which are simple enough, 
+but he wants a shorter way of doing so.
+
+He wants the following functions to work as expected:
+
+even    // [1,2,3,4,5].even() should return [2,4]
+odd     // [1,2,3,4,5].odd() should return [1,3,5]
+under   // [1,2,3,4,5].under(4) should return [1,2,3]
+over    // [1,2,3,4,5].over(4) should return [5]
+inRange // [1,2,3,4,5].inRange(1,3) should return [1,2,3]
+They should also work when used together, for example:
+
+[1,2,18,19,20,21,22,30,40,50,100].even().inRange(18,30) // should return [18, 20, 22, 30]
+And finally the filters should only accept integer values from an array, for example:
+
+["a", 1, "b", 300, "x", "q", 63, 122, 181, "z", 0.83, 0.11].even() // should return [300, 122]
+Note: List with non-numbers will be tested as well.
+*/
+
+Array.prototype.even = function () {
+	return this.filter(item => item % 2 === 0 && typeof item === 'number' && item !== 0 && Number.isInteger(item));
+};
+
+Array.prototype.odd = function () {
+	return this.filter(item => item % 2 !== 0 && typeof item === 'number' && Number.isInteger(item));
+}
+
+Array.prototype.under = function (x) {
+	return this.filter(item => item < x && typeof item === 'number' && Number.isInteger(item));
+}
+
+Array.prototype.over = function (x) {
+	return this.filter(item => item < x && typeof item === 'number' && Number.isInteger(item))
+}
+
+Array.prototype.inRange = function (min, max) {
+	return this.filter(item => item >= min && item <= max && typeof item === 'number' && Number.isInteger(item));
+}
+
+console.log([1, 2, 3, 4, 5].even(), [2, 4])
+console.log([1, 2, 3, 4, 5].odd(), [1, 3, 5])
+console.log([1, 2, 3, 4, 5].under(4), [1, 2, 3])
+console.log([1, 2, 3, 4, 5].over(4), [5])
+console.log([1, 2, 3, 4, 5].inRange(1, 3), [1, 2, 3])
+
+console.log([1, 2, 18, 19, 20, 21, 22, 30, 40, 50, 100].even().inRange(18, 30), [18, 20, 22, 30])
+console.log(["a", 1, "b", 300, "x", "q", 63, 122, 181, "z", 0.83, 0.11].even(), [300, 122])
