bfa97645ddf9f7def6dcd4854e24e1d08c6474de
pavelzhelezniak <zhele3o1992@gmail.com>
2024-02-18 20:28:33 +0300

    Three added Characters (6 kyu)

--- js/script.js
+++ js/script.js
@@ -20089,8 +20089,57 @@ class Dinglemouse {
 
 console.log(new Dinglemouse().setName("Bob").setAge(27).setSex('M'),
 	new Dinglemouse().setName("Bob").setAge(27).setSex('M').hello())
-console.log(new Dinglemouse().setAge(27).setSex('M').setName("Bob"), 
-new Dinglemouse().setAge(27).setSex('M').setName("Bob").hello())
-console.log(new Dinglemouse().setName("Alice").setSex('F'), 
-new Dinglemouse().setName("Alice").setSex('F').hello())
+console.log(new Dinglemouse().setAge(27).setSex('M').setName("Bob"),
+	new Dinglemouse().setAge(27).setSex('M').setName("Bob").hello())
+console.log(new Dinglemouse().setName("Alice").setSex('F'),
+	new Dinglemouse().setName("Alice").setSex('F').hello())
 console.log(new Dinglemouse().setName("Batman"), new Dinglemouse().setName("Batman").hello())
+
+// Three added Characters (6 kyu)
+
+/* 
+Given two strings, the first being a random string and the second being the same as the first, 
+but with three added characters somewhere in the string (three same characters),
+
+Write a function that returns the added character
+
+E.g
+string1 = "hello"
+string2 = "aaahello"
+
+// => 'a'
+The above is just an example; the characters could be anywhere in the string and string2 is actually shuffled.
+
+Another example
+string1 = "abcde"
+string2 = "2db2a2ec"
+
+// => '2'
+Note that the added character could also exist in the original string
+
+string1 = "aabbcc"
+string2 = "aacccbbcc"
+
+// => 'c'
+You can assume that string2 will aways be larger than string1, 
+and there will always be three added characters in string2.
+
+Write the function addedChar() that takes two strings and return the added character as described above.
+*/
+
+const addedChar = (s1, s2) => {
+	const s1Sort = s1.split('').sort().join('');
+	const s2Sort = s2.split('').sort().join('');
+
+	for (let i = 0; i < s1Sort.length; i++) {
+		if (s1Sort[i] !== s2Sort[i]) {
+			return s2Sort[i] || s2Sort[i + 1];
+		} else if (i === s1Sort.length - 1) {
+			return s2Sort[i + 1];
+		}
+	}
+};
+
+console.log(addedChar("hello", "checlclo"), 'c', "Wrong!");
+console.log(addedChar("abcde", "2db2a2ec"), '2', "Wrong!");
+console.log(addedChar("aabbcc", "aacccbbcc"), 'c', "Wrong!");
