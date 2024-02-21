81b1c5d3ce4f09309d4788eb7d89cc7de6efaae9
pavelzhelezniak <zhele3o1992@gmail.com>
2024-02-21 17:08:15 +0300

    Find the unique number (6 kyu)

--- js/script.js
+++ js/script.js
@@ -20209,4 +20209,35 @@ const anagramCounter = wordsArray => {
 
 console.log(anagramCounter(['dell', 'ledl', 'abc', 'cba']), 2, "This should be 2");
 console.log(anagramCounter(['dell', 'ledl', 'lled', 'cba']), 3, "This should be 3");
-console.log(anagramCounter(['dell', 'ledl', 'abc', 'cba', 'bca', 'bac', 'cab']), 11, "This should be 11"); 
+console.log(anagramCounter(['dell', 'ledl', 'abc', 'cba', 'bca', 'bac', 'cab']), 11, "This should be 11");
+
+// Find the unique number (6 kyu)
+
+/* 
+Write a function called findUnique which returns the only unique number from an array.
+
+All numbers in the unsorted array are present twice, except the one you have to find. 
+The numbers are always valid integer values between 1 and 2147483647, so no need for type and error checking. 
+The array contains at least one number and may contain millions of numbers. 
+So make sure your solution is optimized for speed.
+*/
+
+const findUnique = numbers => {
+	let res = {};
+	numbers.forEach(number => {
+		if (res.hasOwnProperty(number)) {
+			res[number]++;
+		} else {
+			res[number] = 1;
+		}
+	});
+	const indexValue = Object.values(res).indexOf(1);
+	const key = Object.keys(res)[indexValue];
+	return Number(key);
+};
+
+console.log(findUnique([1, 8, 4, 4, 6, 1, 8]), 6);
+console.log(findUnique([1234567]), 1234567);
+console.log(findUnique([1, 4, 4, 5, 5, 3, 3, 2, 2]), 1);
+console.log(findUnique([2, 2, 5, 5, 4, 3, 3, 1, 1]), 4);
+console.log(findUnique([3, 5, 5, 4, 4, 3, 2, 2, 9]), 9);
