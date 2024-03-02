a8520fbfe7d9ecdcc1abff37908c4329dd1a4a5c
pavelzhelezniak <zhele3o1992@gmail.com>
2024-03-02 15:08:41 +0300

    Count Repeats (6 kyu)

--- js/script.js
+++ js/script.js
@@ -20439,3 +20439,23 @@ class NamedOne {
 		if (/ /.test(v)) [this.firstName, this.lastName] = v.split(' ');
 	}
 }
+
+// Count Repeats (6 kyu)
+
+/* 
+Write a function that returns the count of characters that have to be removed in order 
+to get a string with no consecutive repeats.
+
+Note: This includes any characters
+
+Examples
+'abbbbc'  => 'abc'    #  answer: 3
+'abbcca'  => 'abca'   #  answer: 2
+'ab cca'  => 'ab ca'  #  answer: 1
+*/
+
+const countRepeats = str => str.length - str.replace(/(.)\1+/g, '$1').length;
+
+console.log(countRepeats('AABCCD'), 2);
+console.log(countRepeats('AABCCDA'), 2)
+console.log(countRepeats('AaBBCCC'), 3);
