33810cf742f8fffeb2d2466988bb8884834b66f6
pavelzhelezniak <zhele3o1992@gmail.com>
2024-03-04 16:48:32 +0300

    The Lamp: Revisited (6 kyu)

--- js/script.js
+++ js/script.js
@@ -20498,3 +20498,39 @@ console.log(maxAndMin([3, 10, 5], [20, 7, 15, 8]), [17, 2]);
 console.log(maxAndMin([3], [20]), [17, 17]);
 console.log(maxAndMin([3, 10, 5], [3, 10, 5]), [7, 0]);
 console.log(maxAndMin([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]), [9, 1]);
+
+// The Lamp: Revisited (6 kyu)
+
+/* 
+Define a class called Lamp. 
+It will have a string attribute for color and boolean attribute, on, that will refer to whether the lamp is on or not. 
+Define your class constructor with a parameter for color and assign on as false on initialize.
+
+Give the lamp an instance method called toggleSwitch that will switch the value of the on attribute.
+
+Define another instance method called state that will return 
+"The lamp is on." if it's on and "The lamp is off." otherwise.
+*/
+
+class Lamp {
+	constructor(color) {
+		this.color = color;
+		this.on = false;
+	}
+
+	toggleSwitch() {
+		this.on = !this.on
+	}
+
+	state() {
+		return this.on ? "The lamp is on." : "The lamp is off."
+	}
+}
+
+const myLamp = new Lamp("Blue")
+console.log(myLamp.color, "Blue")
+console.log(myLamp.on, false)
+console.log(myLamp.state(), "The lamp is off.")
+// now switch it on
+myLamp.toggleSwitch()
+console.log(myLamp.state(), "The lamp is on.")
