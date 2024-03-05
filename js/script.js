3c97dab1e67523cc0bdd0a60517310b56d958218
pavelzhelezniak <zhele3o1992@gmail.com>
2024-03-05 19:54:26 +0300

    Simple Fun #124: Lamps (7 kyu)

--- js/script.js
+++ js/script.js
@@ -20534,3 +20534,56 @@ console.log(myLamp.state(), "The lamp is off.")
 // now switch it on
 myLamp.toggleSwitch()
 console.log(myLamp.state(), "The lamp is on.")
+
+// Simple Fun #124: Lamps (7 kyu)
+
+/* 
+Task
+N lamps are placed in a line, some are switched on and some are off. 
+What is the smallest number of lamps that need to be switched so that on and off lamps will alternate with each other?
+
+You are given an array a of zeros and ones - 1 mean switched-on lamp and 0 means switched-off.
+
+Your task is to find the smallest number of lamps that need to be switched.
+
+Example
+For a = [1, 0, 0, 1, 1, 1, 0], the result should be 3. a --> 1 0 0 1 1 1 0 swith --> 0 1 0 became--> 0 1 0 1 0 1 0
+
+Input/Output
+[input] integer array a
+
+array of zeros and ones - initial lamp setup, 1 mean switched-on lamp and 0 means switched-off.
+
+2 < a.length <= 1000
+
+[output] an integer
+
+minimum number of switches.
+*/
+
+const lamps = a => {
+	let switch1 = 0;
+	let switch2 = 0;
+	for (let i = 0; i < a.length; i++) {
+		if (i % 2 === 0) {
+			if (a[i] !== 1) {
+				switch1++;
+			} else {
+				switch2++;
+			};
+		} else {
+			if (a[i] !== 0) {
+				switch1++;
+			} else {
+				switch2++;
+			};
+		}
+	}
+
+	return Math.min(switch1, switch2);
+};
+
+console.log(lamps([1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1]), 5)
+console.log(lamps([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), 6)
+console.log(lamps([1, 0, 1]), 0)
+console.log(lamps([1, 0, 1, 0]), 0)
