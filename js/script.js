91fa64693744874533ffe4124674c65f5c1f4177
pavelzhelezniak <zhele3o1992@gmail.com>
2024-03-12 17:07:55 +0300

    Find the Nexus of the Codewars Universe (6 kyu)

--- js/script.js
+++ js/script.js
@@ -20727,3 +20727,44 @@ const customers = {
 	'Jane Doe': [5, 17, 30, 33, 40, 22, 26, 10, 11, 45] // Has six orders above 20$, which means FREE PIZZA!
 }
 console.log(pizzaRewards(customers, minOrders, minPrice), ['Jane Doe'])
+
+// Find the Nexus of the Codewars Universe (6 kyu)
+
+/* 
+Not to brag, but I recently became the nexus of the Codewars universe! 
+My honor and my rank were the same number. I cried a little.
+
+Complete the method that takes a hash/object/directory/association list of users, 
+and find the nexus: the user whose rank is the closest is equal to his honor. 
+Return the rank of this user. For each user, the key is the rank and the value is the honor.
+
+If nobody has an exact rank/honor match, return the rank of the user who comes closest. 
+If there are several users who come closest, return the one with the lowest rank (numeric value). 
+The hash will not necessarily contain consecutive rank numbers; return the best match from the ranks provided.
+
+Example
+			rank    honor
+users = {  1  =>  93,
+			 10  =>  55,
+			 15  =>  30,
+			 20  =>  19,    <--- nexus
+			 23  =>  11,
+			 30  =>   2 }
+*/
+
+const nexus = users => {
+	let rank = 0;
+	let diff = Infinity;
+	for (const user in users) {
+		const intermedDiff = Math.abs(user - users[user]);
+		if (intermedDiff < diff) {
+			diff = intermedDiff;
+			rank = Number(user);
+		}
+	}
+
+	return rank;
+};
+
+console.log(nexus({ "1": 3, "3": 3, "5": 3 }), 3);
+console.log(nexus({ '1': 93, '10': 55, '15': 30, '20': 19, '23': 11, '30': 2 }), 20);
