2e4e78e73518d3c2711fe67075b492f1581ba3a6
pavelzhelezniak <zhele3o1992@gmail.com>
2024-03-14 16:55:28 +0300

    Doors in the school (6 kyu)

--- js/script.js
+++ js/script.js
@@ -20768,3 +20768,36 @@ const nexus = users => {
 
 console.log(nexus({ "1": 3, "3": 3, "5": 3 }), 3);
 console.log(nexus({ '1': 93, '10': 55, '15': 30, '20': 19, '23': 11, '30': 2 }), 20);
+
+// Doors in the school (6 kyu)
+
+/* 
+In the morning all the doors in the school are closed. 
+The school is quite big: there are N doors. Then pupils start coming. 
+It might be hard to believe, but all of them want to study! Also, 
+there are exactly N children studying in this school, and they come one by one.
+
+When these strange children pass by some doors they change their status 
+(i.e. Open -> Closed, Closed -> Open). Each student has their number, 
+and each i-th student alters the status of every i-th door. For example: 
+when the first child comes to the schools, he changes every first door (he opens all of them). 
+The second one changes the status of every second door (he closes some doors: the 2nd, the 4th and so on). 
+Finally, when the last one – the n-th – comes to the school, he changes the status of each n-th door 
+(there's only one such door, though).
+
+You need to count how many doors are left opened after all the students have come.
+*/
+
+const doors = n => {
+	const doors = Array.from(Array(n), () => false);
+	for (let i = 0; i < n; i++) {
+		for (let j = i; j < n; j = j + i + 1) {
+			doors[j] = !doors[j];
+		}
+	}
+	return doors.filter(item => item).length
+};
+
+console.log(doors(5), 2);
+console.log(doors(10), 3);
+console.log(doors(100), 10);
