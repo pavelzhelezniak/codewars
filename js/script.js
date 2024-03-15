b9bafb460f35577cf3393a9d678fcc8c29e3f1c8
pavelzhelezniak <zhele3o1992@gmail.com>
2024-03-15 14:39:39 +0300

    PatternCraft - State (6 kyu)

--- js/script.js
+++ js/script.js
@@ -20801,3 +20801,56 @@ const doors = n => {
 console.log(doors(5), 2);
 console.log(doors(10), 3);
 console.log(doors(100), 10);
+
+// PatternCraft - State (6 kyu)
+
+/* 
+The State Design Pattern can be used, for example, to manage the state of a tank in the StarCraft game.
+
+The pattern consists in isolating the state logic in different classes rather 
+than having multiple ifs to determine what should happen.
+
+Your Task
+Complete the code so that when a Tank goes into SiegeMode it cannot move and its damage goes to 20. 
+When it goes to TankMode it should be able to move and the damage should be set to 5.
+
+You have 3 classes:
+
+Tank: has a state, canMove and damage properties
+SiegeState and TankState: has canMove and damage properties
+Note: The tank initial state should be TankState.
+*/
+
+class SiegeState {
+	constructor() {
+		this.move = false;
+		this.dmg = 20;
+	}
+}
+
+class TankState {
+	constructor() {
+		this.move = true;
+		this.dmg = 5;
+	}
+}
+
+class Tank {
+	constructor() {
+		this.state = new TankState();
+	}
+	get canMove() {
+		return this.state.move;
+	}
+	get damage() {
+		return this.state.dmg;
+	}
+}
+
+let tank = new Tank();
+
+console.log(tank.canMove, true);
+console.log(tank.damage, 5);
+tank.state = new SiegeState();
+console.log(tank.canMove, false);
+console.log(tank.damage, 20);
