ec5f7d2c7e8194a1d3c52993f1329f3c3334adc1
pavelzhelezniak <zhele3o1992@gmail.com>
2024-04-07 13:40:42 +0300

    Weight for weight (5 kyu)

--- js/script.js
+++ js/script.js
@@ -20891,11 +20891,19 @@ For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g o
 Ingredients that are not present in the objects, can be considered as 0.
 */
 
-function cakes(recipe, available) {
+const cakes = (recipe, available) => {
+	let count = 100;
 	for (const ingredient in recipe) {
-		console.log(ingredient)
-
+		if (available.hasOwnProperty(ingredient)) {
+			const currentCount = Math.floor(available[ingredient] / recipe[ingredient]);
+			if (currentCount < count) {
+				count = currentCount;
+			}
+		} else {
+			return 0;
+		}
 	}
+	return count;
 };
 
 let recipe = { flour: 500, sugar: 200, eggs: 1 };
@@ -20905,3 +20913,40 @@ console.log(cakes(recipe, available), 2);
 recipe = { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 };
 available = { sugar: 500, flour: 2000, milk: 2000 };
 console.log(cakes(recipe, available), 0);
+
+// Weight for weight (5 kyu)
+
+/* 
+My friend John and I are members of the "Fat to Fit Club (FFC)". 
+John is worried because each month a list with the weights of members is published and each month 
+he is the last on the list which means he is the heaviest.
+
+I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list". 
+It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
+
+For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
+
+Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
+
+Example:
+"56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes: 
+
+"100 180 90 56 65 74 68 86 99"
+When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers:
+
+180 is before 90 since, having the same "weight" (9), it comes before as a string.
+
+All numbers in the list are positive numbers and the list can be empty.
+*/
+
+const orderWeight = strng => {
+	const sum = (str) => str.split('').reduce((acc, num) => (acc + (+num)), 0);
+	return strng.split(' ').sort((a, b) => {
+		const sumA = sum(a);
+		const sumB = sum(b);
+		return sumA === sumB ? a.localeCompare(b) : sumA - sumB;
+	}).join(' ');
+};
+
+console.log(orderWeight("103 123 4444 99 2000"), "2000 103 123 4444 99");
+console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"), "11 11 2000 10003 22 123 1234000 44444444 9999");
