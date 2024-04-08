3651af145fff9e4fb86f838787016d04d4492410
pavelzhelezniak <zhele3o1992@gmail.com>
2024-04-08 19:21:20 +0300

    Tic-Tac-Toe Checker (5 kyu)

--- js/script.js
+++ js/script.js
@@ -20950,3 +20950,47 @@ const orderWeight = strng => {
 
 console.log(orderWeight("103 123 4444 99 2000"), "2000 103 123 4444 99");
 console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"), "11 11 2000 10003 22 123 1234000 44444444 9999");
+
+// Tic-Tac-Toe Checker (5 kyu)
+
+/* 
+If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, 
+wouldn't we? Our goal is to create a function that will check that for us!
+
+Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 
+1 if it is an "X", or 2 if it is an "O", like so:
+
+[[0, 0, 1],
+ [0, 1, 2],
+ [2, 1, 0]]
+We want our function to return:
+
+-1 if the board is not yet finished AND no one has won yet (there are empty spots),
+1 if "X" won,
+2 if "O" won,
+0 if it's a cat's game (i.e. a draw).
+You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+*/
+
+const isSolved = board => {
+	const flatBoard = board.reduce((acc, item) => [...acc, ...item], []);
+	for (let i = 0; i < 3; i++) {
+		if (flatBoard[3 * i] && flatBoard[3 * i] === flatBoard[3 * i + 1] && flatBoard[3 * i] === flatBoard[3 * i + 2]) {
+			return flatBoard[3 * i];
+		}
+		if (flatBoard[i] && flatBoard[i] === flatBoard[i + 3] && flatBoard[i] === flatBoard[i + 6]) {
+			return flatBoard[i];
+		}
+	}
+
+	if (flatBoard[0] && flatBoard[0] === flatBoard[4] && flatBoard[0] === flatBoard[8]) {
+		return flatBoard[0];
+	}
+	if (flatBoard[2] && flatBoard[2] === flatBoard[4] && flatBoard[2] === flatBoard[8]) {
+		return flatBoard[2];
+	}
+
+	return flatBoard.includes(0) ? -1 : 0;
+};
+
+console.log(isSolved([[0, 0, 1], [0, 1, 2], [2, 1, 0]]) === -1);
