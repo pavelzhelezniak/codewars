13c1d34b454e1fe28168078459019e0778e0a017
pavelzhelezniak <zhele3o1992@gmail.com>
2024-04-18 17:02:51 +0300

    Permute a Palindrome (6 kyu)

--- js/script.js
+++ js/script.js
@@ -21125,3 +21125,32 @@ console.log(isMerge('codewars', 'cdwr', 'oeas'), true);
 console.log(isMerge('Making progress', 'Mak pross', 'inggre'), true);
 console.log(isMerge('codewars', 'code', 'code'), false);
 console.log(isMerge('More progress', 'More ess', 'pro'), false);
+
+// Permute a Palindrome (6 kyu)
+
+/* 
+Write a function that will check whether ANY permutation of the characters of the input string is a palindrome. 
+Bonus points for a solution that is efficient and/or that uses only built-in language functions. 
+Deem yourself brilliant if you can come up with a version that does not use any function whatsoever.
+
+For this kata assume that all characters are lowercase.
+
+Example
+madam -> True
+adamm -> True
+junk -> False
+
+Hint
+The brute force approach would be to generate all the permutations of the string and 
+check each one of them whether it is a palindrome. However, an optimized approach will not require this at all.
+*/
+
+const permuteAPalindrome = input => [...input].sort().join('').replace(/(.)\1/g, '').length < 2;
+
+console.log(permuteAPalindrome("aaa"), true);
+console.log(permuteAPalindrome("baa"), true);
+console.log(permuteAPalindrome("aab"), true);
+console.log(permuteAPalindrome("baabcd"), false);
+console.log(permuteAPalindrome("racecars"), false);
+console.log(permuteAPalindrome("abcdefghba"), false);
+console.log(permuteAPalindrome(""), true);
