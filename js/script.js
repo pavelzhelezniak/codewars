b2b23cc5438e5d3d93f07ced6d513ccdfb175375
pavelzhelezniak <zhele3o1992@gmail.com>
2024-04-21 17:46:00 +0300

    Matrix Transpose (6 kyu)

--- js/script.js
+++ js/script.js
@@ -21154,3 +21154,36 @@ console.log(permuteAPalindrome("baabcd"), false);
 console.log(permuteAPalindrome("racecars"), false);
 console.log(permuteAPalindrome("abcdefghba"), false);
 console.log(permuteAPalindrome(""), true);
+
+// Matrix Transpose (6 kyu)
+
+/* 
+Write a function that outputs the transpose of a matrix - a new matrix where the columns and rows of the original are swapped.
+
+For example, the transpose of:
+
+| 1 2 3 |
+| 4 5 6 |
+is
+
+| 1 4 |
+| 2 5 |
+| 3 6 |
+The input to your function will be an array of matrix rows. 
+You can assume that each row has the same length, and that the height and width of the matrix are both positive.
+*/
+
+const transpose = matrix => {
+	const resMatrix = [];
+	for (let i = 0; i < matrix[0].length; i++) {
+		const res = [];
+		for (let j = 0; j < matrix.length; j++) {
+			res.push(matrix[j][i]);
+		}
+		resMatrix.push(res);
+	}
+	return resMatrix;
+};
+
+console.log(transpose([[1]]), [[1]]);
+console.log(transpose([[1, 2, 3], [4, 5, 6]]), [[1, 4], [2, 5], [3, 6]]);
