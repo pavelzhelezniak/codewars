7a073f35c36914ba3c79892a082a3ae7bbcca30d
pavelzhelezniak <zhele3o1992@gmail.com>
2024-04-29 20:43:27 +0300

    Almost Even (6 kyu)

--- js/script.js
+++ js/script.js
@@ -21302,3 +21302,42 @@ console.log(calculateDamage("grass", "electric", 57, 19), 150);
 console.log(calculateDamage("grass", "water", 40, 40), 100);
 console.log(calculateDamage("grass", "fire", 35, 5), 175);
 console.log(calculateDamage("fire", "electric", 10, 2), 250);
+
+// Almost Even (6 kyu)
+
+/* 
+We need the ability to divide an unknown integer into a given number of even parts - or at least as even as they can be. 
+The sum of the parts should be the original value, but each part should be an integer, and they should be as close as possible.
+
+Complete the function so that it returns an array of integers representing the parts. 
+If the input number is too small to split it into requested amount of parts, the additional parts should have value 0. 
+Ignoring the order of the parts, there is only one valid solution for each input to your function!
+
+Example:
+splitInteger(20, 6)  // returns [3, 3, 3, 3, 4, 4]
+Inputs
+The input to your function will always be valid for this kata.
+*/
+
+const splitInteger = (num, parts) => {
+	const n = Math.floor(num / parts);
+	const arr = [];
+	for (let i = 0; i < parts; i++) {
+		arr.push(n)
+	}
+	if (arr.reduce((a, b) => a + b, 0) === num) {
+		return arr;
+	}
+
+	for (let i = 0; i < parts; i++) {
+		arr[i]++;
+		if (arr.reduce((a, b) => a + b, 0) === num) {
+			return arr;
+		}
+	}
+};
+
+console.log(splitInteger(10, 1).sort(), [10]);
+console.log(splitInteger(2, 2).sort(), [1, 1]);
+console.log(splitInteger(20, 5).sort(), [4, 4, 4, 4, 4]);
+console.log(splitInteger(20, 6).sort(), [3, 3, 3, 3, 4, 4]);
